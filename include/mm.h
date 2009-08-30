38dbc87c3af862cdae51f4286b7e62d48b025fab
Rink Springer <rink@rink.nu>
2009-08-30 22:02:23 +0200

    Introduce the memory manager.
    This is just a simple first-fit zone-aware memory allocator, which interacts with the machine-dependant things using the vm_xxx() series of calls.

---
+++ include/mm.h
@@ -0,0 +1,67 @@
+#include "types.h"
+
+#ifndef __MM_H__
+#define __MM_H__
+
+#define MM_CHUNK_FLAG_USED	0x0001
+
+/*
+ * A zone describes a contiguous piece of memory. Every zone consists of a
+ * number of chunks, and a chunk can either be allocated or be available.
+ */
+struct MM_ZONE {
+	/* Zone's base address */
+	addr_t	address;
+
+	/* Zone length, in pages */
+	size_t	length;
+
+	/* Total number of chunks */
+	size_t num_chunks;
+
+	/* Number of available pages */
+	size_t	num_free;
+
+	/*
+	 * Maximum number of available contiguous pages. This is needed because
+	 * even though a zone may have a large amount of memory available, we
+	 * need to know before-hand whether the zone needs to be considered at
+	 * all.
+	 */
+	size_t	num_cont_free;
+
+	/* Flags */
+	int	flags;
+
+	/* Pointer to the first chunk */
+	struct MM_CHUNK* chunks;
+
+	/* Pointer to the next zone, if any */
+	struct MM_ZONE* next_zone;
+};
+
+/*
+ *  A chunk 
+ */
+struct MM_CHUNK {
+	/* Chunk's base address */
+	addr_t	address;
+
+	/* Chunk flags */
+	int	flags;
+
+	/*
+	 * The chain length is used to determine the number of next adjacent
+	 * chunks after us; this is needed when we are freeing a chunk, since
+	 * we need to free anything in the chain too.
+	 */
+	int	chain_length;
+}; 
+
+void mm_init();
+void mm_zone_add(addr_t addr, size_t length);
+void kmem_stats(size_t* avail, size_t* total);
+void* kmem_alloc(size_t len);
+void* kmalloc(size_t len);
+
+#endif /* __MM_H__ */
---
+++ kernel/kern/mm.c
@@ -0,0 +1,167 @@
+#include "mm.h"
+#include "vm.h"
+#include "lib.h"
+
+static int zone_root_initialized;
+static struct MM_ZONE* zone_root;
+
+void
+mm_init()
+{
+	zone_root_initialized = 0;
+	zone_root = NULL;
+}
+
+void
+mm_zone_add(addr_t addr, size_t length)
+{
+	KASSERT(addr % PAGE_SIZE == 0, "addr 0x%x isn't page-aligned", addr);
+	KASSERT(length % PAGE_SIZE == 0, "length 0x%x isn't page-aligned", length);
+
+	/*
+	 * First of all, we need to calculate the number of pages needed to store the
+	 * administration involved in this zone.
+	 *
+	 * Note that we actually allocate these structures from the end of
+	 * the kernel memory, because this ensures we do not overwrite
+	 * important structures etc.
+	 */
+	int num_pages =
+		(
+			/* MM_ZONE struct describing the zone itself */
+			sizeof(struct MM_ZONE) +
+			/* each page is described by a MM_CHUNK structure */
+			(length / PAGE_SIZE) * sizeof(struct MM_CHUNK) +
+			PAGE_SIZE - 1
+		) / PAGE_SIZE;
+	addr_t admin_addr = addr + length - (num_pages * PAGE_SIZE);
+	vm_map(admin_addr, num_pages);
+
+	/*
+	 * Initialize the zone.
+	 */
+	struct MM_ZONE* zone = (struct MM_ZONE*)admin_addr;
+	zone->address = addr;
+	zone->length = length / PAGE_SIZE;
+	zone->num_chunks = length / PAGE_SIZE;
+	zone->num_free = zone->num_chunks - num_pages;
+	zone->num_cont_free = zone->num_free;
+	zone->flags = 0;
+	zone->chunks = (struct MM_CHUNK*)(admin_addr + sizeof(struct MM_ZONE));
+	zone->next_zone = NULL;
+#if 0
+kprintf("zone_add: addr=%x,len=%x,num_chunks=%x,num_free=%x,np=%x\n", addr, length, zone->num_chunks, zone->length, zone->num_free);
+kprintf("zone_add: addr=%x, chunks=%x\n", zone->address, zone->chunks);
+#endif
+
+	/*
+	 * Figure out the location of the first chunk's data - this is directly after
+	 * the zone and chunk structures, rounded to a page.
+	 */
+	addr_t data_addr = addr + (num_pages * PAGE_SIZE);
+
+	/*
+	 * Initialize the chunks, one at a time.
+	 */
+	unsigned int n;
+	for (n = 0; n < zone->num_chunks; n++, data_addr += PAGE_SIZE) {
+		struct MM_CHUNK* chunk = (struct MM_CHUNK*)((addr_t)zone->chunks + n * sizeof(struct MM_CHUNK));
+		chunk->address = data_addr;
+		chunk->flags = 0;
+		chunk->chain_length = zone->num_chunks - n;
+	}
+
+	/* Finally, add this zone to the available zones */
+	if (zone_root_initialized) {
+		struct MM_ZONE* curzone;
+		for (curzone = zone_root; curzone->next_zone != NULL; curzone = curzone->next_zone);
+		curzone->next_zone = zone;
+	} else {
+		zone_root = zone; zone_root_initialized = 1;
+	}
+}
+
+void
+kmem_stats(size_t* avail, size_t* total)
+{
+	*avail = 0; *total = 0;
+
+	if (!zone_root_initialized)
+		return;
+
+	struct MM_ZONE* curzone = zone_root;
+	while (1) {
+		*total += curzone->length * PAGE_SIZE;
+		*avail += curzone->num_free * PAGE_SIZE;
+		curzone = curzone->next_zone;
+		if (curzone == NULL)
+			break;
+	}
+}
+
+/*
+ * Allocates a chunk of memory, but does not map it.
+ */
+void*
+kmem_alloc(size_t len)
+{
+	struct MM_ZONE* curzone = zone_root;
+
+	/* round up to a full page */
+	if (len % PAGE_SIZE > 0)
+		len += PAGE_SIZE - (len % PAGE_SIZE);
+
+	len /= PAGE_SIZE;
+	for (curzone = zone_root; curzone != NULL; curzone = curzone->next_zone) {
+
+//kprintf("kmem_alloc(): inspecting zone %x\n", curzone);
+
+		/* Skip any zone that hasn't got enough space */
+		if (curzone->num_cont_free < len)
+			continue;
+
+		unsigned int n;
+		for (n = 0; n < curzone->num_chunks; n++) {
+			struct MM_CHUNK* chunk = (struct MM_CHUNK*)((addr_t)curzone->chunks + n * sizeof(struct MM_CHUNK));
+
+//kprintf("kmem_alloc():  inspecting chunk %x/%x @ %x (chunks=%x)\n", n, curzone->num_chunks, chunk, curzone->chunks);
+
+			if (chunk->flags & MM_CHUNK_FLAG_USED)
+				continue;
+			if (chunk->chain_length < len)
+				continue;
+
+			/*
+			 * OK, we hit a chunk that we can use. 
+			 */
+			addr_t addr = chunk->address;
+			while (len > 0) {
+				chunk->flags |= MM_CHUNK_FLAG_USED;
+				chunk->chain_length = len;
+				chunk++; len--;
+			}
+
+//kprintf("kmem_alloc():  allocated chunk, address=%x\n", addr);
+
+			return (void*)addr;
+		}
+	}
+
+	/* No zones available to honor this request */
+	return NULL;
+}
+
+void*
+kmalloc(size_t len)
+{
+	/* Round len up to a full page*/
+	if (len % PAGE_SIZE > 0)
+		len += PAGE_SIZE - (len % PAGE_SIZE);
+	void* ptr = kmem_alloc(len);
+	len /= PAGE_SIZE;
+	vm_map((addr_t)ptr, len);
+	kprintf("kmalloc(): got ptr=%x, len=%x\n", ptr, len);
+	return ptr;
+}
+
+/* vim:set ts=2 sw=2: */
