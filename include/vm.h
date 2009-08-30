ead26756e45a5d52418561af0f1f0e319e7874c0
Rink Springer <rink@rink.nu>
2009-08-30 22:14:44 +0200

    Restructure the i386 VM code to be able to cope with memory manager requirements.
    Basically, this means we have a temporary page which is used for mappings until we can safely allocate memory (vm_map will call vm_map_bootstrap in this case). Once we reach the state in which memory is available, vm_init() should be called which ensures the temporary mapping will be upgraded to a true mapping)
    
    Note that the current code in vm_init() expects that NULL is never an available pointer, and that a single page directory entry is enough to map any memory necessary to finish the VM bootstrap.

--- include/vm.h
+++ include/vm.h
@@ -3,7 +3,13 @@
 #ifndef __VM_H__
 #define __VM_H__
 
+/* Map a piece of memory */
+void vm_map(addr_t addr, size_t len);
+
 /* Map memory of a device so it can be accessed */
 void* vm_map_device(addr_t addr, size_t len);
+
+/* Initialize the memory manager */
+void vm_init();
 
 #endif /* __VM_H__ */
--- kernel/arch/i386/vm.c
+++ kernel/arch/i386/vm.c
@@ -1,40 +1,122 @@
 #include "i386/types.h"
 #include "i386/vm.h"
+#include "lib.h"
+#include "mm.h"
 #include "param.h"
 
-/* XXX This is a hack - a terrible hack - and must be nuked soon */
-static uint8_t temp_pt_entry[PAGE_SIZE];
+extern void* temp_pt_entry;
+static int vm_initialized = 0;
 
-void*
-vm_map_device(addr_t addr, size_t len)
+/*
+ * vm_map_bootstrap() is magic; it will be used when the VM isn't fully
+ * initialized yet and can map at most one page safely.
+ */
+static void
+vm_map_bootstrap(addr_t addr, size_t num_pages)
 {
-	void* result = (void*)addr;
-	/*KASSERT(addr & PAGE_SIZE == 0, "vm_map_device: unaligned address 0x%x", addr);*/
+//	kprintf("vm_map_bootstrap(): addr=%x, num_pages=%x\n", addr, num_pages);
 
-	while (len > 0) {
+	while (num_pages > 0) {
 		uint32_t pd_entrynum = addr >> 22;
 		if (pagedir[pd_entrynum] == 0) {
-			/*
-			 * This page isn't yet mapped, so we need to allocate a new page entry
-			 * where we can map the pointer.
-			 *
-			 * XXX until a memory allocator is created; recycle the same page...
-			 *     since we map only a single temporary page... as long as we
-			 *     only map a console, this is fine anyway XXX
-			 */
-			void* new_entry = (void*)&temp_pt_entry;
-			pagedir[pd_entrynum] = ((addr_t)new_entry - KERNBASE) | PDE_P | PTE_RW;
+			pagedir[pd_entrynum] = ((addr_t)&temp_pt_entry & ~KERNBASE) | PDE_P | PTE_RW;
 		}
 
 		uint32_t* pt = (uint32_t*)(pagedir[pd_entrynum] & ~(PAGE_SIZE - 1) | KERNBASE);
 		pt[(((addr >> 12) & ((1 << 10) - 1)))] = addr | PTE_P | PTE_RW;
 
-		if (len < PAGE_SIZE)
-			break;
-		len -= PAGE_SIZE;
+		num_pages--;
+		addr += PAGE_SIZE;
+	}
+}
+
+void
+vm_map(addr_t addr, size_t num_pages)
+{
+	KASSERT(addr % PAGE_SIZE == 0, "unaligned address 0x%x", addr);
+
+	if (!vm_initialized)
+		return vm_map_bootstrap(addr, num_pages);
+
+//	kprintf("vm_map(): addr=%x, num_pages=%x\n", addr, num_pages);
+
+	while (num_pages > 0) {
+		uint32_t pd_entrynum = addr >> 22;
+		if (pagedir[pd_entrynum] == 0) {
+
+			/*
+			 * This page isn't yet mapped, so we need to allocate a new page entry
+			 * where we can map the pointer.
+			 */
+			addr_t new_entry = (addr_t)kmalloc(PAGE_SIZE);
+			memset((void*)new_entry, 0, PAGE_SIZE);
+			pagedir[pd_entrynum] = new_entry | PDE_P | PTE_RW;
+		}
+
+		uint32_t* pt = (uint32_t*)(pagedir[pd_entrynum] & ~(PAGE_SIZE - 1));
+		pt[(((addr >> 12) & ((1 << 10) - 1)))] = addr | PTE_P | PTE_RW;
+
+		num_pages--;
+		addr += PAGE_SIZE;
+	}
+}
+
+void*
+vm_map_device(addr_t addr, size_t len)
+{
+	len = (len + PAGE_SIZE - 1) / PAGE_SIZE;
+	vm_map(addr, len);
+	return (void*)addr;
+}
+
+void
+vm_init()
+{
+	/*
+	 * This will be called once the first memory zone has been allocated; this
+	 * means we can finally truely allocate memory and get rid of the
+	 * temp_pt_entry hack...
+	 */
+	uint32_t j;
+	for (j = 0; j < PAGE_SIZE / sizeof(uint32_t); j++) {
+		if ((pagedir[j] & ~(PAGE_SIZE - 1)) != ((addr_t)&temp_pt_entry & ~KERNBASE))
+			continue;
+
+		/*
+		 * OK, we have found the page entry in question. Grab a new
+		 * page first, so we can copy the data.
+		 */
+		void* new_page = kmem_alloc(PAGE_SIZE);
+		KASSERT(new_page != NULL, "got null pointer from kmem_alloc");
+
+		/*
+		 * Now, we need to create some mapping to this page; it doesn't matter
+		 * where we do it, since this is only temporary (remember that our goal is
+		 * to get rid of the temp_pt_entry stuff altogether, so we can add things to
+		 * it as we please) - for now, ensure 0x0 is available there XXX
+		 */
+		*((uint32_t*)&temp_pt_entry) = (addr_t)new_page | PTE_P | PTE_RW;
+
+		/*
+		 * Calculate the address of the new page we just mapped.
+		 */
+		uint32_t* new_page_ptr = (uint32_t*)(j << 22);
+		memcpy(new_page_ptr, &temp_pt_entry, PAGE_SIZE);
+
+		/* Get rid of the temporary mapping we just created */
+		new_page_ptr[0] = 0;
+
+		/* And create the true mapping */
+		new_page_ptr[(addr_t)new_page >> 12] = (addr_t)new_page | PTE_P | PTE_RW;
+
+		/* Finally, we can activate this mapping */
+		pagedir[j] = (addr_t)new_page | PTE_P | PTE_RW;
 	}
 
-	return (void*)result;
+	/*
+	 * We are in business and can use vm_map() now.
+	 */
+	vm_initialized = 1;
 }
 
 /* vim:set ts=2 sw=2: */
