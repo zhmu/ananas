665bd2a7e2f0126d60380a9a4068e944881ee37d
Rink Springer <rink@rink.nu>
2009-08-30 23:13:42 +0200

    Print some nice information and use the new vga driver.

--- kernel/kern/startup.c
+++ kernel/kern/startup.c
@@ -1,9 +1,15 @@
 #include "dev/console.h"
+#include "lib.h"
 
 void
 mi_startup()
 {
-	console_init();
+	vga_init();
+
+	size_t mem_avail, mem_total;
+	kmem_stats(&mem_avail, &mem_total);
+	kprintf("Hello world, this is Ananas/%s %x.%x\n", "i386", 0, 1);
+	kprintf("Memory available: 0x%x / 0x%x bytes\n", mem_avail, mem_total);
 
 	while (1);
 }
