c0a062bcdc9ff002ac7671f485b1c28312204cab
Rink Springer <rink@rink.nu>
2009-09-17 20:59:14 +0200

    Implement i386-specific I/O instructions.

--- include/i386/io.h
+++ include/i386/io.h
@@ -1,8 +1,13 @@
+#include "types.h"
+
 #ifndef __I386_IO_H__
 #define __I386_IO_H__
 
-#define outb(port,a) asm("outb %0, %1" : : "a" (a), "id" (port))
-#define outw(port,a) asm("outw %0, %1" : : "a" (a), "d" (port))
-#define outl(port,a) asm("outl %0, %1" : : "a" (a), "d" (port))
+void outb(uint16_t port, uint8_t data);
+void outw(uint16_t port, uint16_t data);
+void outl(uint16_t port, uint32_t data);
+uint8_t inb(uint16_t port);
+uint16_t inw(uint16_t port);
+uint32_t inl(uint16_t port);
 
 #endif /* __I386_IO_H__ */
