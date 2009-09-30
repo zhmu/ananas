9d3fb892cc0b896df242a356276ac690b6533330
Rink Springer <rink@rink.nu>
2009-09-30 22:54:50 +0200

    Add a dummy putchar syscall, so we can see something!

--- kernel/kern/syscall.c
+++ kernel/kern/syscall.c
@@ -3,8 +3,15 @@
 register_t
 syscall(struct SYSCALL_ARGS* a)
 {
-	kprintf("syscall, no=%x,a1=%u,a2=%u,a3=%u,a4=%u,a5=%u\n",
-	 a->number, a->arg1, a->arg2, a->arg3, a->arg4, a->arg5);
+	switch(a->number) {
+		case 0xfffe0001: /* XXX temp print */
+			kprintf("%c", a->arg1);
+			break;
+		default:
+			kprintf("syscall, no=%x,a1=%u,a2=%u,a3=%u,a4=%u,a5=%u\n",
+			 a->number, a->arg1, a->arg2, a->arg3, a->arg4, a->arg5);
+			break;
+	}
 
 	return 0xf00db00b;
 }
