9fcf94f3ad09c91c77475392f083f0b72bc5ec10
Rink Springer <rink@rink.nu>
2009-10-18 00:32:53 +0200

    i386: the keyboard only uses a single IRQ

--- kernel/arch/i386/dev/atkbd.c
+++ kernel/arch/i386/dev/atkbd.c
@@ -15,7 +15,7 @@ static int
 atkbd_attach(device_t dev)
 {
 	void* res_io  = device_alloc_resource(dev, RESTYPE_IO, 7);
-	void* res_irq = device_alloc_resource(dev, RESTYPE_IRQ, 1);
+	void* res_irq = device_alloc_resource(dev, RESTYPE_IRQ, 0);
 	if (res_io == NULL || res_irq == NULL)
 		return 1; /* XXX */
 	atkbd_port = (uint32_t)res_io;
