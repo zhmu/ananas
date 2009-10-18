b1c929f4262b014f79bda64a19aa647d7dd0bf4d
Rink Springer <rink@rink.nu>
2009-10-18 13:14:18 +0200

    Make the scheduler multiple-CPU aware!
    If a CPU can't schedule, it will panic; idle threads are yet to be implemented.

--- kernel/kern/scheduler.c
+++ kernel/kern/scheduler.c
@@ -1,32 +1,53 @@
 #include "i386/thread.h"
+#include "pcpu.h"
 #include "lib.h"
+#include "lock.h"
+#include "pcpu.h"
 #include "thread.h"
 
 extern struct THREAD* threads;
 
-struct THREAD* curthread = NULL; /* XXX make me per-CPU */
+struct SPINLOCK spl_scheduler = { 0 };
 
 void
 schedule()
 {
-	struct THREAD* newthread = curthread;
+	struct THREAD* curthread;
+	struct THREAD* newthread;
 
+	spinlock_lock(&spl_scheduler);
+	curthread = PCPU_GET(curthread);
+
+	newthread = curthread;
 	if (newthread == NULL)
 		newthread = threads;
-	else {
+
+	while (newthread->flags & THREAD_FLAG_ACTIVE) {
 		newthread = newthread->next;
+		if (newthread == curthread)
+			break;
 		if (newthread == NULL)
 			newthread = threads;
+		if (newthread == curthread)
+			break;
 	}
 
-	if (newthread == NULL)
-		panic("no threads!");
+	if (curthread != NULL)
+		curthread->flags &= ~THREAD_FLAG_ACTIVE;
+	if (newthread != NULL)
+		newthread->flags |= THREAD_FLAG_ACTIVE;
 
-#if 0
+	PCPU_SET(curthread, newthread);
+	spinlock_unlock(&spl_scheduler);
+
+	if (newthread == NULL)
+		panic("schedule: CPU %u has no threads", PCPU_GET(cpuid));
+
 	if (newthread != curthread)
-		kprintf("schedule: switching to thread %x\n", newthread);
-#endif
-	curthread = newthread;
-	md_thread_switch(curthread);
+		kprintf("schedule: CPU %u: switching %x to %x\n", PCPU_GET(cpuid), curthread, newthread);
+
+	md_thread_switch(newthread, curthread);
 	/* NOTREACHED */
 }
+
+/* vim:set ts=2 sw=2: */
