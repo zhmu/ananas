3124a003c434571af965f58df2885e382604e572
Rink Springer <rink@rink.nu>
2009-10-25 17:57:25 +0100

    Use the built-in variable arguments provided by the compiler.
    There's no point in rolling them ourselves, and the built-in variables will also work if things aren't passed via the stack.

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,34 +1,15 @@
+#include "machine/types.h"
+
 #ifndef __VARARGS_H__
 #define __VARARGS_H__
 
 /*
- * Implements variable arguments; the idea behind this code is based on 
- * FreeBSD's implementation of va_list.
+ * Just chain to whatever the compiler uses.
  */
+typedef __builtin_va_list va_list;
+#define va_start __builtin_va_start
+#define va_arg __builtin_va_arg
+#define va_end __builtin_va_end
 
-typedef char* va_list;
-
-/*
- * Returns the length of 'a', rounded up to the next int. An int is
- * used because an int generally represents register size, which is
- * how the arguments are passed on the stack...
- */
-#define va_length(a) \
-	(sizeof(a) + (sizeof(a) % sizeof(int) == 0 ? 0 : sizeof(int) - (sizeof(a) % sizeof(int))))
-
-/* XXX this depends on GCC */
-#define va_start(ap, last) \
-	((ap) = (va_list)__builtin_next_arg(last))
-
-/*
- * Returns the next argument, cast to type*. This works by first updating the
- * ap pointer to the next item, and subsequently updating subtracing this
- * address so that the current item (as of the call) is returned.
- */
-#define va_arg(ap, type) \
-	(*(type *)((ap) += va_length(type), (ap) - va_length(type)))
-
-/* Included for completeness, not actually used */
-#define va_end(ap)
 
 #endif /* __VARARGS_H__ */
