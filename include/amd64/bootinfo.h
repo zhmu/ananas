484d41f44eff3a846a8e3d20e44ed9ca9bfbdbdb
Rink Springer <rink@rink.nu>
2009-10-29 21:32:02 +0100

    amd64: Add the missing 'kernel end' bootinfo field.

--- include/amd64/bootinfo.h
+++ include/amd64/bootinfo.h
@@ -2,10 +2,10 @@
 #define __AMD64_BOOTINFO_H__
 
 struct BOOTINFO {
-	uint32_t	bi_ident;
+	uint32_t	bi_ident;		/* Magic number */
 #define BI_IDENT	0xBADF00D2
-	uint32_t	bi_e820_addr;
-
+	uint32_t	bi_e820_addr;		/* Pointer to E820 memory map */
+	uint64_t	bi_kern_end;		/* Kernel end address */
 };
 
 #endif /* __AMD64_BOOTINFO_H__ */
