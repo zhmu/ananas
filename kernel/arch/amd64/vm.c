a9f9f4db7fecd0cfdc3e3883dd27b4c472eb7930
Rink Springer <rink@rink.nu>
2009-11-26 23:17:04 +0100

    amd64: Honor the user-mode mapping flag, and restructure the code a bit.
    This makes the vm_mapto_pagedir() and vm_map_pagedir() functions available.

--- kernel/arch/amd64/vm.c
+++ kernel/arch/amd64/vm.c
@@ -19,30 +19,36 @@ vm_get_nextpage()
 	return NULL;
 }
 
-static void
+void
 vm_mapto_pagedir(uint64_t* pml4, addr_t virt, addr_t phys, size_t num_pages, uint32_t user)
 {
 	/* XXX we don't yet strip off bits 52-63 yet */
 	while(num_pages--) {
 		if (pml4[(virt >> 39) & 0x1ff] == 0) {
-			pml4[(virt >> 39) & 0x1ff] = (uint64_t)((addr_t)vm_get_nextpage() & MEMMASK) | PE_RW | PE_P;
+			pml4[(virt >> 39) & 0x1ff] = (uint64_t)((addr_t)vm_get_nextpage() & MEMMASK) | (user ? PE_US : 0) | PE_RW | PE_P;
 		}
 		uint64_t* pdpe = (uint64_t*)(((addr_t)pml4[(virt >> 39) & 0x1ff] & ~0xfff) | KERNBASE);
 
 		if (pdpe[(virt >> 30) & 0x1ff] == 0) {
-			pdpe[(virt >> 30) & 0x1ff] = (uint64_t)((addr_t)vm_get_nextpage() & MEMMASK) | PE_RW | PE_P;
+			pdpe[(virt >> 30) & 0x1ff] = (uint64_t)((addr_t)vm_get_nextpage() & MEMMASK) | (user ? PE_US : 0) | PE_RW | PE_P;
 		}
 		uint64_t* pde = (uint64_t*)(((addr_t)pdpe[(virt >> 30) & 0x1ff] & ~0xfff) | KERNBASE);
 
 		if (pde[(virt >> 21) & 0x1ff] == 0) {
-			pde[(virt >> 21) & 0x1ff] = (uint64_t)((addr_t)vm_get_nextpage() & MEMMASK) | PE_RW | PE_P;
+			pde[(virt >> 21) & 0x1ff] = (uint64_t)((addr_t)vm_get_nextpage() & MEMMASK) | (user ? PE_US : 0) | PE_RW | PE_P;
 		}
 		uint64_t* pte = (uint64_t*)(((addr_t)pde[(virt >> 21) & 0x1ff] & ~0xfff) | KERNBASE);
 
-		pte[(virt >> 12) & 0x1ff] = (uint64_t)phys | PE_RW | PE_P;
+		pte[(virt >> 12) & 0x1ff] = (uint64_t)phys | (user ? PE_US : 0) | PE_RW | PE_P;
 
 		virt += PAGE_SIZE; phys += PAGE_SIZE;
 	}
+}
+
+void
+vm_map_pagedir(uint64_t* pml4, addr_t addr, size_t num_pages, uint32_t user)
+{
+	vm_mapto_pagedir(pml4, addr, addr, num_pages, user);
 }
 
 void
