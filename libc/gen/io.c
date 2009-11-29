61d7913d0a54e09094f253a26a3e5a1763fd2331
Rink Springer <rink@rink.nu>
2009-11-29 17:56:32 +0100

    Remove the debugging syscall and write to stdout now.

--- libc/gen/io.c
+++ libc/gen/io.c
@@ -1,15 +1,9 @@
 #include "lib.h"
 
-register_t syscall1(register_t no, register_t arg1);
-
 void
 putchar(int c)
 {
-	/*
-	 * XXX this syscall won't exist forever, but we need something to
-	 * interact... let's prefix with 0xfffe to state this for now XXX
-	 */
-	syscall1(0xfffe0001, c);
+	write(1 /* STDOUT_FILENO */, &c, 1);
 }
 
 /* vim:set ts=2 sw=2: */
