850ed9d6eb77d8675ecd6551671f9f5c44c3ec02
Rink Springer <rink@rink.nu>
2009-11-29 22:03:06 +0100

    Move to an input_dev and input_dev in the console, and place them in the kernel configurations too

--- kernel/kern/console.c
+++ kernel/kern/console.c
@@ -3,33 +3,47 @@
 #include "lib.h"
 #include "console.inc"
 
-device_t console_dev = NULL;
+device_t input_dev = NULL;
+device_t output_dev = NULL;
 extern const char* config_hints[];
 
 void
 console_init()
 {
+	char tmphint[32 /* XXX */];
+
 	/*
-	 * The console is magic; it's about the first thing we initialize (which
-	 * makes sense, as we want to have a means to show the user what's going on
-	 * as early as possible).
+	 * The console is magic; it consists of two parts: input/output, and it's
+	 * about the first thing we initialize (which makes sense, as we want to have
+	 * a means to show the user what's going on as early as possible).
 	 */
-#ifdef CONSOLE_DRIVER
-	extern struct DRIVER CONSOLE_DRIVER;
-	console_dev = device_alloc(NULL, &CONSOLE_DRIVER);
+#ifdef CONSOLE_INPUT_DRIVER
+	extern struct DRIVER CONSOLE_INPUT_DRIVER;
+	input_dev = device_alloc(NULL, &CONSOLE_INPUT_DRIVER);
 	/*
 	 * Cheat: we have no way to figure out what the eventual bus is the
 	 * console driver will be attached to, so we allow this to be hardcoded
 	 * in the configuration as well.
 	 */
-	char tmphint[32 /* XXX */];
 #ifdef CONSOLE_BUS
-	sprintf(tmphint, "%s.%s.%u.", CONSOLE_BUS, console_dev->name, console_dev->unit);
+	sprintf(tmphint, "%s.%s.%u.", CONSOLE_INPUT_BUS, input_dev->name, input_dev->unit);
 #else
-	sprintf(tmphint, "%s.%u.", console_dev->name, console_dev->unit);
+	sprintf(tmphint, "%s.%u.", input_dev->name, input_dev->unit);
 #endif
-	device_get_resources_byhint(console_dev, tmphint, config_hints);
-	device_attach_single(console_dev);
+	device_get_resources_byhint(input_dev, tmphint, config_hints);
+	device_attach_single(input_dev);
+#endif /* CONSOLE_INPUT_DRIVER */
+
+#ifdef CONSOLE_OUTPUT_DRIVER
+	extern struct DRIVER CONSOLE_OUTPUT_DRIVER;
+	output_dev = device_alloc(NULL, &CONSOLE_OUTPUT_DRIVER);
+#ifdef CONSOLE_BUS
+	sprintf(tmphint, "%s.%s.%u.", CONSOLE_OUTPUT_BUS, output_dev->name, output_dev->unit);
+#else
+	sprintf(tmphint, "%s.%u.", output_dev->name, output_dev->unit);
+#endif
+	device_get_resources_byhint(output_dev, tmphint, config_hints);
+	device_attach_single(output_dev);
 #endif /* CONSOLE_DRIVER */
 }
 
@@ -42,10 +56,21 @@ console_putchar(int c)
 	return;
 #endif
 
-	if (console_dev == NULL)
+	if (output_dev == NULL)
 		return;
 
-	console_dev->driver->drv_write(console_dev, (const char*)&c, 1);
+	output_dev->driver->drv_write(output_dev, (const char*)&c, 1);
+}
+
+uint8_t
+console_getchar()
+{
+	if (input_dev == NULL)
+		return 0;
+	uint8_t c;
+	if (input_dev->driver->drv_read(input_dev, (const char*)&c, 1) < 1)
+		return 0;
+	return c;
 }
 
 /* vim:set ts=2 sw=2: */
