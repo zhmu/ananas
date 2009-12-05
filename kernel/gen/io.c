f41b46a2ef4062cdd066c34fd55ecc500e1fa15a
Rink Springer <rink@rink.nu>
2009-12-05 14:56:31 +0100

    Implement I/O calls for read/write.
    Only descriptor 1 is supported and this always reads/writes to the console.

--- kernel/gen/io.c
+++ kernel/gen/io.c
@@ -1,18 +1,33 @@
 #include "types.h"
+#include "device.h"
 #include "lib.h"
+#include "pcpu.h"
+
+extern device_t input_dev;
+extern device_t output_dev;
 
 ssize_t
 sys_read(int fd, void* buf, size_t count)
 {
-	panic("sys_read");
-	return -1;
+	void* x = md_map_thread_memory(PCPU_GET(curthread), buf, count, 1);
+	if (x == NULL)
+		return -1;
+	if (fd != 1)
+		return 0;
+
+	return input_dev->driver->drv_read(input_dev, x, count); 
 }
 
 ssize_t
 sys_write(int fd, const void* buf, size_t count)
 {
-	kprintf("sys_write: fd=%u, buf=%p=, count=%u\n", fd, buf, count);
-	return -1;
+	void* x = md_map_thread_memory(PCPU_GET(curthread), buf, count, 0);
+	if (x == NULL)
+		return -1;
+	if (fd != 1)
+		return 0;
+
+	return output_dev->driver->drv_write(output_dev, x, count); 
 }
 
 /* vim:set ts=2 sw=2: */
