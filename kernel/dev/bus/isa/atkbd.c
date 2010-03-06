2aa394580247e11704f38ae6cd6a017e5b259607
Rink Springer <rink@rink.nu>
2010-03-06 14:36:04 +0100

    Add support for the SHIFT key.
    While here, fix up the keyboard map so that it's correct.

--- kernel/dev/bus/isa/atkbd.c
+++ kernel/dev/bus/isa/atkbd.c
@@ -1,3 +1,4 @@
+#include "machine/io.h"
 #include "device.h"
 #include "irq.h"
 #include "lib.h"
@@ -7,12 +8,32 @@ uint32_t atkbd_port = 0;
 /* Mapping of a scancode to an ASCII key value */
 static uint8_t atkbd_keymap[128] = {
 	/* 00-07 */    0, 0x1b, '1',  '2',  '3', '4', '5', '6',
-	/* 08-0f */  '7',  '8', '9',  '0',  '-', '+',   8,   9,
+	/* 08-0f */  '7',  '8', '9',  '0',  '-', '=',   8,   9,
 	/* 10-17 */  'q',  'w', 'e',  'r',  't', 'y', 'u', 'i',
 	/* 18-1f */  'o',  'p', '[',  ']', 0x0d,   0, 'a', 's', 
 	/* 20-27 */  'd',  'f', 'g',  'h',  'j', 'k', 'l', ';',
-	/* 28-2f */  ':',  '`',   0, 0x27,  'z', 'x', 'c', 'v',
-	/* 30-37 */  'b',  'n', 'm',  ',',  '.', '/', '?', '*',
+	/* 28-2f */  '\'',  '`',   0, '\\', 'z', 'x', 'c', 'v',
+	/* 30-37 */  'b',  'n', 'm',  ',',  '.', '/',   0, '*',
+	/* 38-3f */    0,  ' ',   0,    0,    0,   0,   0,   0,
+	/* 40-47 */    0,    0,   0,    0,    0,   0,   0, '7',
+	/* 48-4f */  '8',  '9', '-',  '4',  '5', '6', '+', '1',
+	/* 50-57 */  '2',  '3', '0',  '.',    0,   0, 'Z',   0,
+	/* 57-5f */    0,    0,   0,    0,    0,   0,   0,   0,
+	/* 60-67 */    0,    0,   0,    0,    0,   0,   0,   0,
+	/* 68-6f */    0,    0,   0,    0,    0,   0,   0,   0,
+	/* 70-76 */    0,    0,   0,    0,    0,   0,   0,   0,
+	/* 77-7f */    0,    0,   0,    0,    0,   0,   0,   0
+};
+
+/* Mapping of a scancode to an ASCII key value, if shift is active */
+static uint8_t atkbd_keymap_shift[128] = {
+	/* 00-07 */    0, 0x1b, '!',  '@',  '#', '$', '%', '^',
+	/* 08-0f */  '&',  '*', '(',  ')',  '_', '+',   8,   9,
+	/* 10-17 */  'Q',  'W', 'E',  'R',  'T', 'Y', 'U', 'I',
+	/* 18-1f */  'O',  'P', '{',  '}', 0x0d,   0, 'A', 'S', 
+	/* 20-27 */  'D',  'F', 'G',  'H',  'J', 'K', 'L', ':',
+	/* 28-2f */  '"',  '~',   0,  '|',  'Z', 'X', 'C', 'V',
+	/* 30-37 */  'B',  'N', 'M',  '<',  '>', '?',   0, '*',
 	/* 38-3f */    0,  ' ',   0,    0,    0,   0,   0,   0,
 	/* 40-47 */    0,    0,   0,    0,    0,   0,   0, '7',
 	/* 48-4f */  '8',  '9', '-',  '4',  '5', '6', '+', '1',
@@ -25,19 +46,29 @@ static uint8_t atkbd_keymap[128] = {
 };
 
 #define ATKBD_BUFFER_SIZE	16
+#define ATKBD_FLAG_SHIFT 1
 
 uint8_t atkbd_buffer[ATKBD_BUFFER_SIZE];
 uint8_t atkbd_buffer_readpos = 0;
 uint8_t atkbd_buffer_writepos = 0;
+uint8_t atkbd_flags = 0;
 
 void
 atkbd_irq(device_t dev)
 {
 	uint8_t scancode = inb(atkbd_port);
+	if ((scancode & 0x7f) == 0x2a /* LSHIFT */ ||
+	    (scancode & 0x7f) == 0x36 /* RSHIFT */) {
+		if (scancode & 0x80)
+			atkbd_flags &= ~ATKBD_FLAG_SHIFT;
+		else
+			atkbd_flags |=  ATKBD_FLAG_SHIFT;
+		return;
+	}
 	if (scancode & 0x80)
 		return;
 
-	uint8_t ascii = atkbd_keymap[scancode];
+	uint8_t ascii = ((atkbd_flags & ATKBD_FLAG_SHIFT) ? atkbd_keymap_shift : atkbd_keymap)[scancode];
 	if (ascii == 0)
 		return;
 
