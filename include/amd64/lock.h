6b29161b4bb766f0f89646bab1af528fc8983120
Rink Springer <rink@rink.nu>
2010-03-08 20:56:55 +0100

    amd64: Introduce spinlocks.
    This is based on the i386 code.

--- include/amd64/lock.h
+++ include/amd64/lock.h
@@ -3,15 +3,19 @@
 #ifndef __AMD64_LOCK_H__
 #define __AMD64_LOCK_H__
 
-/* TODO */
 typedef struct SPINLOCK* spinlock_t;
 
 struct SPINLOCK {
 	uint64_t	var;
 };
 
-#define spinlock_lock(x)
-#define spinlock_unlock(x) 
-#define spinlock_init(x)
+void md_spinlock_lock(spinlock_t l);
+void md_spinlock_unlock(spinlock_t l);
+void md_spinlock_init(spinlock_t l);
+
+#define spinlock_lock(x) md_spinlock_lock(x)
+#define spinlock_unlock(x) md_spinlock_unlock(x)
+#define spinlock_init(x) md_spinlock_init(x)
+
 
 #endif /* __AMD64_LOCK_H__ */
---
+++ kernel/arch/amd64/md_lock.c
@@ -0,0 +1,40 @@
+#include "types.h"
+#include "lock.h"
+
+/*
+ * The spinlock implementation is based on IA-32 Intel Architecture Software
+ * Developer's Manual, Volume 3: System programming guide, section 
+ * 7.6.9.1: "Use the pause instruction in spin-wait loops".
+ */
+void
+md_spinlock_lock(spinlock_t s)
+{
+	__asm(
+"l1:\n"
+		"cmpl $0, (%%rax)\n"
+		"je		l2\n"
+		"pause\n"
+		"jmp	l1\n"
+"l2:\n"
+		"movq	$1, %%rbx\n"
+		"xchg	(%%rax), %%rbx\n"
+		"cmpq	$0, %%rbx\n"
+		"jne	l1\n"
+	: : "a" (&s->var) : "rbx");
+}
+
+void
+md_spinlock_unlock(spinlock_t s)
+{
+	__asm(
+		"movl	$0, (%%rax)\n"
+	: : "a" (&s->var));
+}
+
+void
+md_spinlock_init(spinlock_t s)
+{
+	s->var = 0;
+}
+
+/* vim:set ts=2 sw=2: */
