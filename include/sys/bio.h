6e54f53d35e7031d263bd8b9db0bb4aff353a184
Rink Springer <rink@rink.nu>
2010-03-23 19:56:33 +0100

    Introduce a block I/O layer.
    This needs some serious work, but the fundaments (=API) are there and are functional.

---
+++ include/sys/bio.h
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/device.h>
+#include <machine/param.h>
+
+#ifndef __BIO_H__
+#define __BIO_H__
+
+/* XXX */
+#define BIO_BUF_DATASIZE	PAGE_SIZE
+
+#define BIO_IS_INUSE(bio)	((bio)->flags & BIO_FLAG_INUSE)
+#define BIO_IS_DONE(bio)	((bio)->flags & BIO_FLAG_DONE)
+#define BIO_IS_READ(bio)	((bio)->flags & BIO_FLAG_READ)
+#define BIO_IS_WRITE(bio)	((bio)->flags & BIO_FLAG_WRITE)
+#define BIO_IS_ERROR(bio)	((bio)->flags & BIO_FLAG_ERROR)
+#define BIO_DATA(bio)		((bio)->data)
+
+/*
+ * A basic BIO buffer, the root of all I/O requests. 
+ */
+struct BIO {
+	uint32_t	flags;
+#define BIO_FLAG_INUSE	0x0001		/* I/O is in use */
+#define BIO_FLAG_DONE	0x0002		/* Request completed */
+#define BIO_FLAG_READ	0x0004		/* Must read data */
+#define BIO_FLAG_WRITE	0x0008		/* Must write data */
+#define BIO_FLAG_ERROR	0x8000		/* Request failed */
+	device_t	device;		/* Device I/O'ing from */
+	block_t		block;		/* Block number to I/O */
+	size_t		length;		/* Length in bytes */
+	uint8_t		data[BIO_BUF_DATASIZE];
+};
+
+void bio_init();
+int bio_waitcomplete(struct BIO* bio);
+struct BIO* bio_read(device_t dev, block_t block, size_t len);
+struct BIO* bio_get_next(device_t dev);
+void bio_free(struct BIO* bio);
+
+#endif /* __BIO_H__ */
---
+++ kernel/kern/bio.c
@@ -0,0 +1,100 @@
+#include <sys/mm.h>
+#include <sys/bio.h>
+#include <sys/lib.h>
+
+#define SECTOR_SIZE	 512	/* XXX */
+#define BIO_NUM_BUFS 128
+
+static struct BIO* bio_buffer;
+
+void
+bio_init()
+{
+	bio_buffer = kmalloc(BIO_NUM_BUFS * sizeof(struct BIO));
+	memset(bio_buffer, 0, BIO_NUM_BUFS * sizeof(struct BIO));
+	kprintf("bio: allocated %u KB for buffers\n", (BIO_NUM_BUFS * sizeof(struct BIO)) / 1024);
+}
+
+/*
+ * Allocate a new bio buffer. This must always return a valid bio and wait for
+ * one if needed.
+ */
+struct BIO*
+bio_new()
+{
+	/*
+	 * XXX yes, we should implement a freelist some day. oh, and locking. yeah...
+	 */
+	for (unsigned int i = 0; i < BIO_NUM_BUFS; i++) {
+		if (bio_buffer[i].flags & BIO_FLAG_INUSE)
+			continue;
+		/* XXX race */
+
+		/*
+		 * Throw away any flags the buffer has (as this is a new request,
+		 * we can't anything more sensible yet)
+		 */
+		bio_buffer[i].flags = BIO_FLAG_INUSE;
+		return &bio_buffer[i];
+	}
+
+	/* XXX we should wait until a bio is available */
+	panic("bio_new(): out of bio's!");
+}
+
+void
+bio_free(struct BIO* bio)
+{
+	KASSERT(bio->flags & BIO_FLAG_INUSE, "bio is not in use");
+	bio->flags &= ~BIO_FLAG_INUSE;
+}
+
+/*
+ * Grabs the next bio to handle for a given device, or NULL if there is none.
+ * XXX we should use per-device lists etc.
+ */
+struct BIO*
+bio_get_next(device_t dev)
+{
+	/* XXX locking */
+	for (unsigned int i = 0; i < BIO_NUM_BUFS; i++) {
+		struct BIO* bio = &bio_buffer[i];
+		if ((bio->flags & BIO_FLAG_INUSE) && !(bio->flags & BIO_FLAG_DONE) && bio->device == dev)
+			return bio;
+	}
+	return NULL;
+}
+
+struct BIO*
+bio_alloc(device_t dev, block_t block, size_t len)
+{
+	KASSERT(len <= BIO_BUF_DATASIZE, "size out of range");
+
+	struct BIO* bio = bio_new();
+	bio->device = dev;
+	bio->block = block;
+	bio->length = len;
+	return bio;
+}
+
+int
+bio_waitcomplete(struct BIO* bio)
+{	
+	KASSERT(bio->flags & BIO_FLAG_INUSE, "bio is not in use");
+
+	while(!(bio->flags & BIO_FLAG_DONE)) /* nothing */;
+}
+
+struct BIO*
+bio_read(device_t dev, block_t block, size_t len)
+{
+	struct BIO* bio = bio_alloc(dev, block, len);
+	bio->flags |= BIO_FLAG_READ;
+
+	/* kick the device; we want it to read */
+	device_t biodev = dev->biodev;
+	biodev->driver->drv_start(bio->device);
+	return bio;
+}
+
+/* vim:set ts=2 sw=2: */
