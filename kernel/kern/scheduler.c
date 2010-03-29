976d965df7014f4bfadafdd13ba72bf2414eebbb
Rink Springer <rink@rink.nu>
2010-03-29 20:17:28 +0200

    Rewrite the scheduler so it correctly handles suspended threads.
    This is a simplied implementation as we can rely on at least one thread being present (the idle thread) all the time.

--- kernel/kern/scheduler.c
+++ kernel/kern/scheduler.c
@@ -16,15 +16,22 @@ schedule()
 	struct THREAD* newthread;
 
 	spinlock_lock(&spl_scheduler);
-	curthread = PCPU_GET(curthread);
+	newthread = NULL;
 
-	newthread = curthread;
-	if (newthread == NULL) {
-		newthread = threads;
-		if (newthread == NULL)
-			goto no_thread;
+	curthread = PCPU_GET(curthread);
+	if (curthread != NULL) {
+		/* We have a current thread; remove the active flag */
+		curthread->flags &= ~THREAD_FLAG_ACTIVE;
+
+		/* Start with the next thread, if any */
+		newthread = curthread->next;
 	}
 
+	/* If we cannot pick a next thread, use the first */
+	if (newthread == NULL)
+		 newthread = threads;
+
+	/* Scan for the next thread to run */
 	while (newthread->flags & (THREAD_FLAG_ACTIVE | THREAD_FLAG_SUSPENDED)) {
 		newthread = newthread->next;
 		if (newthread == curthread)
@@ -35,20 +42,27 @@ schedule()
 			break;
 	}
 
-	if (curthread != NULL)
-		curthread->flags &= ~THREAD_FLAG_ACTIVE;
-	if (newthread != NULL)
+	/*
+	 * OK, scanning was done. If newthread is identical to curthread
+	 * and we cannot schedule curthread, it means no new thread can be
+	 * found and as such, we must activate the idle thread.
+	 */
+	if (newthread != curthread) {
+		/* This new thread will do */
 		newthread->flags |= THREAD_FLAG_ACTIVE;
 
-	PCPU_SET(curthread, newthread);
-	spinlock_unlock(&spl_scheduler);
-
-	if (newthread == NULL) {
-no_thread:
-		/* No (usuable) thread; pick the idle thread */
+		/*
+		 * Safety checks. Note that we must skip this check in the idlethread case,
+		 * because it will always be marked as suspended. This prevents the scheduler
+		 * from picking it up, yet we can see if in the process list.
+		 */
+		KASSERT((newthread->flags & THREAD_FLAG_SUSPENDED) == 0, "schedule: activating non-idle thread %p", newthread);
+	} else {
 		newthread = PCPU_GET(idlethread_ptr);
 	}
 
+	PCPU_SET(curthread, newthread);
+	spinlock_unlock(&spl_scheduler);
 	if (newthread != curthread && 0)
 		kprintf("schedule: CPU %u: switching %x to %x\n", PCPU_GET(cpuid), curthread, newthread);
 
