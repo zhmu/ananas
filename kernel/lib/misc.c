c26f347505ce6947473282f3698821627ad7f177
Rink Springer <rink@rink.nu>
2010-05-05 11:35:58 +0200

    Deactivate scheduler on panics.

--- kernel/lib/misc.c
+++ kernel/lib/misc.c
@@ -1,10 +1,14 @@
 #include <sys/lib.h>
+#include <sys/schedule.h>
 
 void
 panic(const char* fmt, ...)
 {
 	va_list ap;
 	char hack[128 /* XXXX */];
+
+	/* disable the scheduler - this ensures any extra BSP's will not run threads either */
+	scheduler_deactivate();
 
 	strcpy(hack, "panic: ");
 	strcat(hack, fmt);
