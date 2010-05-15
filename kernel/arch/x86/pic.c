577f07c8a26d031e6ff7cbff06f280dbfc9515fc
Rink Springer <rink@rink.nu>
2010-05-15 22:06:37 +0200

    x86: Enable all interrupts instead of restoring the previous mask.
    Bochs has a very liberal interrupt mask once it reaches us, but QEMU (and real hardware) generally do not. Do not take any chances, and just enable everything.

--- kernel/arch/x86/pic.c
+++ kernel/arch/x86/pic.c
@@ -5,8 +5,6 @@ void
 x86_pic_remap() {
 #define IO_WAIT() do { int i = 10; while (i--) /* NOTHING */ ; } while (0);
 
-	unsigned int mask1 = inb(PIC1_DATA);
-	unsigned int mask2 = inb(PIC2_DATA);
 	/* Start initialization: the PIC will wait for 3 command bta ytes */
 	outb(PIC1_CMD, ICW1_INIT | ICW1_ICW4); IO_WAIT();
 	outb(PIC2_CMD, ICW1_INIT | ICW1_ICW4); IO_WAIT();
@@ -19,9 +17,9 @@ x86_pic_remap() {
 	/* Data byte 3 contains environment flags */
 	outb(PIC1_DATA, ICW4_8086); IO_WAIT();
 	outb(PIC2_DATA, ICW4_8086); IO_WAIT();
-	/* Restore PIC masks */
-	outb(PIC1_DATA, mask1);
-	outb(PIC2_DATA, mask2);
+	/* Enable all interrupts */
+	outb(PIC1_DATA, 0);
+	outb(PIC2_DATA, 0);
 
 #undef IO_WAIT
 }
