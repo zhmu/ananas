c9d5566660d27d805a611b8e6962b731c18867ec
Rink Springer <rink@rink.nu>
2010-05-24 19:30:16 +0200

    Add loader include files as well.

---
+++ include/loader/diskio.h
@@ -0,0 +1,9 @@
+#ifndef __DISKIO_H__
+#define __DISKIO_H__
+
+#define MAX_DISKS 8
+#define SECTOR_SIZE 512
+
+int diskio_init();
+
+#endif /* __DISKIO_H__ */
---
+++ include/loader/platform.h
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
+/* Retrieve a block of length bytes available for use - must be zeroed out */
+void* platform_get_memory(uint32_t length);
+
+/* Display a single charachter */
+void platform_putch(uint8_t ch);
+
+/* Get a keystroke from the input provider, blocks if nothing is available */
+int platform_getch();
+
+/* Checks whether a keystroke is available */
+int platform_check_key();
+
+/* Retrieve the platform's memory map for passing to the kernel. Returns KB's of memory available */
+uint64_t platform_init_memory_map();
+
+/* Initialize disk subsystem. Returns number of disks available */
+int platform_init_disks();
+
+/* Initialze disk slices. Returns number of slices total */
+int platform_init_slices();
+
+/* Reads a block from a disk to buffer. Returns numbers of bytes read */
+int platform_read_disk(int disk, uint32_t lba, void* buffer, int num_bytes);
+
+#endif /* __PLATFORM_H__ */
