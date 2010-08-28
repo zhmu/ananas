45eb9ebbfe078b1c56066ed7e873713c41867c7c
Rink Springer <rink@rink.nu>
2010-08-28 19:25:24 +0200

    powerpc: Establish a proper OFW context prior to doing calls and restore as needed.
    This still has a problem (sprg's must not be restored) but that will have to wait for another time to debug.

--- kernel/arch/powerpc/startup.c
+++ kernel/arch/powerpc/startup.c
@@ -16,30 +16,39 @@ struct PCPU bsp_pcpu;
 struct STACKFRAME bsp_sf;
 static ofw_entry_t ofw_entry;
 
+static uint32_t orig_msr;
+static uint32_t orig_sprg0;
+static uint32_t orig_sr[PPC_NUM_SREGS];
+
 int
 ofw_call(void* arg)
 {
-	uint32_t orig_msr;
-	uint32_t orig_sprg[4];
 	int retval;
 
 	/* Save our MSR and restore the OFW one */
 	orig_msr = rdmsr();
 	wrmsr(ofw_msr);
 
-	/* Save our SPRG's and restore the OFW ones */
-	__asm __volatile("mfsprg0 %0; mtsprg0 %1" : "=r" (orig_sprg[0]) : "r" (ofw_sprg[0]));
-	__asm __volatile("mfsprg1 %0; mtsprg1 %1" : "=r" (orig_sprg[1]) : "r" (ofw_sprg[1]));
-	__asm __volatile("mfsprg2 %0; mtsprg2 %1" : "=r" (orig_sprg[2]) : "r" (ofw_sprg[2]));
-	__asm __volatile("mfsprg3 %0; mtsprg3 %1" : "=r" (orig_sprg[3]) : "r" (ofw_sprg[3]));
+	/* Save our SPRG's - we only care about %sprg0 for now */
+	__asm __volatile("mfsprg0 %0" : "=r" (orig_sprg0));
+
+	/* FIXME The following causes OFW to hang... need to investigate why */
+#ifdef NOTYET
+	/* ...and restore the OFW ones */
+	__asm __volatile(
+		"mtsprg0 %0\n"
+		"mtsprg1 %1\n"
+		"mtsprg2 %2\n"
+		"mtsprg3 %3\n"
+	: : "r" (ofw_sprg[0]), "r" (ofw_sprg[1]),
+	    "r" (ofw_sprg[2]), "r" (ofw_sprg[3]));
+#endif
+	__asm __volatile("isync");
 
 	retval = ofw_entry(arg);
 
-	/* Restore our SPRG's */
-	__asm("mtsprg0 %0" : : "r" (orig_sprg[0]));
-	__asm("mtsprg1 %0" : : "r" (orig_sprg[1]));
-	__asm("mtsprg2 %0" : : "r" (orig_sprg[2]));
-	__asm("mtsprg3 %0" : : "r" (orig_sprg[3]));
+	/* Restore our SPRG */
+	__asm __volatile("mtsprg0 %0" : : "r" (orig_sprg0));
 
 	/* And restore our MSR */
 	wrmsr(orig_msr);
@@ -55,10 +64,10 @@ md_startup(uint32_t r3, uint32_t r4, uint32_t r5)
 	 * tumble if we do not restore them.
 	 */
 	ofw_msr = rdmsr();
-	__asm("mfsprg0 %0" : "=r" (ofw_sprg[0]));
-	__asm("mfsprg1 %0" : "=r" (ofw_sprg[1]));
-	__asm("mfsprg2 %0" : "=r" (ofw_sprg[2]));
-	__asm("mfsprg3 %0" : "=r" (ofw_sprg[3]));
+	__asm __volatile("mfsprg0 %0" : "=r" (ofw_sprg[0]));
+	__asm __volatile("mfsprg1 %0" : "=r" (ofw_sprg[1]));
+	__asm __volatile("mfsprg2 %0" : "=r" (ofw_sprg[2]));
+	__asm __volatile("mfsprg3 %0" : "=r" (ofw_sprg[3]));
 	ofw_entry = (ofw_entry_t)r5;
 
 	/* Initial OpenFirmware, this will make kprintf() work */
