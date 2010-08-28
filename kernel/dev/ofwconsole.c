e379edc0695aec6010a5a4cc175333d6e3b0ccf9
Rink Springer <rink@rink.nu>
2010-08-28 19:17:00 +0200

    Introduce a basic OpenFirmware-based console driver.

---
+++ kernel/dev/ofwconsole.c
@@ -0,0 +1,41 @@
+#include <sys/device.h>
+#include <sys/irq.h>
+#include <sys/lib.h>
+#include <sys/mm.h>
+
+static int
+ofw_attach(device_t dev)
+{
+	return 0;
+}
+
+static ssize_t
+ofw_write(device_t dev, const void* data, size_t len, off_t offset)
+{
+	uint8_t* ptr = (uint8_t*)data;
+	const char* ch = (const char*)data;
+	size_t amount = 0;
+
+	for (amount = 0; amount < len; amount++, ch++)
+		ofw_putch(*ch);
+	return amount;
+}
+
+static ssize_t
+ofw_read(device_t dev, void* data, size_t len, off_t offset)
+{
+	return 0;
+}
+
+struct DRIVER drv_ofwconsole = {
+	.name					= "ofwconsole",
+	.drv_probe		= NULL,
+	.drv_attach		= ofw_attach,
+	.drv_write		= ofw_write,
+	.drv_read     = ofw_read
+};
+
+DRIVER_PROBE(ofwconsole)
+DRIVER_PROBE_END()
+
+/* vim:set ts=2 sw=2: */
