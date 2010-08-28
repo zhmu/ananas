7cfba49fce49cc61b03aacbd6c336c940d8fb088
Rink Springer <rink@rink.nu>
2010-08-28 19:29:20 +0200

    powerpc: Implement proper thread support.
    This makes it possible to have user threads - the necessary modifications to the more low-level code will follow in the next commit.

--- kernel/arch/powerpc/md_thread.c
+++ kernel/arch/powerpc/md_thread.c
@@ -8,8 +8,14 @@
 #include <sys/pcpu.h>
 #include <sys/vm.h>
 
-extern struct TSS kernel_tss;
-void md_idle_thread();
+void
+md_idle_thread()
+{
+	/* XXX this needs some work */
+	while(1);
+}
+
+#define THREAD_TEMP_SPACE 0xd0008000
 
 int
 md_thread_init(thread_t t)
@@ -22,44 +28,64 @@ md_thread_init(thread_t t)
 	for (int i = 0; i < PPC_NUM_SREGS; i++)
 		t->md_ctx.sf.sf_sr[i] = INVALID_VSID;
 
+	/* Establish a kernel mapping */
+	mmu_map_kernel(&t->md_ctx.sf);
+
 	/* Allocate a stack for the thread */
+	uint32_t stack_addr = 0x8000000;
 	t->md_stack  = kmalloc(THREAD_STACK_SIZE);
-	md_thread_map(t, t->md_stack, t->md_stack, THREAD_STACK_SIZE, 0);
-	t->md_ctx.sf.sf_reg[1] = (addr_t)t->md_stack + THREAD_STACK_SIZE;
+	memset(t->md_stack, 0, THREAD_STACK_SIZE);
+	thread_mapto(t, stack_addr, t->md_stack, THREAD_STACK_SIZE, 0);
+	t->md_ctx.sf.sf_reg[1] = stack_addr + THREAD_STACK_SIZE;
 
 	/*
-	 * XXX Figure out why this is necessary; why do our threads try to access
-	 * things outside of the stack frame ?
+	 * Initially, the E500 ABI specifies that the stack frame
+	 * must be 16 byte aligned, and that it must point to a single
+	 * zero word.
 	 */
-	t->md_ctx.sf.sf_reg[1] -= 64;
+	t->md_ctx.sf.sf_reg[1] -= 16;
 
 	/* Establish initial context */
 	t->md_ctx.sf.sf_srr1 = MSR_DR | MSR_IR | MSR_RI | MSR_PR;
 	/* XXX Until FPU support is properly implemented */
 	t->md_ctx.sf.sf_srr1 |= MSR_FP;
 
-	t->next_mapping = 1048576;
+	/* XXX We should use a more sensible address here */
+	t->next_mapping = 0x4000000;
 	return 1;
 }
 
 void
 md_thread_destroy(thread_t t)
 {
-	panic("md_thread_destroy(): not implemented");
+	/* XXX we should unmap everything the thread has mapped here */
+	kfree(t->md_stack);
 }
 
 void*
 md_map_thread_memory(thread_t thread, void* ptr, size_t length, int write)
 {
-	panic("md_map_thread_memory(): not implemented");
-	return NULL;
+	KASSERT(length <= PAGE_SIZE, "cannot map more than a page yet"); /* XXX */
+//	kprintf("md_map_thread_memory(): t=%x, ptr=%p, length=%u\n", thread, ptr, length);
+
+	addr_t addr = thread_find_mapping(thread, ptr);
+	if (addr == 0)
+		return NULL;
+	uint32_t delta = (addr_t)ptr & (PAGE_SIZE - 1);
+
+	/* yes this is cruel */
+	vm_unmap(THREAD_TEMP_SPACE, 1);
+
+	vm_mapto(THREAD_TEMP_SPACE, (addr_t)(addr - delta), 1);
+	__asm __volatile("isync"); /* FIXME why is this needed? */
+	return (void*)(THREAD_TEMP_SPACE + delta);
 }
 
 void*
 md_thread_map(thread_t thread, void* to, void* from, size_t length, int flags)
 {
-	KASSERT((((addr_t)to % PAGE_SIZE) == 0), "to address not page-aligned");
-	KASSERT((((addr_t)from % PAGE_SIZE) == 0), "from address not page-aligned");
+	KASSERT((((addr_t)to % PAGE_SIZE) == 0), "to address %p not page-aligned", to);
+	KASSERT((((addr_t)from % PAGE_SIZE) == 0), "from address %p not page-aligned", from);
 
 	int num_pages = length / PAGE_SIZE;
 	if ((length % PAGE_SIZE) > 0)
@@ -84,12 +110,13 @@ md_thread_unmap(thread_t thread, void* addr, size_t length)
 void
 md_thread_setidle(thread_t thread)
 {
+	thread->md_ctx.sf.sf_srr0 = (addr_t)&md_idle_thread;
 }
 
 void
 md_thread_set_entrypoint(thread_t thread, addr_t entry)
 {
-	thread->md_ctx.sf.sf_nia = entry;
+	thread->md_ctx.sf.sf_srr0 = entry;
 }
 
 /* vim:set ts=2 sw=2: */
