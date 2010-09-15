2f29ec3d5649253edcc87d276f2dbb89cd0475b3
Rink Springer <rink@rink.nu>
2010-09-15 23:05:33 +0200

    libc: Fix prototype so libc builds again...

--- lib/libc/posix/ftruncate.c
+++ lib/libc/posix/ftruncate.c
@@ -5,7 +5,7 @@
 #include <syscalls.h>
 #include <_posix/fdmap.h>
 
-off_t ftruncate( int fd, off_t length )
+int ftruncate( int fd, off_t length )
 {
 	void* handle = fdmap_deref(fd);
 	if (handle == NULL)
