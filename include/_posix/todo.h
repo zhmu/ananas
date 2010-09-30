ba11f117008281023856f84a03a8a135c6b59a2e
Rink Springer <rink@rink.nu>
2010-09-30 22:22:17 +0200

    libc: Disable TODO's by default, they only clutter up the screen.

--- include/_posix/todo.h
+++ include/_posix/todo.h
@@ -4,10 +4,19 @@
 #include <stdio.h>	/* for printf() */
 #include <errno.h>	/* for errno :-) */
 
-#define TODO 								\
+#undef TODO_VERBOSE
+
+#ifdef TODO_VERBOSE
+# define TODO 								\
 	do {								\
 		printf("%s: not implemented yet\n", __func__); 		\
 		errno = ENOSYS;						\
 	} while (0)
+#else
+# define TODO 								\
+	do {								\
+		errno = ENOSYS;						\
+	} while (0)
+#endif
 
 #endif /* _POSIX_TODO_H_ */
