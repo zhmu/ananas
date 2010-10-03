f4018f61c80a3d7437756d58913484f03f8225fb
Rink Springer <rink@rink.nu>
2010-10-03 16:22:53 +0200

    loader/x86: commit missed files.

--- include/loader/x86.h
+++ include/loader/x86.h
@@ -3,6 +3,9 @@
 
 #include <ananas/types.h>
 
+struct LOADER_ELF_INFO;
+struct BOOTINFO;
+
 struct REALMODE_REGS {
 	uint32_t	eax;
 	uint32_t	ebx;
@@ -38,5 +41,7 @@ extern void* rm_stack;
 void x86_realmode_init(struct REALMODE_REGS* regs);
 void x86_realmode_push16(struct REALMODE_REGS* regs, uint16_t value);
 void x86_realmode_call(struct REALMODE_REGS* regs);
+void x86_64_exec(struct LOADER_ELF_INFO* loadinfo, struct BOOTINFO* bootinfo);
+void x86_64_launch(uint64_t elf_start_addr, struct BOOTINFO* bootinfo);
 
 #endif /* __LOADER_X86_H__ */
