84f328b42e7314796df2a53946c1838a6b07dbb2
Rink Springer <rink@rink.nu>
2010-11-09 16:32:38 +0100

    Wii: Add USBGecko debugging port support
    This was essential in getting the port to work; many thanks to Tim Brugman for lending his USBGecko to me.

--- kernel/arch/powerpc/debug-console.c
+++ kernel/arch/powerpc/debug-console.c
@@ -1,16 +1,28 @@
 #include <ananas/types.h>
 #include <ananas/debug-console.h>
+#include <ananas/console.h>
 #include <ofw.h>
+#include "options.h"
+
+void usbgecko_putch(char ch);
 
 void
 debugcon_init()
 {
+#ifdef WII
+	usbgecko_init();
+#endif
 }
 
 void
 debugcon_putch(int ch)
 {
+#ifdef OFW
 	ofw_putch(ch);
+#endif
+#ifdef WII
+	usbgecko_putch(ch);
+#endif
 }
 
 int
