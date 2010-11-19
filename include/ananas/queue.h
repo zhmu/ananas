a1a5cddc7732efcb692b3886263a7703fefda736
Rink Springer <rink@rink.nu>
2010-11-19 12:58:30 +0100

    Clean up the singly-linked queuing structure.
    Kernel code changes will follow.

--- include/ananas/queue.h
+++ include/ananas/queue.h
@@ -6,59 +6,46 @@
  * O(1), adding a new item takes O(1). Iterating through the entire queue takes
  * O(n) (this also holds for locating a single item).
  *
- * Each queue has a 'head' and 'tail' element. It should be used like this:
- *
- * struct test_item {
- *   ...
- *   QUEUE_FIELDS;
- * };
- * QUEUE_DEFINE(test_queue);
- *
- * void main() {
- *   struct test_queue t;
- *   QUEUE_INIT(&t);
- *
- *   struct test_item* ti;
-
- *   QUEUE_ADD_TAIL(&t, ti, struct test_queue);
- *   QUEUE_ADD_TAIL(&t, ti, struct test_queue);
- *   while (!QUEUE_EMPTY(&t)) {
- *     struct test_item* headitem = QUEUE_HEAD(&t, struct test_queue);
- *     QUEUE_POP_HEAD(&t, struct test_queue);
- *     ...
- *   }
- * }
+ * Usage instructions can be found in the 'queue' structure test.
  */
 
-#define QUEUE_FIELDS						\
-	void* qi_next;
+#define QUEUE_FIELDS(TYPE)					\
+	TYPE* qi_next;
 
-#define QUEUE_DEFINE(name)					\
+#define QUEUE_DEFINE(name, TYPE)				\
 	struct name {						\
-		void* head;					\
-		void* tail;					\
+		TYPE* head;					\
+		TYPE* tail;					\
 	};
 
 #define QUEUE_INIT(q) 						\
 	(q)->head = NULL;					\
 	(q)->tail = NULL;					\
 
-#define QUEUE_ADD_TAIL(q, item, TYPE)				\
+#define QUEUE_ADD_TAIL(q, item)					\
 	(item)->qi_next = NULL;					\
 	if ((q)->head == NULL) {				\
 		(q)->head = (item);				\
 	} else {						\
-		((TYPE*)(q)->tail)->qi_next = (item);		\
+		(q)->tail->qi_next = (item);			\
 	}							\
 	(q)->tail = (item);
 
-#define QUEUE_HEAD(q, TYPE) 					\
-	(TYPE*)((q)->head)
+#define QUEUE_HEAD(q) 						\
+	((q)->head)
 
-#define QUEUE_POP_HEAD(q, TYPE)					\
-	(q)->head = ((TYPE*)(q)->head)->qi_next;
+#define QUEUE_POP_HEAD(q)					\
+	(q)->head = (q)->head->qi_next;
 
 #define QUEUE_EMPTY(q)						\
 	((q)->head == NULL)
+
+#define QUEUE_NEXT(it)						\
+	(it)->qi_next
+
+#define QUEUE_FOREACH(q, it, TYPE)				\
+	for (TYPE* (it) = QUEUE_HEAD(q);			\
+	     (it) != NULL;					\
+	     (it) = QUEUE_NEXT(it))
 
 #endif /* __ANANAS_QUEUE_H__ */
