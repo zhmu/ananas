7d939bb0d154d07973dc45a59f1e9d0fecf6876c
Rink Springer <rink@rink.nu>
2011-01-30 12:11:54 +0100

    Fix minimum values of signed {8,16} bit integers and ensure no evil macro stuff occurs by adding parentheses

--- include/ananas/powerpc/_stdint.h
+++ include/ananas/powerpc/_stdint.h
@@ -2,20 +2,20 @@
 #define __POWERPC_STDINT_H__
 
 /* 7.18.2.1: Limits of exact-width integer types */
-#define INT8_MIN		-127
-#define INT16_MIN		-32767
+#define INT8_MIN		(-128)
+#define INT16_MIN		(-32768)
 #define INT32_MIN		-2147483648
 #define INT64_MIN		-9223372036854775808LL
 
-#define INT8_MAX		127
-#define INT16_MAX		32767
-#define INT32_MAX		2147483647
-#define INT64_MAX		9223372036854775807LL
+#define INT8_MAX		(127)
+#define INT16_MAX		(32767)
+#define INT32_MAX		(2147483647)
+#define INT64_MAX		(9223372036854775807LL)
 
-#define UINT8_MAX		255
-#define UINT16_MAX		65535
-#define UINT32_MAX		4294967295U
-#define UINT64_MAX		18446744073709551615ULL
+#define UINT8_MAX		(255)
+#define UINT16_MAX		(65535)
+#define UINT32_MAX		(4294967295U)
+#define UINT64_MAX		(18446744073709551615ULL)
 
 /* 7.18.2.2: Limits of minimum-width integer types */
 #define INT_LEAST8_MIN		INT8_MIN
