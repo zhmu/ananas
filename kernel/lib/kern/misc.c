488336e0967b4e51a0a80e4697ad4bcfa253c181
Rink Springer <rink@rink.nu>
2011-02-27 23:01:12 +0100

    kdb: Enter the debugger after a panic() if KDB is enabled, at least if a thread was running.
    This isn't perfect, but it'll get the job done for now.

--- kernel/lib/kern/misc.c
+++ kernel/lib/kern/misc.c
@@ -1,22 +1,43 @@
 #include <ananas/lib.h>
+#include <ananas/pcpu.h>
 #include <ananas/schedule.h>
+#include <ananas/kdb.h>
+#include "options.h"
 
 void
 _panic(const char* file, const char* func, int line, const char* fmt, ...)
 {
-#if defined(__i386__) || defined(__amd64__)
-	/* XXX This is a horrible hack */
-	__asm ("cli");
-#endif
 	va_list ap;
+#ifndef KDB
 	/* disable the scheduler - this ensures any extra BSP's will not run threads either */
 	scheduler_deactivate();
+#endif
 
 	kprintf("panic in %s:%u (%s): ", file, line, func);
 
 	va_start(ap, fmt);
 	vaprintf(fmt, ap);
 	va_end(ap);
+	kprintf("\n");
 
+#ifdef KDB
+	/*
+	 * For the KDB case, disable the current thread and enter the debugger; note
+	 * that this cannot be done if there is no current thread (i.e. in early
+	 * bootup) so we'll just halt there for the time being XXX
+	 */
+	struct THREAD* curthread = PCPU_GET(curthread);
+	if (curthread != NULL) {
+		thread_suspend(curthread);
+		kdb_enter("panic");
+		reschedule();
+	} else {
+		scheduler_deactivate();
+		while(1);
+	}
+#else
 	while(1);
+#endif
 }
+
+/* vim:set ts=2 sw=2: */
