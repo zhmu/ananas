cc1c8c97dfb7c86e1df7dd6944891ce340cc9616
Rink Springer <rink@rink.nu>
2011-03-12 16:47:21 +0100

    Ugh, add the vfs_generic_write() function to the corresponding include file as well

--- include/ananas/vfs/generic.h
+++ include/ananas/vfs/generic.h
@@ -3,5 +3,6 @@
 
 errorcode_t vfs_generic_lookup(struct VFS_INODE* dirinode, struct VFS_INODE** destinode, const char* dentry);
 errorcode_t vfs_generic_read(struct VFS_FILE* file, void* buf, size_t* len);
+errorcode_t vfs_generic_write(struct VFS_FILE* file, void* buf, size_t* len);
 
 #endif /* __ANANAS_VFS_GENERIC_H__ */
