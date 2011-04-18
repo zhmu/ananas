4b78c18560ce861fd43fe53d49e5a9f8a51b44e2
Rink Springer <rink@rink.nu>
2011-04-18 21:55:14 +0200

    vga: Restructure the VGA driver and make it possible to use a dedicated update thread
    The thread is disabled for now as it doesn't interact nicely with the debugger; I'll still need to think about the correct solution here.

--- kernel/dev/vga.c
+++ kernel/dev/vga.c
@@ -1,14 +1,20 @@
 #include <ananas/types.h>
 #include <ananas/error.h>
 #include <machine/vm.h>
+#include <machine/param.h>
 #include <ananas/console.h>
 #include <ananas/x86/io.h>
 #include <ananas/device.h>
 #include <ananas/lib.h>
 #include <ananas/trace.h>
+#include <ananas/thread.h>
+#include <ananas/waitqueue.h>
 #include <ananas/mm.h>
 
 #include <teken.h>
+
+/* If defined, we use a kernel thread to periodically update the screen */
+#undef VGA_KERNEL_THREAD
 
 /* Console height */
 #define VGA_HEIGHT 25
@@ -18,10 +24,27 @@
 
 TRACE_SETUP;
 
-static uint32_t vga_io = 0;
-static uint8_t* vga_mem = NULL;
-static uint8_t vga_attr = 0xf;
-static teken_t vga_teken;
+struct pixel {
+	teken_char_t	c;
+	teken_attr_t	a;
+};
+
+struct VGA_PRIVDATA {
+	uint32_t vga_io;
+	uint16_t* vga_video_mem;
+	struct pixel* vga_buffer;
+	uint8_t  vga_attr;
+	int      vga_dirty;
+	teken_t  vga_teken;
+#ifdef VGA_KERNEL_THREAD
+	int      vga_cursor_x;
+	int      vga_cursor_y;
+	struct WAIT_QUEUE vga_wq;
+	struct THREAD vga_workerthread;
+#endif
+};
+
+#define VGA_PIXEL(s,x,y) ((s)->vga_buffer[(y) * VGA_WIDTH + (x)])
 
 /* XXX this doesn't really belong here */
 static tf_bell_t		term_bell;
@@ -32,7 +55,7 @@ static tf_copy_t		term_copy;
 static tf_param_t		term_param;
 static tf_respond_t	term_respond;
 
-static void vga_crtc_write(device_t dev, uint8_t reg, uint8_t val);
+static void vga_crtc_write(struct VGA_PRIVDATA* p, uint8_t reg, uint8_t val);
 
 static teken_funcs_t tf = {
 	.tf_bell		= term_bell,
@@ -44,27 +67,41 @@ static teken_funcs_t tf = {
 	.tf_respond	= term_respond
 };
 
-struct pixel {
-	teken_char_t	c;
-	teken_attr_t	a;
-};
-
-static struct pixel buffer[VGA_WIDTH][VGA_HEIGHT];
-
-static void
-printchar(const teken_pos_t* p)
+inline static void
+vga_printchar(struct VGA_PRIVDATA* priv, int x, int y, struct pixel* px)
 {
-	KASSERT(p->tp_row < VGA_HEIGHT, "row not in range");
-	KASSERT(p->tp_col < VGA_WIDTH, "col not in range");
-	struct pixel* px = &buffer[p->tp_col][p->tp_row];
-
-	if (vga_mem == NULL)
-		return;
-
-	addr_t offs = (p->tp_row * VGA_WIDTH + p->tp_col) * 2;
-	*(vga_mem + offs    ) = px->c;
-	*(vga_mem + offs + 1) = teken_256to8(px->a.ta_fgcolor) + 8 * teken_256to8(px->a.ta_bgcolor);
+	/* XXX little endian */
+	*(uint16_t*)(priv->vga_video_mem + y * VGA_WIDTH + x) =
+	 (uint16_t)(teken_256to8(px->a.ta_fgcolor) + 8 * teken_256to8(px->a.ta_bgcolor)) << 8 |
+	 px->c;
 }
+
+inline static void
+vga_place_cursor(struct VGA_PRIVDATA* priv, int x, int y)
+{
+	uint32_t offs = (x + y * VGA_WIDTH);
+	vga_crtc_write(priv, 0xe, offs >> 8);
+	vga_crtc_write(priv, 0xf, offs & 0xff);
+}
+
+#ifdef VGA_KERNEL_THREAD
+static void
+vga_syncthread(void* ptr)
+{
+	struct VGA_PRIVDATA* priv = ptr;
+	while (1) {
+		waitqueue_wait(&priv->vga_wq);
+
+		struct pixel* px = priv->vga_buffer;
+		for (unsigned int y = 0; y < VGA_HEIGHT; y++)
+			for (unsigned int x = 0; x < VGA_WIDTH; x++, px++)
+				vga_printchar(priv, x, y, px);
+
+		/* reposition the cursor */
+		vga_place_cursor(priv, priv->vga_cursor_x, priv->vga_cursor_y);
+	}
+}
+#endif
 
 static void
 term_bell(void* s)
@@ -75,18 +112,27 @@ term_bell(void* s)
 static void
 term_cursor(void* s, const teken_pos_t* p)
 {
-	/* reposition the cursor */
-	int offs = (p->tp_col + p->tp_row * VGA_WIDTH);
-	vga_crtc_write(s, 0xe, offs >> 8);
-	vga_crtc_write(s, 0xf, offs & 0xff);
+	struct VGA_PRIVDATA* priv = s;
+#ifdef VGA_KERNEL_THREAD
+	priv->vga_cursor_x = p->tp_col;
+	priv->vga_cursor_y = p->tp_row;
+	waitqueue_signal(&priv->vga_wq);
+#else
+	vga_place_cursor(priv, p->tp_col, p->tp_row);
+#endif
 }
 	
 static void
 term_putchar(void *s, const teken_pos_t* p, teken_char_t c, const teken_attr_t* a)
 {
-	buffer[p->tp_col][p->tp_row].c = c;
-	buffer[p->tp_col][p->tp_row].a = *a;
-	printchar(p);
+	struct VGA_PRIVDATA* priv = s;
+	VGA_PIXEL(priv, p->tp_col, p->tp_row).c = c;
+	VGA_PIXEL(priv, p->tp_col, p->tp_row).a = *a;
+#ifdef VGA_KERNEL_THREAD
+	waitqueue_signal(&priv->vga_wq);
+#else
+	vga_printchar(priv, p->tp_col, p->tp_row, &VGA_PIXEL(priv, p->tp_col, p->tp_row));
+#endif
 }
 
 static void
@@ -103,68 +149,80 @@ term_fill(void* s, const teken_rect_t* r, teken_char_t c, const teken_attr_t* a)
 static void
 term_copy(void* s, const teken_rect_t* r, const teken_pos_t* p)
 {
-       int nrow, ncol, x, y; /* has to be signed - >= 0 comparison */
-        teken_pos_t d;
+	struct VGA_PRIVDATA* priv = s;
+	int nrow, ncol, x, y; /* has to be signed - >= 0 comparison */
+	teken_pos_t d;
 
-        /*
-         * copying is a little tricky. we must make sure we do it in
-         * correct order, to make sure we don't overwrite our own data.
-         */
+#ifdef VGA_KERNEL_THREAD
+	#define VGA_PLACE_PIXEL \
+		(void)0
+#else
+	#define VGA_PLACE_PIXEL \
+		vga_printchar(priv, d.tp_col, d.tp_row, &VGA_PIXEL(priv, d.tp_col, d.tp_row))
+#endif
 
-        nrow = r->tr_end.tp_row - r->tr_begin.tp_row;
-        ncol = r->tr_end.tp_col - r->tr_begin.tp_col;
+	/*
+	* copying is a little tricky. we must make sure we do it in
+	* correct order, to make sure we don't overwrite our own data.
+	*/
 
-        if (p->tp_row < r->tr_begin.tp_row) {
-                /* copy from top to bottom. */
-                if (p->tp_col < r->tr_begin.tp_col) {
-                        /* copy from left to right. */
-                        for (y = 0; y < nrow; y++) {
-                                d.tp_row = p->tp_row + y;
-                                for (x = 0; x < ncol; x++) {
-                                        d.tp_col = p->tp_col + x;
-                                        buffer[d.tp_col][d.tp_row] =
-                                            buffer[r->tr_begin.tp_col + x][r->tr_begin.tp_row + y];
-                                        printchar(&d);
-                                }
-                        }
-                } else {
-                        /* copy from right to left. */
-                        for (y = 0; y < nrow; y++) {
-                                d.tp_row = p->tp_row + y;
-                                for (x = ncol - 1; x >= 0; x--) {
-                                        d.tp_col = p->tp_col + x;
-                                        buffer[d.tp_col][d.tp_row] =
-                                            buffer[r->tr_begin.tp_col + x][r->tr_begin.tp_row + y];
-                                        printchar(&d);
-                                }
-                        }
-                }
-        } else {
-                /* copy from bottom to top. */
-                if (p->tp_col < r->tr_begin.tp_col) {
-                        /* copy from left to right. */
-                        for (y = nrow - 1; y >= 0; y--) {
-                                d.tp_row = p->tp_row + y;
-                                for (x = 0; x < ncol; x++) {
-                                        d.tp_col = p->tp_col + x;
-                                        buffer[d.tp_col][d.tp_row] =
-                                            buffer[r->tr_begin.tp_col + x][r->tr_begin.tp_row + y];
-                                        printchar(&d);
-                                }
-                        }
-                } else {
-                        /* copy from right to left. */
-                        for (y = nrow - 1; y >= 0; y--) {
-                                d.tp_row = p->tp_row + y;
-                                for (x = ncol - 1; x >= 0; x--) {
-                                        d.tp_col = p->tp_col + x;
-                                        buffer[d.tp_col][d.tp_row] =
-                                            buffer[r->tr_begin.tp_col + x][r->tr_begin.tp_row + y];
-                                        printchar(&d);
-                                }
-                        }
-                }
-        }
+	nrow = r->tr_end.tp_row - r->tr_begin.tp_row;
+	ncol = r->tr_end.tp_col - r->tr_begin.tp_col;
+
+	if (p->tp_row < r->tr_begin.tp_row) {
+		/* copy from top to bottom. */
+		if (p->tp_col < r->tr_begin.tp_col) {
+			/* copy from left to right. */
+			for (y = 0; y < nrow; y++) {
+				d.tp_row = p->tp_row + y;
+				for (x = 0; x < ncol; x++) {
+					d.tp_col = p->tp_col + x;
+					VGA_PIXEL(priv, d.tp_col, d.tp_row) =
+					 VGA_PIXEL(priv, r->tr_begin.tp_col + x, r->tr_begin.tp_row + y);
+					VGA_PLACE_PIXEL;
+				}
+			}
+		} else {
+			/* copy from right to left. */
+			for (y = 0; y < nrow; y++) {
+				d.tp_row = p->tp_row + y;
+				for (x = ncol - 1; x >= 0; x--) {
+					d.tp_col = p->tp_col + x;
+					VGA_PIXEL(priv, d.tp_col, d.tp_row) =
+					 VGA_PIXEL(priv, r->tr_begin.tp_col + x, r->tr_begin.tp_row + y);
+					VGA_PLACE_PIXEL;
+				}
+			}
+		}
+	} else {
+		/* copy from bottom to top. */
+		if (p->tp_col < r->tr_begin.tp_col) {
+			/* copy from left to right. */
+			for (y = nrow - 1; y >= 0; y--) {
+				d.tp_row = p->tp_row + y;
+				for (x = 0; x < ncol; x++) {
+					d.tp_col = p->tp_col + x;
+					VGA_PIXEL(priv, d.tp_col, d.tp_row) =
+					 VGA_PIXEL(priv, r->tr_begin.tp_col + x, r->tr_begin.tp_row + y);
+					VGA_PLACE_PIXEL;
+				}
+			}
+		} else {
+			/* copy from right to left. */
+			for (y = nrow - 1; y >= 0; y--) {
+				d.tp_row = p->tp_row + y;
+				for (x = ncol - 1; x >= 0; x--) {
+					d.tp_col = p->tp_col + x;
+					VGA_PIXEL(priv, d.tp_col, d.tp_row) =
+					 VGA_PIXEL(priv, r->tr_begin.tp_col + x, r->tr_begin.tp_row + y);
+					VGA_PLACE_PIXEL;
+				}
+			}
+		}
+	}
+#ifdef VGA_KERNEL_THREAD
+	waitqueue_signal(&priv->vga_wq);
+#endif
 }
 
 static void
@@ -180,50 +238,72 @@ term_respond(void* s, const void* buf, size_t len)
 }
 
 static void
-vga_crtc_write(device_t dev, uint8_t reg, uint8_t val)
+vga_crtc_write(struct VGA_PRIVDATA* p, uint8_t reg, uint8_t val)
 {
-	outb(vga_io + 0x14, reg);
-	outb(vga_io + 0x15, val);
+	outb(p->vga_io + 0x14, reg);
+	outb(p->vga_io + 0x15, val);
 }
 
 static errorcode_t
 vga_attach(device_t dev)
 {
-	vga_mem = device_alloc_resource(dev, RESTYPE_MEMORY, 0x7fff);
+	void* mem = device_alloc_resource(dev, RESTYPE_MEMORY, 0x7fff);
 	void* res = device_alloc_resource(dev, RESTYPE_IO, 0x1f);
-	if (vga_mem == NULL || res == NULL)
+	if (mem == NULL || res == NULL)
 		return ANANAS_ERROR(NO_RESOURCE);
-	vga_io = (uintptr_t)res;
 
-	/* Clear the display; we must set the attribute everywhere for the cursor */
-	uint8_t* ptr = vga_mem;
+	struct VGA_PRIVDATA* vga_privdata = kmalloc(sizeof(*vga_privdata));
+	memset(vga_privdata, 0, sizeof(*vga_privdata));
+	vga_privdata->vga_io        = (uintptr_t)res;
+	vga_privdata->vga_video_mem = mem;
+	vga_privdata->vga_buffer    = kmalloc(VGA_HEIGHT * VGA_WIDTH * sizeof(struct pixel));
+	vga_privdata->vga_attr      = 0xf;
+	dev->privdata = vga_privdata;
+
+	/* Clear the display; we must set the attribute everywhere for the cursor XXX little endian */
+	uint16_t* ptr = vga_privdata->vga_video_mem;
 	int size = VGA_HEIGHT * VGA_WIDTH;
 	while(size--) {
-		*ptr++ = ' '; *ptr++ = vga_attr;
+		*ptr++ = (vga_privdata->vga_attr << 8) | ' ';
 	}
 
 	/* set cursor shape */
-	vga_crtc_write(dev, 0xa, 14);
-	vga_crtc_write(dev, 0xb, 15);
+	vga_crtc_write(vga_privdata, 0xa, 14);
+	vga_crtc_write(vga_privdata, 0xb, 15);
 
 	/* initialize teken library */
 	teken_pos_t tp;
 	tp.tp_row = VGA_HEIGHT; tp.tp_col = VGA_WIDTH;
-	teken_init(&vga_teken, &tf, NULL);
-	teken_set_winsize(&vga_teken, &tp);
+	teken_init(&vga_privdata->vga_teken, &tf, vga_privdata);
+	teken_set_winsize(&vga_privdata->vga_teken, &tp);
+
+#ifdef VGA_KERNEL_THREAD
+	/*
+	 * Spawn a thread to synchronize our VGA buffer with the screen; this ensures
+	 * we will not busy wait attempting to write the video memory while we could
+	 * do a lot more useful things.
+	 */
+  thread_init(&vga_privdata->vga_workerthread, NULL);
+  thread_set_args(&vga_privdata->vga_workerthread, "[vga]\0\0", PAGE_SIZE);
+	md_thread_setkthread(&vga_privdata->vga_workerthread, vga_syncthread, vga_privdata);
+
+	waitqueue_init(&vga_privdata->vga_wq);
+	thread_resume(&vga_privdata->vga_workerthread);
+#endif
 	return ANANAS_ERROR_OK;
 }
 
 static errorcode_t
 vga_write(device_t dev, const void* data, size_t* len, off_t off)
 {
+	struct VGA_PRIVDATA* priv = dev->privdata;
 	const uint8_t* ptr = data;
 	size_t left = *len;
 	while(left--) {
 		/* XXX this is a hack which should be performed in a TTY layer, once we have one */
 		if(*ptr == '\n')
-			teken_input(&vga_teken, "\r", 1);
-		teken_input(&vga_teken, ptr, 1);
+			teken_input(&priv->vga_teken, "\r", 1);
+		teken_input(&priv->vga_teken, ptr, 1);
 		ptr++;
 	}
 	return ANANAS_ERROR_OK;
