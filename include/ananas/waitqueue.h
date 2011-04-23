d20762eeccdbae87043f35ec46362c4d670abdf8
Rink Springer <rink@rink.nu>
2011-04-23 16:55:09 +0200

    waitqueue: Restructure waitqueues to use an 'add', 'wait' and 'remove' paradigm.
    The reason is that waiting on a waitqueue must not construct a waiter; this is in fact quite undesirable because the event may have happened before the waitqueue_wait() function is called. This means the waiter is deadlocked because the event will already have happened and thus waiting should never have commenced.
    
    However, splitting results in the issue that waiting must do nothing if the event is signalled before waiting is used - but as interrupts and syscalls are preemptible, the only way to avoid the race conditions is to use non-premptible spinlocks.

--- include/ananas/waitqueue.h
+++ include/ananas/waitqueue.h
@@ -5,7 +5,7 @@
 #include <ananas/thread.h>
 #include <ananas/lock.h>
 #include <ananas/pcpu.h>
-#include <ananas/queue.h>
+#include <ananas/dqueue.h>
 
 /*
  * A waiter is a thread waiting on some event; these are implemented by having
@@ -13,24 +13,30 @@
  * designed to be light-weight.
  */
 struct WAITER {
+	struct SPINLOCK	w_lock;			/* spinlock protecting the waiter */
 	thread_t	w_thread;		/* waiting thread */
-	QUEUE_FIELDS(struct WAITER);
+	int		w_signalled;		/* number of times signalled */
+	struct WAIT_QUEUE* w_wq;		/* owner queue */
+	DQUEUE_FIELDS(struct WAITER);
 };
 
-QUEUE_DEFINE_BEGIN(WAIT_QUEUE, struct WAITER)
+DQUEUE_DEFINE_BEGIN(WAIT_QUEUE, struct WAITER)
 	struct SPINLOCK	wq_lock;
-QUEUE_DEFINE_END
+DQUEUE_DEFINE_END
 
 /* Initialize a given wait queue; if wq is NULL, initializes the freelist */
 void waitqueue_init(struct WAIT_QUEUE* wq);
 
-/* Adds the current thread to a waitqueue and blocks until it signals */
-void waitqueue_wait(struct WAIT_QUEUE* wq);
+/* Allocate a waiter slot for the current thread in the specified waitqueue */
+struct WAITER* waitqueue_add(struct WAIT_QUEUE* wq);
 
-/* Wakes the first waiter of wait queue wq */
+/* Removes the current thread's waiter slot */
+void waitqueue_remove(struct WAITER* w);
+
+/* Wait until the given waiter is released signals */
+void waitqueue_wait(struct WAITER* w);
+
+/* Wakes all waiters of the given waitqueue */
 void waitqueue_signal(struct WAIT_QUEUE* wq);
-
-/* Wakes all waiters of wait queue wq */
-void waitqueue_signal_all(struct WAIT_QUEUE* wq);
 
 #endif /* __WAITQUEUE_H__ */
--- kernel/kern/waitqueue.c
+++ kernel/kern/waitqueue.c
@@ -16,9 +16,9 @@ waitqueue_init(struct WAIT_QUEUE* wq)
 	if (wq == NULL) {
 		/* Create the freelist with WAITER_QUEUE_SIZE items */
 		struct WAITER* w_items = kmalloc(sizeof(struct WAIT_QUEUE) * WAITER_QUEUE_SIZE);
-		QUEUE_INIT(&w_freelist);
+		DQUEUE_INIT(&w_freelist);
 		for (int i = 0; i < WAITER_QUEUE_SIZE; i++) {
-			QUEUE_ADD_TAIL(&w_freelist, w_items);
+			DQUEUE_ADD_TAIL(&w_freelist, w_items);
 			w_items++;
 		}
 
@@ -26,76 +26,112 @@ waitqueue_init(struct WAIT_QUEUE* wq)
 		return;
 	}
 
-	QUEUE_INIT(wq);
+	DQUEUE_INIT(wq);
 	spinlock_init(&wq->wq_lock);
 }
 
-void
-waitqueue_wait(struct WAIT_QUEUE* wq)
+struct WAITER*
+waitqueue_add(struct WAIT_QUEUE* wq)
 {
-	thread_t t = PCPU_GET(curthread);
-	if (t == NULL) {
-		reschedule();
-		return;
-	}
-
 	/* Fetch a fresh new waiter */
 	spinlock_lock(&spl_freelist);
-	KASSERT(!QUEUE_EMPTY(&w_freelist), "out of waiters"); /* XXX deal with this */
-	struct WAITER* w = QUEUE_HEAD(&w_freelist);
-	QUEUE_POP_HEAD(&w_freelist);
+	KASSERT(!DQUEUE_EMPTY(&w_freelist), "out of waiters"); /* XXX deal with this */
+	struct WAITER* w = DQUEUE_HEAD(&w_freelist);
+	DQUEUE_POP_HEAD(&w_freelist);
 	spinlock_unlock(&spl_freelist);
+
+	/* Initialize the waiter */
+	w->w_thread = PCPU_GET(curthread);
+	w->w_wq = wq;
+	w->w_signalled = 0;
 
 	/* Append our waiter to the queue */
-	w->w_thread = t;
 	spinlock_lock(&wq->wq_lock);
-	QUEUE_ADD_TAIL(wq, w);
-	thread_suspend(t); /* must be done within the lock to ensure we're not signalled right before we wait */
+	DQUEUE_ADD_TAIL(wq, w);
 	spinlock_unlock(&wq->wq_lock);
 
-	/* Wait... we'll awake once someone calls waitqueue_signal */
-	reschedule();
-
-	/* Once this is reached, we've been woken up */
+	return w;
 }
 
-static int
-waitqueue_signal_first(struct WAIT_QUEUE* wq)
+void
+waitqueue_remove(struct WAITER* w)
 {
-	struct WAITER* w = NULL;
+	struct THREAD* t = PCPU_GET(curthread);
+	KASSERT(w->w_thread == t, "waiter does not belong to our thread");
 
-	/* Obtain the first item to signal */
-	spinlock_lock(&wq->wq_lock);
-	if (!QUEUE_EMPTY(wq)) {
-		w = QUEUE_HEAD(wq);
-		QUEUE_POP_HEAD(wq);
-	}
-	spinlock_unlock(&wq->wq_lock);
+	/* Remove our waiter from the waitqueue */
+	spinlock_lock(&w->w_wq->wq_lock);
+	KASSERT(!DQUEUE_EMPTY(w->w_wq), "queue cannot be empty");
+	DQUEUE_REMOVE(w->w_wq, w);
+	spinlock_unlock(&w->w_wq->wq_lock);
 
-	if (w == NULL)
-		return 0; /* nothing signalled */
-
-	/* Wake up the corresponding thread */
-	thread_resume(w->w_thread);
-
-	/* Add the waiter to the free list */
+	/* Hook it up to the freelist */
 	spinlock_lock(&spl_freelist);
-	QUEUE_ADD_TAIL(&w_freelist, w);
+	DQUEUE_ADD_TAIL(&w_freelist, w);
 	spinlock_unlock(&spl_freelist);
-	return 1; /* thread signalled */
+}
+
+void
+waitqueue_wait(struct WAITER* w)
+{
+	struct THREAD* t = PCPU_GET(curthread);
+	KASSERT(w->w_thread == t, "waiter does not belong to our thread");
+
+	if (t == NULL)
+		return;
+
+	/* Wait until we are adequately scheduled */
+	for (;;) {
+		/*
+		 * Note that we must be cautious here: once a thread has called
+		 * thread_suspend(), there's a possibility that the scheduler irq kicks in
+		 * and the thread will not continue.
+		 *
+		 * This is troublesome, as we need to *atomically* check whether w_signalled is
+		 * zero _and_ suspend the thread if it is. The invariant that needs to be
+	 	 * honored is that thread_suspend() is only called iff w_signalled = 0.
+		 *
+		 * This solves the possible race of:
+		 *
+		 * 1) <thread t> w_signalled is zero ...
+		 * 2) <irq> waitqueue_signal() gets called, increments
+		 *    w_signalled and resumes the thread.
+		 * 3) <thread t> w_signalled was zero, so we suspend.
+		 *
+		 * The end result is that <thread t> never wakes up; this problem is solved
+		 * by using a spinlock to ensure the signalled state doesn't change while
+	 	 * our thread is being suspended  - note that this spinlock must be
+		 * unpremptible because the scheduler irq can otherwise kick in while
+		 * we still need to unlock the spinlock, causing a deadlock.
+		 */
+		int state = spinlock_lock_unpremptible(&w->w_lock);
+		if (w->w_signalled > 0) {
+			spinlock_unlock_unpremptible(&w->w_lock, state);
+			break;
+		}
+		thread_suspend(t);
+		spinlock_unlock_unpremptible(&w->w_lock, state);
+		reschedule();
+	}
 }
 
 void
 waitqueue_signal(struct WAIT_QUEUE* wq)
 {
-	waitqueue_signal_first(wq);
-}
+	spinlock_lock(&wq->wq_lock);
+	KASSERT(!DQUEUE_EMPTY(wq), "signalling without waiters");
+	DQUEUE_FOREACH(wq, w, struct WAITER) {
+		spinlock_lock(&w->w_lock);
 
-void
-waitqueue_signal_all(struct WAIT_QUEUE* wq)
-{
-	while (waitqueue_signal_first(wq))
-		/* nothing; waitqueue_signal_first() does all the work */ ;
+		/* Signal the waiter */
+		w->w_signalled++;
+
+		/* Wake up the corresponding thread */
+		thread_resume(w->w_thread);
+
+		spinlock_unlock(&w->w_lock);
+	}
+	spinlock_unlock(&wq->wq_lock);
 }
 
 /* vim:set ts=2 sw=2: */
