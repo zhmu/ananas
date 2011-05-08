9c60d4881a338352f22e5ccdd7e7da1325e527b5
Rink Springer <rink@rink.nu>
2011-05-08 15:53:50 +0200

    include: Add maximum command length limit
    While here, clean up the ananas/limits.h file a bit.

--- include/ananas/limits.h
+++ include/ananas/limits.h
@@ -1,5 +1,5 @@
-#ifndef __SYS_LIMTS_H__
-#define __SYS_LIMTS_H__
+#ifndef __ANANAS_LIMITS_H__
+#define __ANANAS_LIMITS_H__
 
 /* Maximum number of bytes in an input queue */
 #define MAX_INPUT	256
@@ -7,4 +7,4 @@
 /* Maximum path size */
 #define MAX_PATH	256
 
-#endif /* __SYS_LIMTS_H__ */
+#endif /* __ANANAS_LIMITS_H__ */
