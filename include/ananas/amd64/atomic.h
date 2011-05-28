cb49c7b5f6c030d392352d69c91fc55da026ba08
Rink Springer <rink@rink.nu>
2011-05-28 12:00:34 +0200

    amd64: Add atomics

---
+++ include/ananas/amd64/atomic.h
@@ -0,0 +1,29 @@
+#ifndef __AMD64_ATOMIC_H__
+#define __AMD64_ATOMIC_H__
+
+/* The atomic is the building block of all our locking preemtives */
+typedef struct {
+	int value;
+} atomic_t;
+
+static inline void atomic_set(atomic_t* a, int v)
+{
+	*(volatile int*)&a->value = v;
+}
+
+static inline int atomic_xchg(atomic_t* a, int v)
+{
+	register int m = v;
+	__asm __volatile(
+		"xchg (%1), %0"
+	: "=r" (m) : "r" (&a->value));
+	return m;
+		
+}
+
+static inline int atomic_read(atomic_t* a)
+{
+	return *(volatile int*)&a->value;
+}
+
+#endif /* __AMD64_ATOMIC_H__ */
