d2b1dad7286db0dcdc7ffc120d590c185593484d
Rink Springer <rink@rink.nu>
2011-05-28 12:00:53 +0200

    amd64: Add MD interrupt functions

--- include/ananas/amd64/interrupts.h
+++ include/ananas/amd64/interrupts.h
@@ -3,4 +3,50 @@
 
 #define MAX_IRQS 16
 
+static inline void md_interrupts_enable()
+{
+	__asm __volatile("sti");
+}
+
+static inline void md_interrupts_disable()
+{
+	__asm __volatile("cli");
+}
+
+static inline void md_interrupts_restore(int enabled)
+{
+	__asm __volatile(
+		/* get eflags in %edx */
+		"pushfq\n"
+		"popq %%rdx\n"
+		/* mask interrupt flag and re-enable flag if needed */
+		"andq $~0x200, %%rdx\n"
+		"orl %0, %%edx\n"
+		/* activate new eflags */
+		"pushq %%rdx\n"
+		"popfq\n"
+	: : "a" (enabled) : "%rdx");
+}
+
+static inline int md_interrupts_save()
+{
+	register int r;
+	__asm __volatile(
+		/* get eflags */
+		"pushfq\n"
+		"popq %%rdx\n"
+		/* but only the interrupt flag */
+		"andq $0x200, %%rdx\n"
+		"movl %%edx, %0"
+	: "=r" (r) : : "%rdx");
+	return r;
+}
+
+static inline int md_interrupts_save_and_disable()
+{
+	int status = md_interrupts_save();
+	md_interrupts_disable();
+	return status;
+}
+
 #endif /* __AMD64_INTERRUPTS_H__ */
