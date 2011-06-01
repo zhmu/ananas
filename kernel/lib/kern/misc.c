00886212cb7163ed3693c613c0385d8b0b9dc4ea
Rink Springer <rink@rink.nu>
2011-06-01 21:49:17 +0200

    Attempt to detect double panics and just loop in this case.

--- kernel/lib/kern/misc.c
+++ kernel/lib/kern/misc.c
@@ -1,12 +1,29 @@
 #include <ananas/lib.h>
 #include <ananas/schedule.h>
 #include <ananas/kdb.h>
+#include <ananas/pcpu.h>
+#include <machine/atomic.h>
+#include <machine/pcpu.h>
+#include <machine/interrupts.h>
 #include "options.h"
+
+static atomic_t kdb_panicing;
 
 void
 _panic(const char* file, const char* func, int line, const char* fmt, ...)
 {
 	va_list ap;
+
+	/*
+	 * Do our best to detect double panics; this will at least give more of a
+	 * clue what's going on instead of making it worse.
+	 */
+	if (atomic_read(&kdb_panicing) == (int)&_panic) {
+		md_interrupts_disable();
+		kprintf("double panic: %s:%u (%s) - dying!\n", file, line, func);
+		for(;;);
+	}
+	atomic_set(&kdb_panicing, (int)&_panic);
 
 	/* disable the scheduler - this ensures any extra BSP's will not run threads either */
 	scheduler_deactivate();
@@ -20,9 +37,8 @@ _panic(const char* file, const char* func, int line, const char* fmt, ...)
 
 #ifdef KDB
 	kdb_panic();
-#else
-	while(1);
 #endif
+	for(;;);
 }
 
 /* vim:set ts=2 sw=2: */
