1833396fc8c569cb36017e29aee15f1b514bfb49
Rink Springer <rink@rink.nu>
2011-07-17 16:36:59 +0200

    x86: Show timestamp along with trace messages
    The implementation is a kludge for now, but hopefully this will force me to make it nicer

--- kernel/kern/trace.c
+++ kernel/kern/trace.c
@@ -7,9 +7,17 @@ uint32_t trace_subsystem_mask[TRACE_SUBSYSTEM_LAST];
 void
 tracef(int fileid, const char* func, const char* fmt, ...)
 {
+	uint32_t timestamp = 0;
+	#if defined(__i386__) || defined(__amd64__)
+#if 1
+		uint32_t x86_get_ms_since_boot();
+		timestamp = x86_get_ms_since_boot();
+#endif
+	#endif
+
 	/* XXX This function is a SMP-disaster for now */
   va_list ap;
-	kprintf("%s: ", func);
+	kprintf("[% 4u.%03u] %s: ", timestamp / 1000, timestamp % 1000, func);
   va_start(ap, fmt);
   vaprintf(fmt, ap);
   va_end(ap);
