da33fae9652bdd5d16ef94fc81fa75e16c2f2c69
Rink Springer <rink@rink.nu>
2011-07-17 18:29:26 +0200

    x86: Use inline versions of the I/O functions
    This saves quite a lot of function calls; note that the assembly constraints were wrong previously, but since these were functions, the values couldn't directly be output either way.

--- include/ananas/x86/io.h
+++ include/ananas/x86/io.h
@@ -3,12 +3,51 @@
 
 #include <sys/types.h>
 
-void outb(uint16_t port, uint8_t data);
-void outw(uint16_t port, uint16_t data);
-void outl(uint16_t port, uint32_t data);
-uint8_t inb(uint16_t port);
-uint16_t inw(uint16_t port);
-uint32_t inl(uint16_t port);
-uint64_t rdtsc();
+static inline void
+outb(uint16_t port, uint8_t data) {
+	__asm volatile ("outb %0, %w1" : : "a" (data), "d" (port));
+}
+
+static inline void
+outw(uint16_t port, uint16_t data) {
+	__asm volatile ("outw %0, %w1" : : "a" (data), "d" (port));
+}
+
+static inline void
+outl(uint16_t port, uint32_t data) {
+	__asm volatile ("outl %0, %w1" : : "a" (data), "d" (port));
+}
+
+static inline uint8_t
+inb(uint16_t port)
+{
+	register uint8_t a;
+	__asm volatile ("inb %w1, %0" : "=a" (a) : "d" (port));
+	return a;
+}
+
+static inline uint16_t
+inw(uint16_t port)
+{
+	register uint16_t a;
+	__asm volatile ("inw %w1, %0" : "=a" (a) : "d" (port));
+	return a;
+}
+
+static inline uint32_t
+inl(uint16_t port)
+{
+	register uint32_t a;
+	__asm volatile ("inl %w1, %0" : "=a" (a) : "d" (port));
+	return a;
+}
+
+static inline uint64_t
+rdtsc()
+{
+	register uint64_t v;
+	__asm __volatile("rdtsc" : "=A" (v));
+	return v;
+}
 
 #endif /* __X86_IO_H__ */
