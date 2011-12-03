95e98501404f1507be068b26cbb422386d90a1bc
Rink Springer <rink@rink.nu>
2011-12-03 17:36:14 +0100

    dqueue: Make it possible to override the variable name prefix in the prev/next pointer fields
    This is useful when multiple items are to be added to distinct queues; this is being done in the bio structure, which can now be rewritten to use the general dqueue macro's.

--- include/ananas/dqueue.h
+++ include/ananas/dqueue.h
@@ -8,14 +8,21 @@
  * a single item).
  *
  * Each queue has a 'head' and 'tail' element, and every item has a previous
- * and next pointer.
+ * and next pointer. Usage instructions can be found in the 'dqueue' structure
+ * test.
  *
- * Usage instructions can be found in the 'dqueue' structure test.
+ * The _IP-suffixed macro's allow a type-specific item prefix (ip) to be given;
+ * this is useful when an item is to be placed in multiple queues. This is rare,
+ * so the normal macro's simply expand to the item prefix macro's with the 'qi'
+ * prefix for 'queue item'.
  */
 
-#define DQUEUE_FIELDS(TYPE)					\
-	TYPE* qi_prev;						\
-	TYPE* qi_next;
+#define DQUEUE_FIELDS_IT(TYPE, ip)				\
+	TYPE* ip ## _prev;					\
+	TYPE* ip ## _next;
+
+#define DQUEUE_FIELDS(type)					\
+	DQUEUE_FIELDS_IT(type, qi)
 
 #define DQUEUE_DEFINE_BEGIN(name, TYPE)				\
 	struct name {						\
@@ -35,28 +42,28 @@ do {								\
 	(q)->dq_tail = NULL;					\
 } while(0)
 
-#define DQUEUE_ADD_TAIL(q, item)				\
+#define DQUEUE_ADD_TAIL_IP(q, ip, item)				\
 do {								\
-	(item)->qi_next = NULL;					\
+	(item)->ip ## _next = NULL;				\
 	if ((q)->dq_head == NULL) {				\
-		(item)->qi_prev = NULL;				\
+		(item)->ip ## _prev = NULL;			\
 		(q)->dq_head = (item);				\
 	} else {						\
-		(item)->qi_prev = (q)->dq_tail;			\
-		(q)->dq_tail->qi_next = (item);			\
+		(item)->ip ## _prev = (q)->dq_tail;		\
+		(q)->dq_tail->ip ## _next = (item);		\
 	}							\
 	(q)->dq_tail = (item);					\
 } while(0)
 
-#define DQUEUE_ADD_HEAD(q, item)				\
+#define DQUEUE_ADD_HEAD_IP(q, ip, item)				\
 do {								\
-	(item)->qi_prev = NULL;					\
+	(item)->ip ## _prev = NULL;				\
 	if ((q)->dq_head == NULL) {				\
-		(item)->qi_next = NULL;				\
+		(item)->ip ## _next = NULL;			\
 		(q)->dq_tail = (item);				\
 	} else {						\
-		(item)->qi_next = (q)->dq_head;			\
-		(q)->dq_head->qi_prev = (item);			\
+		(item)->ip ## _next = (q)->dq_head;		\
+		(q)->dq_head->ip ## _prev = (item);		\
 	}							\
 	(q)->dq_head = (item);					\
 } while(0)
@@ -67,63 +74,96 @@ do {								\
 #define DQUEUE_TAIL(q) 						\
 	((q)->dq_tail)
 
-#define DQUEUE_POP_HEAD(q)					\
+#define DQUEUE_POP_HEAD_IP(q, ip)				\
 do {								\
-	(q)->dq_head = (q)->dq_head->qi_next;			\
+	(q)->dq_head = (q)->dq_head->ip ## _next;		\
 	if ((q)->dq_head != NULL)				\
-		(q)->dq_head->qi_prev = NULL;			\
+		(q)->dq_head->ip ## _prev = NULL;		\
 } while(0)
 
-#define DQUEUE_POP_TAIL(q)					\
+#define DQUEUE_POP_TAIL_IP(q, ip)				\
 do {								\
-	(q)->dq_tail = (q)->dq_tail->qi_prev;			\
+	(q)->dq_tail = (q)->dq_tail->ip ## _prev;		\
 	if ((q)->dq_tail != NULL)				\
-		(q)->dq_tail->qi_next = NULL;			\
+		(q)->dq_tail->ip ## _next = NULL;		\
 } while(0)
 
 #define DQUEUE_EMPTY(q)						\
 	((q)->dq_head == NULL)
 
-#define DQUEUE_NEXT(it)						\
-	(it)->qi_next
+#define DQUEUE_NEXT_IP(it, ip)					\
+	(it)->ip ## _next
 
-#define DQUEUE_PREV(it)						\
-	(it)->qi_prev
+#define DQUEUE_PREV_IP(it, ip)					\
+	(it)->ip ## _prev
 
-#define DQUEUE_FOREACH(q, it, TYPE)				\
+#define DQUEUE_FOREACH_IP(q, ip, it, TYPE)			\
 	for (TYPE* (it) = DQUEUE_HEAD(q);			\
 	     (it) != NULL;					\
-	     (it) = DQUEUE_NEXT(it))
+	     (it) = DQUEUE_NEXT_IP(it, ip))
 
-#define DQUEUE_FOREACH_SAFE(q, it, TYPE)			\
+#define DQUEUE_FOREACH_SAFE_IP(q, ip, it, TYPE)			\
 	for (TYPE *(it) = DQUEUE_HEAD(q),			\
-	     *__it = DQUEUE_NEXT(it);				\
+	     *__it = DQUEUE_NEXT_IP(it, ip);			\
 	     (it) != NULL;					\
 	     (it) = __it,					\
-	     __it = (__it != NULL) ? DQUEUE_NEXT(__it) : NULL)
+	     __it = (__it != NULL) ? DQUEUE_NEXT_IP(__it, ip) : NULL)
 
-#define DQUEUE_FOREACH_REVERSE(q, it, TYPE)			\
+#define DQUEUE_FOREACH_REVERSE_IP(q, ip, it, TYPE)		\
 	for (TYPE* (it) = DQUEUE_TAIL(q);			\
 	     (it) != NULL;					\
-	     (it) = DQUEUE_PREV(it))
+	     (it) = DQUEUE_PREV_IP(it, ip))
 
-#define DQUEUE_FOREACH_REVERSE_SAFE(q, it, TYPE)		\
+#define DQUEUE_FOREACH_REVERSE_SAFE_IP(q, ip, it, TYPE)		\
 	for (TYPE *(it) = DQUEUE_TAIL(q),			\
-	     *__it = DQUEUE_PREV(it);				\
+	     *__it = DQUEUE_PREV_IP(it, ip);			\
 	     (it) != NULL;					\
 	     (it) = __it,					\
-	     __it = (__it != NULL) ? DQUEUE_PREV(__it) : NULL)
+	     __it = (__it != NULL) ? DQUEUE_PREV_IP(__it, ip) : NULL)
+
+#define DQUEUE_REMOVE_IP(q, ip, it)					\
+do {									\
+	if ((it)->ip ## _prev != NULL)					\
+		(it)->ip ## _prev->ip ## _next = (it)->ip ## _next;	\
+	if ((it)->ip ## _next != NULL)					\
+		(it)->ip ## _next->ip ## _prev = (it)->ip ## _prev;	\
+	if ((q)->dq_head == (it)) 					\
+		(q)->dq_head = (it)->ip ## _next;			\
+	if ((q)->dq_tail == (it)) 					\
+		(q)->dq_tail = (it)->ip ## _prev;			\
+} while(0)
+
+#define DQUEUE_ADD_TAIL(q, item)				\
+	DQUEUE_ADD_TAIL_IP(q, qi, item)
+
+#define DQUEUE_ADD_HEAD(q, item)				\
+	DQUEUE_ADD_HEAD_IP(q, qi, item)
+
+#define DQUEUE_POP_HEAD(q)					\
+	DQUEUE_POP_HEAD_IP(q, qi)
+
+#define DQUEUE_POP_TAIL(q)					\
+	DQUEUE_POP_TAIL_IP(q, qi)
+
+#define DQUEUE_NEXT(it)						\
+	DQUEUE_NEXT_IP(it, qi)
 
 #define DQUEUE_REMOVE(q, it)					\
-do {								\
-	if ((it)->qi_prev != NULL)				\
-		(it)->qi_prev->qi_next = (it)->qi_next;		\
-	if ((it)->qi_next != NULL)				\
-		(it)->qi_next->qi_prev = (it)->qi_prev;		\
-	if ((q)->dq_head == (it)) 				\
-		(q)->dq_head = (it)->qi_next;			\
-	if ((q)->dq_tail == (it)) 				\
-		(q)->dq_tail = (it)->qi_prev;			\
-} while(0)
+	DQUEUE_REMOVE_IP(q, qi, it)
+
+#define DQUEUE_PREV(it)						\
+	DQUEUE_PREV_IP(it, qi)
+
+#define DQUEUE_FOREACH(q, it, TYPE)				\
+	DQUEUE_FOREACH_IP(q, qi, it, TYPE)
+
+#define DQUEUE_FOREACH_SAFE(q, it, TYPE)			\
+	DQUEUE_FOREACH_SAFE_IP(q, qi, it, TYPE)
+
+#define DQUEUE_FOREACH_REVERSE(q, it, TYPE)			\
+	DQUEUE_FOREACH_REVERSE_IP(q, qi, it, TYPE)
+
+#define DQUEUE_FOREACH_REVERSE_SAFE(q, it, TYPE)		\
+	DQUEUE_FOREACH_REVERSE_SAFE_IP(q, qi, it, TYPE)
 
 #endif /* __ANANAS_DQUEUE_H__ */
