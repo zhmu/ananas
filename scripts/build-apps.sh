24d61018dcd8eda16b3b806327b731d82b7b0673
Rink Springer <rink@rink.nu>
2012-07-10 21:40:22 +0200

    scripts: Catch up with the new building layout
    This also properly installs the headers for the cross compiler to use.

--- scripts/build-apps.sh
+++ scripts/build-apps.sh
@@ -16,15 +16,19 @@ do_crt()
 	make install
 }
 
+do_headers()
+{
+	cd $R/include
+	make ARCH=${ARCH} install
+}
+
 do_dash()
 {
 	cd $R/apps/dash
 	rm -rf build.${ARCH}
 	rm -f dash-${ARCH}
-	mkdir -p build.${ARCH}
 	make clean
 	make
-	cp dash-${ARCH} ${TARGET}/bin/sh
 }
 
 do_coreutils()
@@ -38,84 +42,60 @@ do_coreutils()
 	# installs what we do care about, that's good enough for now
 	#
 	make clean
-	make || true
-	cd coreutils-8.7
-	make install || true
-	if [ ! -f $R/apps/coreutils/build.${ARCH}/bin/yes ]; then
+	make .install || true
+	if [ ! -f $R/apps/output.${ARCH}/bin/yes ]; then
 		echo  "*** coreutils build failure"
 		exit 1
 	fi
-	cp -R $R/apps/coreutils/build.${ARCH}/* ${TARGET}
 }
 
 do_gmp()
 {
 	cd $R/apps/gmp
-	rm -rf build.${ARCH}
-	mkdir -p build.${ARCH}
 	make clean
 	make
-	cp -R $R/apps/gmp/build.${ARCH}/* ${TARGET}/usr
 }
 
 do_mpfr()
 {
 	cd $R/apps/mpfr
-	rm -rf build.${ARCH}
-	mkdir -p build.${ARCH}
 	make clean
 	make
-	cp -R $R/apps/mpfr/build.${ARCH}/* ${TARGET}/usr
 }
 
 do_mpc()
 {
 	cd $R/apps/mpc
-	rm -rf build.${ARCH}
-	mkdir -p build.${ARCH}
 	make clean
 	make
-	cp -R $R/apps/mpc/build.${ARCH}/* ${TARGET}/usr
 }
 
 do_binutils()
 {
 	cd $R/apps/binutils
-	rm -rf build.${ARCH}
-	mkdir -p build.${ARCH}
 	make clean
 	make
-	cp -R $R/apps/binutils/build.${ARCH}/* ${TARGET}/usr
 }
 
 do_gcc()
 {
 	cd $R/apps/gcc
-	rm -rf build.${ARCH}
-	mkdir -p build.${ARCH}
 	make clean
 	make
-	cp -R $R/apps/gcc/build.${ARCH}/* ${TARGET}/usr
 }
 
 do_make()
 {
 	cd $R/apps/make
-	rm -rf build.${ARCH}
-	mkdir -p build.${ARCH}
 	make clean
 	make
-	cp -R $R/apps/make/build.${ARCH}/* ${TARGET}/usr
 }
 
 do_jpeg()
 {
 	cd $R/apps/jpeg
-	rm -rf build.${ARCH}
-	mkdir -p build.${ARCH}
 	make clean
 	make
-	cp -R $R/apps/jpeg/build.${ARCH}/* ${TARGET}/usr
 }
 
 install_includes()
@@ -130,6 +110,12 @@ install_lib()
 	cp $R/lib/crt/${ARCH}/crt1*.o ${TARGET}/usr/lib
 }
 
+install_build()
+{
+	cd $R/apps/output.${ARCH}
+	cp -R * ${TARGET}
+}
+
 ARCH=i386
 TARGET=/mnt
 R=`cd .. && pwd`
@@ -137,11 +123,10 @@ cd $R
 
 # prepare target
 rm -rf ${TARGET}/*
-mkdir -p ${TARGET}/bin
-mkdir -p ${TARGET}/usr
 
 do_libc
 do_crt
+do_headers
 do_dash
 do_coreutils
 do_gmp
@@ -152,5 +137,6 @@ do_binutils
 do_gcc
 do_make
 
+install_build
 install_includes
 install_lib
