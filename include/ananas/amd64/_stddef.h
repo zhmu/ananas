5b2fa5ddb82de03a810a6046b879cd7fa4f3503e
Rink Springer <rink@rink.nu>
2012-08-26 14:19:03 +0200

    include: Accept __SIZE_TYPE__ and __PTRDIFF_TYPE__ as valid size_t/ptrdiff_t type definitions
    This is mainly useful when building libgcc and crt*.o as gcc has the tendancy to declare {size,ptrdiff}_t itself in its stddef.h which gets included before our version - there is no functional change as the result is the same.

--- include/ananas/amd64/_stddef.h
+++ include/ananas/amd64/_stddef.h
@@ -1,7 +1,16 @@
 #ifndef __AMD64_STDDEF_H__
 #define __AMD64_STDDEF_H__
 
+#ifdef __SIZE_TYPE__
+typedef __SIZE_TYPE__ size_t;
+#else
 typedef unsigned long	size_t;
+#endif
+
+#ifdef __PTRDIFF_TYPE__
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+#else
 typedef unsigned long	ptrdiff_t;
+#endif
 
 #endif
