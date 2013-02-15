ef5931fca049fd3933981aeef05438b462a48394
Rink Springer <rink@rink.nu>
2013-02-15 19:21:28 +0100

    kernel: Make the exit() syscall correctly tracable

--- kernel/gen/std.c
+++ kernel/gen/std.c
@@ -1,10 +1,14 @@
 #include <ananas/lib.h>
 #include <ananas/pcpu.h>
 #include <ananas/thread.h>
+#include <ananas/trace.h>
+
+TRACE_SETUP;
 
 void
 sys_exit(int exitcode)
 {
+	TRACE(SYSCALL, FUNC, "t=%p, exitcode=%u", PCPU_GET(curthread), exitcode);
 	thread_exit(THREAD_MAKE_EXITCODE(THREAD_TERM_SYSCALL, exitcode));
 }
 /* vim:set ts=2 sw=2: */
