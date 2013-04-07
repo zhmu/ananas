3bd12d97c46ab6e5c21bddc3e23db4bc1b2e2f97
Rink Springer <rink@rink.nu>
2013-04-07 11:32:40 +0200

    arm: Prefix the Marvell registers to make things a bit cleaner

--- include/ananas/arm/mv-regs.h
+++ include/ananas/arm/mv-regs.h
@@ -5,29 +5,29 @@
  * Marvell registers as they appear on the SheevaPlug.
  */
 
-#define REG_BASE	0xf1000000
-#define UART_BASE (REG_BASE+0x12000)
+#define MV_REG_BASE	0xf1000000
+#define MV_UART_BASE	(MV_REG_BASE+0x12000)
 
 /* Register names are from 88F6281 Function Specification, p 678 */
-#define UART0_THR		(UART_BASE+0x0000)			/* Transmit Holding Register */
-#define UART0_DLL		(UART_BASE+0x0000)			/* Divisor Latch Low register */
-#define UART0_RBR		(UART_BASE+0x0000)			/* Receive Buffer Register */
-#define UART0_IER		(UART_BASE+0x0004)			/* Interrupt Enable Register */
-#define UART0_DLH		(UART_BASE+0x0004)			/* Divisor Latch High register */
-#define UART0_IIR		(UART_BASE+0x0008)			/* Interrupt Identify Register */
-#define UART0_FCR		(UART_BASE+0x0008)			/* FIFO Control Register */
-#define UART0_LCR		(UART_BASE+0x000c)			/* Line Control Register */
-#define UART0_MCR		(UART_BASE+0x0010)			/* Modem Control Register */
-#define UART0_LSR		(UART_BASE+0x0014)			/* Line Status Register */
-#define UART0_LSR_DATARXSTAT		(1 << 0)			/* Receive buffer contains char */
-#define UART0_LSR_OVERRUNERR		(1 << 1)			/* Overrun error */
-#define UART0_LSR_PARERR		(1 << 2)			/* Parity error */
-#define UART0_LSR_FRAMEERR		(1 << 3)			/* Frame error */
-#define UART0_LSR_BI			(1 << 4)			/* Serial input held lo too long */
-#define UART0_LSR_THRE			(1 << 5)			/* Transmitter Holding Register Empty */
-#define UART0_LSR_TXEMPTY		(1 << 6)			/* Transmitter Holding TX Empty */
-#define UART0_LSR_RXFIFOERR		(1 << 7)			/* FIFO parity/framing error */
-#define UART0_MSR		(UART_BASE+0x0018)			/* Modem Status Register */
-#define UART0_SCR		(UART_BASE+0x001c)			/* Scratch Pad Register */
+#define MV_UART_THR(x)		((x)+0x0000)			/* Transmit Holding Register */
+#define MV_UART_DLL(x)		((x)+0x0000)			/* Divisor Latch Low register */
+#define MV_UART_RBR(x)		((x)+0x0000)			/* Receive Buffer Register */
+#define MV_UART_IER(x)		((x)+0x0004)			/* Interrupt Enable Register */
+#define MV_UART_DLH(x)		((x)+0x0004)			/* Divisor Latch High register */
+#define MV_UART_IIR(x)		((x)+0x0008)			/* Interrupt Identify Register */
+#define MV_UART_FCR(x)		((x)+0x0008)			/* FIFO Control Register */
+#define MV_UART_LCR(x)		((x)+0x000c)			/* Line Control Register */
+#define MV_UART_MCR(x)		((x)+0x0010)			/* Modem Control Register */
+#define MV_UART_LSR(x)		((x)+0x0014)			/* Line Status Register */
+#define MV_UART_LSR_DATARXSTAT		(1 << 0)			/* Receive buffer contains char */
+#define MV_UART_LSR_OVERRUNERR		(1 << 1)			/* Overrun error */
+#define MV_UART_LSR_PARERR		(1 << 2)			/* Parity error */
+#define MV_UART_LSR_FRAMEERR		(1 << 3)			/* Frame error */
+#define MV_UART_LSR_BI			(1 << 4)			/* Serial input held lo too long */
+#define MV_UART_LSR_THRE			(1 << 5)			/* Transmitter Holding Register Empty */
+#define MV_UART_LSR_TXEMPTY		(1 << 6)			/* Transmitter Holding TX Empty */
+#define MV_UART_LSR_RXFIFOERR		(1 << 7)			/* FIFO parity/framing error */
+#define MV_UART_MSR(x)		((x)+0x0018)			/* Modem Status Register */
+#define MV_UART_SCR(x)		((x)+0x001c)			/* Scratch Pad Register */
 
 #endif /* __ANANAS_MV_REGS_H__ */
