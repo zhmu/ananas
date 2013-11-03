d0eae12bff19b9f3f99ce975da4f8b2ec6c23d82
Rink Springer <rink@rink.nu>
2013-11-03 10:17:26 +0100

    kernel: Rework locking by ditching waitqueue's and relying on semaphores, mutexes and spinlocks
    Pretty sure this won't be the end of it...

--- include/ananas/lock.h
+++ include/ananas/lock.h
@@ -1,9 +1,8 @@
 #ifndef __LOCK_H__
 #define __LOCK_H__
 
-#include <machine/atomic.h>
-#include <ananas/_types/spinlock.h>
-#include <ananas/waitqueue.h>
+#include <ananas/types.h>
+#include <ananas/dqueue.h>
 
 /*
  * Spinlocks are simply locks that just keep the CPU busy waiting if they can't
@@ -16,25 +15,12 @@
  * waitqueues, so they can't be declared in this file...
  */
 
-/* If set, keep trace of where mutexes were locked to diagnose deadlocks */
-#undef MUTEX_DEBUG
-
-/*
- * Mutexes are sleepable locks that will suspend the current thread when the
- * lock is already being held. They cannot be used from interrupt context.
- *
- * XXX These could be re-implemented using binary semaphores
- */
-typedef struct {
-	const char*		mtx_name;
-	thread_t*		mtx_owner;
-	atomic_t		mtx_var;
-	struct WAIT_QUEUE	mtx_wq;
-#ifdef MUTEX_DEBUG
-	const char*		mtx_fname;
-	int			mtx_line;
-#endif
-} mutex_t;
+struct SEMAPHORE_WAITER {
+	thread_t*		sw_thread;
+	int			sw_signalled;
+	DQUEUE_FIELDS(struct SEMAPHORE_WAITER);
+};
+DQUEUE_DEFINE(semaphore_wq, struct SEMAPHORE_WAITER);
 
 /*
  * Semaphores are sleepable locks which guard an amount of units of a
@@ -43,10 +29,24 @@ typedef struct {
 typedef struct {
 	spinlock_t		sem_lock;
 	unsigned int		sem_count;
-	struct WAIT_QUEUE	sem_wq;
+	struct semaphore_wq	sem_wq;
 } semaphore_t;
 
 #define SPINLOCK_DEFAULT_INIT { { 0 } }
+
+/*
+ * Mutexes are sleepable locks that will suspend the current thread when the
+ * lock is already being held. They cannot be used from interrupt context; they
+ * are implemented as binary semaphores.
+ */
+typedef struct {
+	const char*		mtx_name;
+	thread_t*		mtx_owner;
+	semaphore_t		mtx_sem;
+	const char*		mtx_fname;
+	int			mtx_line;
+} mutex_t;
+
 
 /* Ordinary spinlocks which can be preempted at any time */
 void spinlock_lock(spinlock_t* l);
@@ -61,12 +61,11 @@ void spinlock_unlock_unpremptible(spinlock_t* l, register_t state);
 void mutex_init(mutex_t* mtx, const char* name);
 void mutex_lock_(mutex_t* mtx, const char* file, int line);
 void mutex_unlock(mutex_t* mtx);
+#define mutex_lock(mtx) mutex_lock_(mtx, __FILE__, __LINE__)
 
 /* Semaphores */
 void sem_init(semaphore_t* sem, int count);
 void sem_signal(semaphore_t* sem);
 void sem_wait(semaphore_t* sem);
-
-#define mutex_lock(mtx) mutex_lock_(mtx, __FILE__, __LINE__)
 
 #endif /* __LOCK_H__ */
