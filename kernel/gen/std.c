864ef03ec9ec8b3346f5aef422761e18893fb852
Rink Springer <rink@rink.nu>
2014-04-12 12:25:04 +0200

    kernel: Fix the prototype of sys_exit() so that the intended return code is used and not the thread pointer

--- kernel/gen/std.c
+++ kernel/gen/std.c
@@ -6,9 +6,10 @@
 TRACE_SETUP;
 
 void
-sys_exit(int exitcode)
+sys_exit(thread_t* t, int exitcode)
 {
-	TRACE(SYSCALL, FUNC, "t=%p, exitcode=%u", PCPU_GET(curthread), exitcode);
+	TRACE(SYSCALL, FUNC, "t=%p, exitcode=%u", t, exitcode);
+	
 	thread_exit(THREAD_MAKE_EXITCODE(THREAD_TERM_SYSCALL, exitcode));
 }
 /* vim:set ts=2 sw=2: */
