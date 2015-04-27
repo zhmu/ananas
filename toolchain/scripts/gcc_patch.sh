d541729f7ca2707cecef755f9cec15b14ca37fc8
Rink Springer <rink@rink.nu>
2015-04-27 11:51:41 +0200

    toolchain: Remove avr32 specifics, this was never supported

--- toolchain/scripts/gcc_patch.sh
+++ toolchain/scripts/gcc_patch.sh
@@ -15,7 +15,6 @@ ${SED} -r 's/(\-aros\*) \\$/\1 \| -ananas\* \\/' < $T/config.sub.tmp > $T/config
 # patch 'gcc/config.gcc'
 # XXX we do not override powerpc-*-elf yet (should we?)
 # XXX we do not override arm-*-elf yet (should we?)
-# XXX is using the linux tmake fiels for avr32 ok?
 awk '{ print }
 /# Common parts for widely ported systems/ { STATE = 1 }
 /case \${target} in/ && STATE == 0 { STATE = 2 }
@@ -41,12 +40,6 @@ awk '{ print }
 	print "\textra_options=\"${extra_options} rs6000/sysv4.opt\""
 	print "\ttmake_file=\"rs6000/t-fprules rs6000/t-fprules-fpbit rs6000/t-ppcos ${tmake_file} rs6000/t-ppccomm\""
         print "\t;;"
-	print "avr32-*-ananas*)"
-	print "\ttm_file=\"dbxelf.h elfos.h ananas.h ananas-avr32.h avr32/avr32.h\""
-	print "\ttmake_file=\"t-linux avr32/t-avr32 avr32/t-elf\""
-        print "\textra_modes=avr32/avr32-modes.def"
-        print "\tgnu_ld=yes"
-	print "\t;;"
 	print "arm-*-ananas*)"
 	print "\ttm_file=\"dbxelf.h elfos.h arm/elf.h arm/aout.h arm/arm.h ananas.h\""
 	print "\ttmake_file=\"${tmake_file} t-linux arm/t-arm\""
@@ -94,32 +87,19 @@ crtend.o%s crtn.o%s			\
 "
 ' > $T/gcc/config/ananas.h
 
-# generate 'gcc/config/ananas-avr32.h'
-echo '#undef TARGET_CPU_CPP_BUILTINS
-#define TARGET_CPU_CPP_BUILTINS()        \
-  do {                                   \
-    builtin_define_std ("__avr32__");    \
-    builtin_define_std ("__AVR32__");    \
-    builtin_define (avr32_part->macro);  \
-    builtin_define (avr32_arch->macro);  \
-  } while(0);
-' > $T/gcc/config/ananas-avr32.h
-
-# patch 'libgcc/config.host' - note: not for gcc 4.2
-if [ -f $T/libgcc/config.host ]; then
-	awk '
-	{ print }
-	/case \${host} in/ { STATE = 1 }
-	/# Support site-specific machine types./ && STATE == 1 {
-		print "i[3-7]86-*-ananas*)"
-		print "\t;;"
-		print "x86_64-*-ananas*)"
-		print "\t;;"
-		print "powerpc-*-ananas*)"
-		print "\t;;"
-		print "arm-*-ananas*)"
-		print "\t;;"
-		STATE = 0
-	}' < $T/libgcc/config.host > $T/libgcc/config.host.new
-	mv $T/libgcc/config.host.new $T/libgcc/config.host
-fi
+# patch 'libgcc/config.host'
+awk '
+{ print }
+/case \${host} in/ { STATE = 1 }
+/# Support site-specific machine types./ && STATE == 1 {
+	print "i[3-7]86-*-ananas*)"
+	print "\t;;"
+	print "x86_64-*-ananas*)"
+	print "\t;;"
+	print "powerpc-*-ananas*)"
+	print "\t;;"
+	print "arm-*-ananas*)"
+	print "\t;;"
+	STATE = 0
+}' < $T/libgcc/config.host > $T/libgcc/config.host.new
+mv $T/libgcc/config.host.new $T/libgcc/config.host
