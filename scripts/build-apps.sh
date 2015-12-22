3bae0088ea73fadcb28bb82374d0548a78bcc458
Rink Springer <rink@rink.nu>
2015-12-22 19:08:27 +0100

    apps: Improve cross-compilation support

--- scripts/build-apps.sh
+++ scripts/build-apps.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -e
 
-#MAKE_ARGS=-j8
+MAKE_ARGS=-j8
 
 do_libc()
 {
 	cd $R/lib/libc/compile/${ARCH}
-	make clean
-	make ${MAKE_ARGS}
-	make install
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
+	make ARCH=${ARCH} install
 }
 
 do_crt()
 {
 	cd $R/lib/crt/${ARCH}
-	make clean
-	make ${MAKE_ARGS}
-	make install
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
+	make ARCH=${ARCH} install
 }
 
 do_headers()
@@ -29,8 +29,8 @@ do_dash()
 	cd $R/apps/dash
 	rm -rf build.${ARCH}
 	rm -f dash-${ARCH}
-	make clean
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 do_coreutils()
@@ -38,49 +38,49 @@ do_coreutils()
 	cd $R/apps/coreutils
 	rm -rf build.${ARCH}
 	mkdir -p build.${ARCH}
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 do_gmp()
 {
 	cd $R/apps/gmp
-	make clean
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 do_mpfr()
 {
 	cd $R/apps/mpfr
-	make clean
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 do_mpc()
 {
 	cd $R/apps/mpc
-	make clean
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 do_binutils()
 {
 	cd $R/apps/binutils
-	make clean
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 do_gcc()
 {
 	cd $R/apps/gcc
-	make clean
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 do_make()
 {
 	cd $R/apps/make
-	make clean
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 do_jpeg()
@@ -88,8 +88,8 @@ do_jpeg()
 	cd $R/apps/jpeg
 	rm -rf build.${ARCH}
 	mkdir -p build.${ARCH}
-	make clean
-	make ${MAKE_ARGS}
+	make ARCH=${ARCH} clean
+	make ARCH=${ARCH} ${MAKE_ARGS}
 }
 
 install_includes()
@@ -110,17 +110,18 @@ install_build()
 	cp -R * ${TARGET}
 }
 
-ARCH=i386
+ARCH=amd64
 TARGET=/mnt
 R=`cd .. && pwd`
 
 # ensure the immediate output directory exists, or configure
 # will try to place things in /
+rm -rf $R/apps/output.${ARCH}
 mkdir -p $R/apps/output.${ARCH}
 
 # make sure we have our compiler in our path; configure will not raise red
 # flags if it can't find it
-CROSS_CC=`which i386-elf-ananas-gcc || true`
+CROSS_CC=`which ${ARCH}-elf-ananas-gcc || true`
 if [ ! -x "$CROSS_CC" ]; then
 	echo "*** gcc not found; is the correct cross-path set?"
 	exit 1
