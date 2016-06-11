c2c12e8ffe7cf5d2a0221e984a2cb371f9c6e597
Rink Springer <rink@rink.nu>
2016-06-11 11:23:24 +0200

    kernel: Display current thread name during tracing

--- kernel/kern/trace.c
+++ kernel/kern/trace.c
@@ -1,5 +1,7 @@
 #include <ananas/types.h>
 #include <ananas/lib.h>
+#include <ananas/pcpu.h>
+#include <ananas/threadinfo.h>
 #include <ananas/trace.h>
 
 uint32_t trace_subsystem_mask[TRACE_SUBSYSTEM_LAST];
@@ -15,9 +17,12 @@ tracef(int fileid, const char* func, const char* fmt, ...)
 #endif
 	#endif
 
+	thread_t* curthread = PCPU_GET(curthread);
+	const char* tname = curthread->t_threadinfo->ti_args;
+
 	/* XXX This function is a SMP-disaster for now */
   va_list ap;
-	kprintf("[% 4u.%03u] %s: ", timestamp / 1000, timestamp % 1000, func);
+	kprintf("[% 4u.%03u] (%s) %s: ", timestamp / 1000, timestamp % 1000, tname, func);
   va_start(ap, fmt);
   vaprintf(fmt, ap);
   va_end(ap);
