d01beae4220fda6b74687be86b00f6c4ce8539e7
Rink Springer <rink@rink.nu>
2016-06-26 11:34:36 +0200

    kernel: Ensure TRACE() also buffers on a per-message base

--- kernel/kern/trace.c
+++ kernel/kern/trace.c
@@ -1,8 +1,11 @@
 #include <ananas/types.h>
+#include <ananas/console.h>
 #include <ananas/lib.h>
 #include <ananas/pcpu.h>
 #include <ananas/threadinfo.h>
 #include <ananas/trace.h>
+
+#define TRACE_PRINTF_BUFSIZE 256
 
 uint32_t trace_subsystem_mask[TRACE_SUBSYSTEM_LAST];
 
@@ -10,23 +13,27 @@ void
 tracef(int fileid, const char* func, const char* fmt, ...)
 {
 	uint32_t timestamp = 0;
-	#if defined(__i386__) || defined(__amd64__)
-#if 1
-		uint32_t x86_get_ms_since_boot();
-		timestamp = x86_get_ms_since_boot();
+#if defined(__i386__) || defined(__amd64__)
+	/* XXX This should be generic somehow */
+	uint32_t x86_get_ms_since_boot();
+	timestamp = x86_get_ms_since_boot();
 #endif
-	#endif
+
+	char buf[TRACE_PRINTF_BUFSIZE];
 
 	thread_t* curthread = PCPU_GET(curthread);
 	const char* tname = curthread->t_threadinfo->ti_args;
 
-	/* XXX This function is a SMP-disaster for now */
-  va_list ap;
-	kprintf("[% 4u.%03u] (%s) %s: ", timestamp / 1000, timestamp % 1000, tname, func);
-  va_start(ap, fmt);
-  vaprintf(fmt, ap);
-  va_end(ap);
-	kprintf("\n");
+	snprintf(buf, sizeof(buf), "[%4u.%03u] (%s) %s: ", timestamp / 1000, timestamp % 1000, tname, func);
+
+	va_list va;
+	va_start(va, fmt);
+	vsnprintf(buf + strlen(buf), sizeof(buf) - strlen(buf) - 2, fmt, va);
+	buf[sizeof(buf) - 2] = '\0';
+	strcat(buf, "\n");
+	va_end(va);
+
+	console_putstring(buf);
 }
 
 /* vim:set ts=2 sw=2: */
