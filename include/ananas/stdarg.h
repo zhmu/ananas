0ef964ebe21ab64411011dcee6e2ea6da7fbb0d6
Rink Springer <rink@rink.nu>
2016-09-19 11:15:18 +0200

    include: Add kernel includes for std{arg,int}.h
    We can no longer use the userland ones as these header files aren't installed
    when we build the kernel (and it's much cleaner to separate them)

---
+++ include/ananas/stdarg.h
@@ -0,0 +1,9 @@
+#ifndef __ANANAS_STDARG_H__
+#define __ANANAS_STDARG_H__
+
+#define va_arg __builtin_va_arg
+#define va_start __builtin_va_start
+#define va_end __builtin_va_end
+#define va_list __builtin_va_list
+
+#endif /* __ANANAS_STDARG_H__ */
