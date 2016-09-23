ed3c68cd26b360063cb7114f1aaa1f447bb161a0
Rink Springer <rink@rink.nu>
2016-09-23 13:47:32 +0200

    libc: Stub mbstowcs/wcstombs wide-char functions
    It's very odd that these aren't implemented. Should at least report this.

---
+++ lib/libc/functions/stdlib/unimplemented/mbstowcs.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <errno.h>
+
+size_t mbstowcs(wchar_t * pwcs, const char * s, size_t n)
+{
+	errno = EILSEQ;
+	return (size_t)-1;
+}
---
+++ lib/libc/functions/stdlib/unimplemented/wcstombs.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <errno.h>
+
+size_t wcstombs(char * s, const wchar_t * pwcs, size_t n)
+{
+	errno = EILSEQ;
+	return (size_t)-1;
+}
