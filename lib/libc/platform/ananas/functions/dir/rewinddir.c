0653518653e7985d12a868f023553c1ac45aab2a
Rink Springer <rink@rink.nu>
2016-09-23 14:03:45 +0200

    libc: Implement rewinddir()

---
+++ lib/libc/platform/ananas/functions/dir/rewinddir.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <dirent.h>
+
+void rewinddir(DIR* dirp)
+{
+	/* Discard what we have so far */
+	dirp->d_cur_pos = 0;
+
+	/* And rewind the stream - we cannot recover on failure as we have no return code */
+	(void)lseek(dirp->d_fd, 0, SEEK_SET);
+}
