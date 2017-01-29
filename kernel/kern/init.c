f39867777a68068603e4af20f509af4b236bd1ae
Rink Springer <rink@rink.nu>
2017-01-29 16:47:42 +0100

    kernel: Use commandline arguments to set up root filesystem and init

--- kernel/kern/init.c
+++ kernel/kern/init.c
@@ -1,5 +1,6 @@
 #include <ananas/device.h>
 #include <ananas/console.h>
+#include <ananas/cmdline.h>
 #include <ananas/error.h>
 #include <ananas/exec.h>
 #include <ananas/mm.h>
@@ -14,12 +15,6 @@
 #include <machine/param.h> /* for PAGE_SIZE */
 
 #include "options.h"
-
-#define SHELL_BIN "/bin/sh"
-#define ROOT_DEVICE "slice0"
-#define ROOT_FS_TYPE "fatfs"
-#define DEVFS_MOUNTPOINT "/dev"
-#undef RAMDISK
 
 /* If set, display the entire init tree before launching it */
 #undef VERBOSE_INIT
@@ -144,73 +139,70 @@ hello_world()
 
 INIT_FUNCTION(hello_world, SUBSYSTEM_CONSOLE, ORDER_LAST);
 
-#if 0
 static errorcode_t
 mount_filesystems()
 {
 	errorcode_t err;
 
-	kprintf("- Mounting / from %s...", ROOT_DEVICE);
-	err = vfs_mount(ROOT_DEVICE, "/", ROOT_FS_TYPE, NULL);
+	/* We expect root=type:device here */
+	const char* rootfs_arg = cmdline_get_string("root");
+	if (rootfs_arg == NULL)
+		return ANANAS_ERROR_NONE;
+
+	char rootfs_type[64];
+	const char* p = strchr(rootfs_arg, ':');
+	if (p == NULL)
+		return ANANAS_ERROR_NONE;
+
+	memcpy(rootfs_type, rootfs_arg, p - rootfs_arg);
+	rootfs_type[p - rootfs_arg] = '\0';
+	const char* rootfs = p + 1;
+
+	kprintf("- Mounting / (type %s) from %s...", rootfs_type, rootfs);
+	err = vfs_mount(rootfs, "/", rootfs_type, NULL);
 	if (err == ANANAS_ERROR_NONE) {
 		kprintf(" ok\n");
 	} else {
 		kprintf(" failed, error %i\n", err);
 	}
 
-#ifdef OPTION_SYSFS
-	kprintf("- Mounting sysfs on %s...", SYSFS_MOUNTPOINT);
-	err = vfs_mount(NULL, SYSFS_MOUNTPOINT, "sysfs", NULL);
-	if (err == ANANAS_ERROR_NONE) {
-		kprintf(" ok\n");
-	} else {
-		kprintf(" failed, error %i\n", err);
-	}
-#endif
-
-#ifdef RAMDISK
-	kprintf("- Mounting romdisk on /rom...");
-	err = vfs_mount(RAMDISK, "/rom", "cramfs", NULL);
-	if (err == ANANAS_ERROR_NONE) {
-		kprintf(" ok\n");
-	} else {
-		kprintf(" failed, error %i\n", err);
-	}
-#endif
 	return ANANAS_ERROR_OK;
 }
 
 INIT_FUNCTION(mount_filesystems, SUBSYSTEM_VFS, ORDER_LAST);
 
-#ifdef SHELL_BIN
 static errorcode_t
-launch_shell()
+launch_init()
 {
 	errorcode_t err = ANANAS_ERROR_NONE;
 	process_t* proc;
 	thread_t* t;
+
+	const char* init_path = cmdline_get_string("init");
+	if (init_path == NULL || init_path[0] == '\0')
+		return err;
 
 	err = process_alloc(NULL, &proc);
 	if (err != ANANAS_ERROR_NONE) {
 		kprintf(" couldn't create process, %i\n", err);
 		return err;
 	}
-	err = thread_alloc(proc, &t, "sh", THREAD_ALLOC_DEFAULT);
+	err = thread_alloc(proc, &t, "init", THREAD_ALLOC_DEFAULT);
 	process_deref(proc); /* 't' should have a ref, we don't need it anymore */
 	if (err != ANANAS_ERROR_NONE) {
 		kprintf(" couldn't create thread, %i\n", err);
 		return err;
 	}
 
-	kprintf("- Lauching %s...", SHELL_BIN);
+	kprintf("- Lauching init from %s...", init_path);
 	struct VFS_FILE file;
-	err = vfs_open(SHELL_BIN, proc->p_cwd, &file);
+	err = vfs_open(init_path, proc->p_cwd, &file);
 	if (err != ANANAS_ERROR_NONE) {
-		kprintf(" couldn't open file, %i\n", err);
+		kprintf(" couldn't open init executable, %i\n", err);
 		return err;
 	}
 
-	const char args[] = "sh\0-l\0\0";
+	const char args[] = "init\0\0";
 	const char env[] = "OS=Ananas\0USER=root\0\0";
 	process_set_args(proc, args, sizeof(args));
 	process_set_environment(proc, env, sizeof(env));
@@ -228,9 +220,7 @@ launch_shell()
 	return err;
 }
 
-INIT_FUNCTION(launch_shell, SUBSYSTEM_SCHEDULER, ORDER_MIDDLE);
-#endif /* SHELL_BIN  */
-#endif
+INIT_FUNCTION(launch_init, SUBSYSTEM_SCHEDULER, ORDER_MIDDLE);
 
 static void
 init_thread_func(void* done)
@@ -252,6 +242,7 @@ init_thread_func(void* done)
 void
 mi_startup()
 {
+	cmdline_init();
 	LIST_INIT(&initfunc_dynamics);
 
 	/*
