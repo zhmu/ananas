50f8111a476c6e105e7014cd588e1937a4cef2a4
Rink Springer <rink@rink.nu>
2017-04-29 16:40:17 +0200

    kernel: Add the corebus device to the tree
    Wonder how I missed this.

---
+++ kernel/dev/generic/corebus.cpp
@@ -0,0 +1,53 @@
+#include <ananas/device.h>
+#include <ananas/driver.h>
+#include <ananas/lib.h>
+#include <ananas/trace.h>
+
+TRACE_SETUP;
+
+namespace {
+
+class CoreBus : public Ananas::Device, private Ananas::IDeviceOperations
+{
+public:
+	CoreBus()
+	{
+		strcpy(d_Name, "corebus");
+		d_Unit = 0;
+	}
+
+	IDeviceOperations& GetDeviceOperations() override {
+		return *this;
+	}
+
+	errorcode_t Attach() override
+	{
+		return ananas_success();
+	}
+
+	errorcode_t Detach() override
+	{
+		return ananas_success();
+	}
+
+	void DebugDump() override
+	{
+	}
+
+	errorcode_t DeviceControl(process_t* proc, unsigned int op, void* buffer, size_t len) override
+	{
+		return ANANAS_ERROR(BAD_OPERATION);
+	}
+};
+
+errorcode_t
+Initialize()
+{
+	return Ananas::DeviceManager::AttachSingle(*new CoreBus);
+}
+
+INIT_FUNCTION(Initialize, SUBSYSTEM_DEVICE, ORDER_LAST);
+
+} // unnamed namespace
+
+/* vim:set ts=2 sw=2: */
