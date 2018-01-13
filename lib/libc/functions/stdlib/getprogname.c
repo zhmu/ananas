ac755295e83d8c4ebfe3beab2470d518435af340
Rink Springer <rink@rink.nu>
2018-01-13 11:01:52 +0100

    libc: Add [gs]etprogname(), which is a BSD-ism
    However, I don't see much harm in adding it here - it could be useful.

---
+++ lib/libc/functions/stdlib/getprogname.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+extern const char* __progname;
+
+const char* getprogname()
+{
+	return __progname;
+}
---
+++ lib/libc/functions/stdlib/setprogname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+
+const char* __progname = NULL;
+
+void setprogname(const char* progname)
+{
+	const char* ptr = strrchr(progname, '/');
+	if (ptr != NULL)
+		__progname = ptr + 1;
+	else
+		__progname = progname;
+}
