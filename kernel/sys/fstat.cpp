d4f2299ec6ec1ef59dc2c6ac6e65092a48b713fa
Rink Springer <rink@rink.nu>
2018-02-11 14:08:41 +0100

    kernel: Allow fstat() to be used on devices
    This allows cat(1) to work as it tries to check whether stdin/stdout overlap
    and such.

--- kernel/sys/fstat.cpp
+++ kernel/sys/fstat.cpp
@@ -1,6 +1,7 @@
 #include <ananas/types.h>
 #include <ananas/errno.h>
 #include "kernel/lib.h"
+#include "kernel/device.h"
 #include "kernel/handle.h"
 #include "kernel/result.h"
 #include "kernel/trace.h"
@@ -12,6 +13,8 @@ TRACE_SETUP;
 Result
 sys_fstat(Thread* t, handleindex_t hindex, struct stat* buf)
 {
+	TRACE(SYSCALL, FUNC, "t=%p, hindex=%d, buf=%p", t, hindex, buf);
+
 	/* Get the handle */
 	struct HANDLE* h;
 	RESULT_PROPAGATE_FAILURE(
@@ -24,8 +27,21 @@ sys_fstat(Thread* t, handleindex_t hindex, struct stat* buf)
         struct VFS_FILE* file = &h->h_data.d_vfs_file;
 	if (file->f_dentry != NULL) {
 		memcpy(buf, &file->f_dentry->d_inode->i_sb, sizeof(struct stat));
+	} else if (file->f_device != nullptr) {
+		memset(buf, 0, sizeof(struct stat));
+		buf->st_dev = (dev_t)(uintptr_t)file->f_device;
+		buf->st_rdev = buf->st_dev;
+		buf->st_ino = 0;
+		if (file->f_device->GetCharDeviceOperations() != nullptr)
+			buf->st_mode |= S_IFCHR;
+		else if (file->f_device->GetBIODeviceOperations() != nullptr)
+			buf->st_mode |= S_IFBLK;
+		else
+			buf->st_mode |= S_IFIFO; // XXX how did you open this?
+		buf->st_blksize = PAGE_SIZE; // XXX
 	} else {
-		return RESULT_MAKE_FAILURE(EINVAL); /* XXX maybe re-think this for devices */
+		// What's this?
+		return RESULT_MAKE_FAILURE(EINVAL);
 	}
 
 	return Result::Success();
