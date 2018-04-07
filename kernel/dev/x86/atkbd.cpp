cc2672485a841dc8a5a5f9453436afec59b28e4d
Rink Springer <rink@rink.nu>
2018-04-07 12:01:05 +0200

    atkbd: C++-ize the code a bit

--- kernel/dev/x86/atkbd.cpp
+++ kernel/dev/x86/atkbd.cpp
@@ -16,12 +16,34 @@ TRACE_SETUP;
 
 namespace {
 
+namespace scancode {
+constexpr uint8_t Escape = 0x01;
+constexpr uint8_t Control = 0x1d; // right-control has 0xe0 prefix
+constexpr uint8_t Tilde = 0x29;
+constexpr uint8_t LeftShift = 0x2a;
+constexpr uint8_t RightShift = 0x36;
+constexpr uint8_t Alt = 0x38; // right-alt has 0xe0 prefix
+constexpr uint8_t Delete = 0x53;
+constexpr uint8_t ReleasedBit = 0x80;
+}
+
+namespace flag {
+constexpr int Shift = 1;
+constexpr int Control = 2;
+constexpr int Alt = 4;
+}
+
+namespace port {
+constexpr int Status = 4;
+constexpr int StatusOutputFull = 1;
+}
+
 // Mapping of a scancode to an ASCII key value
-uint8_t atkbd_keymap[128] = {
+constexpr uint8_t atkbd_keymap[128] = {
 	/* 00-07 */    0, 0x1b, '1',  '2',  '3', '4', '5', '6',
 	/* 08-0f */  '7',  '8', '9',  '0',  '-', '=',   8,   9,
 	/* 10-17 */  'q',  'w', 'e',  'r',  't', 'y', 'u', 'i',
-	/* 18-1f */  'o',  'p', '[',  ']', 0x0d,   0, 'a', 's', 
+	/* 18-1f */  'o',  'p', '[',  ']', 0x0d,   0, 'a', 's',
 	/* 20-27 */  'd',  'f', 'g',  'h',  'j', 'k', 'l', ';',
 	/* 28-2f */  '\'',  '`',   0, '\\', 'z', 'x', 'c', 'v',
 	/* 30-37 */  'b',  'n', 'm',  ',',  '.', '/',   0, '*',
@@ -37,11 +59,11 @@ uint8_t atkbd_keymap[128] = {
 };
 
 // Mapping of a scancode to an ASCII key value, if shift is active
-uint8_t atkbd_keymap_shift[128] = {
+constexpr uint8_t atkbd_keymap_shift[128] = {
 	/* 00-07 */    0, 0x1b, '!',  '@',  '#', '$', '%', '^',
 	/* 08-0f */  '&',  '*', '(',  ')',  '_', '+',   8,   9,
 	/* 10-17 */  'Q',  'W', 'E',  'R',  'T', 'Y', 'U', 'I',
-	/* 18-1f */  'O',  'P', '{',  '}', 0x0d,   0, 'A', 'S', 
+	/* 18-1f */  'O',  'P', '{',  '}', 0x0d,   0, 'A', 'S',
 	/* 20-27 */  'D',  'F', 'G',  'H',  'J', 'K', 'L', ':',
 	/* 28-2f */  '"',  '~',   0,  '|',  'Z', 'X', 'C', 'V',
 	/* 30-37 */  'B',  'N', 'M',  '<',  '>', '?',   0, '*',
@@ -55,14 +77,6 @@ uint8_t atkbd_keymap_shift[128] = {
 	/* 70-76 */    0,    0,   0,    0,    0,   0,   0,   0,
 	/* 77-7f */    0,    0,   0,    0,    0,   0,   0,   0
 };
-
-#define ATKBD_BUFFER_SIZE	16
-#define ATKBD_FLAG_SHIFT 1
-#define ATKBD_FLAG_CONTROL 2
-#define ATKBD_FLAG_ALT 4
-
-#define ATKBD_PORT_STATUS 4
-#define ATKBD_STATUS_OUTPUT_FULL 1
 
 class ATKeyboard : public Ananas::Device, private Ananas::IDeviceOperations
 {
@@ -96,48 +110,50 @@ private:
 void
 ATKeyboard::OnIRQ()
 {
-	while(inb(kbd_ioport + ATKBD_PORT_STATUS) & ATKBD_STATUS_OUTPUT_FULL) {
+	while(inb(kbd_ioport + port::Status) & port::StatusOutputFull) {
 		uint8_t scancode = inb(kbd_ioport);
-		if ((scancode & 0x7f) == 0x2a /* LSHIFT */ ||
-				(scancode & 0x7f) == 0x36 /* RSHIFT */) {
-			if (scancode & 0x80)
-				kbd_flags &= ~ATKBD_FLAG_SHIFT;
-			else
-				kbd_flags |=  ATKBD_FLAG_SHIFT;
-			continue;
+		bool isReleased = (scancode & scancode::ReleasedBit) != 0;
+		scancode &= ~scancode::ReleasedBit;
+
+		// Handle setting control, alt, shift flags
+		{
+			auto setOrClearFlag = [&](int flag) {
+				if (isReleased)
+					kbd_flags &= ~flag;
+				else
+					kbd_flags |= flag;
+			};
+
+			switch(scancode) {
+				case scancode::LeftShift:
+				case scancode::RightShift:
+					setOrClearFlag(flag::Shift);
+					continue;
+				case scancode::Alt:
+					setOrClearFlag(flag::Alt);
+					continue;
+				case scancode::Control:
+					setOrClearFlag(flag::Control);
+					continue;
+			}
 		}
-		/* right-alt is 0xe0 0x38 but that does not matter */
-		if ((scancode & 0x7f) == 0x38 /* ALT */) {
-			if (scancode & 0x80)
-				kbd_flags &= ~ATKBD_FLAG_ALT;
-			else
-				kbd_flags |=  ATKBD_FLAG_ALT;
-			continue;
-		}
-		/* right-control is 0xe0 0x1d but that does not matter */
-		if ((scancode & 0x7f) == 0x1d /* CONTROL */) {
-			if (scancode & 0x80)
-				kbd_flags &= ~ATKBD_FLAG_CONTROL;
-			else
-				kbd_flags |=  ATKBD_FLAG_CONTROL;
-			continue;
-		}
-		if (scancode & 0x80) /* release event */
-			continue;
+		if (isReleased)
+			continue; // ignore released keys
 
 #ifdef OPTION_KDB
-		if ((kbd_flags == (ATKBD_FLAG_CONTROL | ATKBD_FLAG_SHIFT)) && scancode == 1 /* escape */) {
+		if ((kbd_flags == (flag::Control | flag::Shift)) && scancode == scancode::Escape) {
 			kdb_enter("keyboard sequence");
 			continue;
 		}
-		if (kbd_flags == ATKBD_FLAG_CONTROL && scancode == 0x29 /* tilde */)
+		if (kbd_flags == flag::Control && scancode == scancode::Tilde)
 			panic("forced by kdb");
 #endif
 
-		if (kbd_flags == (ATKBD_FLAG_CONTROL | ATKBD_FLAG_ALT) && scancode == 83 /* delete */)
+		if (kbd_flags == (flag::Control | flag::Alt) && scancode == scancode::Delete)
 			md_reboot();
 
-		uint8_t ch = ((kbd_flags & ATKBD_FLAG_SHIFT) ? atkbd_keymap_shift : atkbd_keymap)[scancode];
+		// Look up the scancode - if this yields anything, store the key
+		uint8_t ch = ((kbd_flags & flag::Shift) ? atkbd_keymap_shift : atkbd_keymap)[scancode];
 		if (ch != 0)
 			kbdmux_on_input(ch);
 	}
