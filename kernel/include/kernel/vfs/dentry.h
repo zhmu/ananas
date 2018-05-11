e1dae4aaad7965c98d7f6142fc35eb8e1f2e4d8c
Rink Springer <rink@rink.nu>
2018-05-11 09:58:34 +0200

    kernel/vfs: Correctly initialize DEntry objects
    This will prevent all kinds of seemingly random panics due to dereferencing
    non-nullptr d_parent's.

--- kernel/include/kernel/vfs/dentry.h
+++ kernel/include/kernel/vfs/dentry.h
@@ -11,14 +11,14 @@ struct VFS_MOUNTED_FS;
 struct INode;
 
 struct DEntry : util::List<DEntry>::NodePtr {
-	refcount_t d_refcount;			/* Reference count, >0 */
-	struct VFS_MOUNTED_FS* d_fs;
-	DEntry* d_parent;			/* Parent directory entry */
-	INode*	d_inode;			/* Backing entry inode, or NULL */
-	uint32_t d_flags;			/* Item flags */
+	refcount_t d_refcount{};		/* Reference count, >0 */
+	struct VFS_MOUNTED_FS* d_fs = nullptr;
+	DEntry* d_parent = nullptr;		/* Parent directory entry */
+	INode*	d_inode = nullptr;		/* Backing entry inode, or NULL */
+	uint32_t d_flags = 0;			/* Item flags */
 #define DENTRY_FLAG_NEGATIVE	0x0001		/* Negative entry; does not exist */
 #define DENTRY_FLAG_ROOT			0x0002		/* Root dentry; must not be removed */
-	char	d_entry[DCACHE_MAX_NAME_LEN];	/* Entry name */
+	char	d_entry[DCACHE_MAX_NAME_LEN] = {};/* Entry name */
 };
 
 typedef util::List<DEntry> DEntryList;
