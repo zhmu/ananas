6fe9f565e1d85bcd140414a1db04f588d1bfade6
Rink Springer <rink@rink.nu>
2018-05-26 12:50:34 +0200

    util: Add util::refcounted<> as a base for all referenced counted objects
    The idea is to prevent weird things like unlocking on deref etc. This class only does refcounting, nothing else.

---
+++ include/ananas/util/refcounted.h
@@ -0,0 +1,63 @@
+#ifndef ANANAS_UTIL_REFCOUNTED_H
+#define ANANAS_UTIL_REFCOUNTED_H
+
+#ifdef KERNEL
+#include "kernel/lib.h" // for KASSERT
+#endif
+#include "atomic.h"
+
+namespace util {
+
+namespace detail {
+
+// Default cleanup just delete's the object
+template<typename T>
+struct default_refcounted_cleanup
+{
+	static void Cleanup(T* obj) {
+		delete obj;
+	}
+};
+
+} // namespace detail
+
+template<typename T, typename Cleanup = detail::default_refcounted_cleanup<T> >
+class refcounted
+{
+public:
+	void AddReference()
+	{
+		++rc_refcount;
+	}
+
+	void RemoveReference()
+	{
+		if (--rc_refcount > 0)
+			return;
+
+		Cleanup::Cleanup(static_cast<T*>(this));
+	}
+
+	// HACK This is only for DEBUGGING / TESTING use
+	int GetReferenceCount() const
+	{
+		return rc_refcount;
+	}
+
+protected:
+	refcounted() = default;
+	~refcounted()
+	{
+		KASSERT(rc_refcount == 0, "destroyed while still %d refs", GetReferenceCount());
+	}
+
+	// Prevent copying and assignment
+	refcounted(const refcounted&) = delete;
+	refcounted& operator=(const refcounted&) = delete;
+
+private:
+	util::atomic<int> rc_refcount{1}; // caller gets one ref
+};
+
+} // namespace util
+#endif // ANANAS_UTIL_REFCOUNTED_H
---
+++ unittests/util/refcounted-test.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+
+#define KASSERT(x, ...) \
+	EXPECT_TRUE(x) << __VA_ARGS__
+
+#include "../../include/ananas/util/refcounted.h"
+
+TEST(Refcounted, refcounted)
+{
+	struct Dummy : util::refcounted<Dummy>
+	{
+		Dummy(int& d)
+		 : destroyed(d)
+		{
+		}
+
+		~Dummy()
+		{
+			++destroyed;
+		}
+
+		int& destroyed;
+	};
+
+	{
+		int destructor_calls{};
+		auto dummy = new Dummy(destructor_calls);
+		EXPECT_EQ(1, dummy->GetReferenceCount());
+		dummy->RemoveReference();
+		// dummy is removed here now
+		EXPECT_EQ(1, destructor_calls);
+	}
+
+	{
+		int destructor_calls{};
+		auto dummy = new Dummy(destructor_calls);
+		EXPECT_EQ(1, dummy->GetReferenceCount());
+		dummy->AddReference();
+		EXPECT_EQ(2, dummy->GetReferenceCount());
+		EXPECT_EQ(0, destructor_calls);
+		dummy->AddReference();
+		EXPECT_EQ(3, dummy->GetReferenceCount());
+		EXPECT_EQ(0, destructor_calls);
+		dummy->RemoveReference();
+		EXPECT_EQ(2, dummy->GetReferenceCount());
+		EXPECT_EQ(0, destructor_calls);
+		dummy->RemoveReference();
+		EXPECT_EQ(1, dummy->GetReferenceCount());
+		EXPECT_EQ(0, destructor_calls);
+		dummy->RemoveReference();
+		EXPECT_EQ(1, destructor_calls);
+	}
+}
