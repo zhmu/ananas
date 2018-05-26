5334d2dc09900da80926d94d8e29d6d4b3435710
Rink Springer <rink@rink.nu>
2018-05-26 14:38:16 +0200

    util: Introduce util::locked<> as a wrapper to ensure an object is locked
    This takes advantage of C++11's move semantics as they are essential for this to work.

---
+++ include/ananas/util/locked.h
@@ -0,0 +1,98 @@
+#ifndef ANANAS_UTIL_LOCKED_H
+#define ANANAS_UTIL_LOCKED_H
+
+#ifdef KERNEL
+#include "kernel/lib.h" // for KASSERT
+#endif
+
+namespace util {
+
+/*
+ * Expresses a currently locked object: if this holds a pointer, that pointer
+ * is locked until it is explicitly transferred or unlocked.
+ *
+ * - Default constructors owns nothing
+ * - Other locked<T>'s can be moved, which makes them own nothing
+ * - Unlock() must be called (dangling locks are not allowed!)
+ * - Extract() can be used to extract the locked object (object will own nothing)
+ *
+ * Extract() is mainly useful for cleanup scenarios where there is nothing left to check.
+ */
+template<typename T>
+class locked
+{
+public:
+	locked() = default;
+	locked(const locked&) = delete;
+	locked& operator=(const locked&) = delete;
+
+	explicit locked(T& obj) noexcept
+	 : l_obj(&obj)
+	{
+		l_obj->AssertLocked();
+	}
+
+	locked(locked&& rhs) noexcept
+	 : l_obj(rhs.l_obj)
+	{
+		l_obj->AssertLocked();
+		rhs.l_obj = nullptr;
+	}
+
+	locked& operator=(locked&& rhs) noexcept
+	{
+		KASSERT(!isLocked(), "object still locked");
+		l_obj = rhs.l_obj;
+		if (l_obj != nullptr)
+			l_obj->AssertLocked();
+		rhs.l_obj = nullptr;
+		return *this;
+	}
+
+	~locked() noexcept
+	{
+		KASSERT(!isLocked(), "object still locked");
+	}
+
+	T* operator->() const noexcept
+	{
+		return l_obj;
+	}
+
+	T& operator*() const noexcept
+	{
+		return *l_obj;
+	}
+
+	void Unlock() noexcept
+	{
+		KASSERT(isLocked(), "no object present");
+		l_obj->Unlock();
+		l_obj = nullptr;
+	}
+
+	T* Extract() noexcept
+	{
+		KASSERT(isLocked(), "no object present");
+		T* obj = l_obj;
+		l_obj = nullptr;
+		return obj;
+	}
+
+	operator bool() const noexcept
+	{
+		return isLocked();
+	}
+
+private:
+	T* l_obj = nullptr;
+
+	bool isLocked() const
+	{
+		return l_obj != nullptr;
+	}
+};
+
+} // namespace util
+
+#endif // ANANAS_UTIL_LOCKED_H
---
+++ unittests/util/locked-test.cpp
@@ -0,0 +1,105 @@
+#include <gtest/gtest.h>
+#define KASSERT(x, ...) \
+	EXPECT_TRUE(x) << __VA_ARGS__
+#include "../../include/ananas/util/locked.h"
+#include "../../include/ananas/util/utility.h"
+
+struct Widget
+{
+	Widget(int v = {})
+	 : w_value(v)
+	{
+	}
+
+	void Lock() {
+		w_locked = true;
+	}
+
+	void Unlock() {
+		w_locked = false;
+	}
+
+	void AssertLocked() {
+		EXPECT_TRUE(w_locked);
+	}
+
+	void AssertUnlocked() {
+		EXPECT_FALSE(w_locked);
+	}
+
+	int w_value;
+	bool w_locked = false;
+};
+
+TEST(Locked, constructor)
+{
+	util::locked<Widget> lw;
+}
+
+TEST(Locked, basic_usage)
+{
+	Widget w(123);
+	w.Lock();
+
+	{
+		util::locked<Widget> lw(w);
+		EXPECT_EQ(lw->w_value, 123);
+		EXPECT_EQ(&w, &*lw);
+		lw.Unlock();
+	}
+
+	w.AssertUnlocked();
+}
+
+TEST(Locked, extract_object)
+{
+	Widget w(123);
+	w.Lock();
+
+	auto p = [](Widget& w) {
+		util::locked<Widget> lw(w);
+		return lw.Extract(); // note: must not assert as we give up ownership of w here
+	}(w);
+	EXPECT_EQ(p, &w);
+	EXPECT_TRUE(w.w_locked);
+	w.Unlock();
+
+}
+
+TEST(Locked, transfer_object)
+{
+	Widget w;
+	w.Lock();
+
+	util::locked<Widget> lw1(w);
+	util::locked<Widget> lw2(util::move(lw1));
+	lw2.Unlock();
+	w.AssertUnlocked();
+
+}
+
+TEST(Locked, transfer_temporary)
+{
+	Widget w;
+	w.Lock();
+
+	util::locked<Widget> lw;
+	lw = util::locked<Widget>(w);
+	lw.Unlock();
+	w.AssertUnlocked();
+}
+
+TEST(Locked, transfer_return)
+{
+	Widget w;
+	w.Lock();
+
+	auto func = [&](Widget& w) {
+		util::locked<Widget> lw(w);
+		return lw;
+	};
+
+	auto lw = func(w);
+	lw.Unlock();
+	w.AssertUnlocked();
+}
