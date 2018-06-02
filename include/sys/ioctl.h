939b91941a5b601bdf5201dc2b0df4ad19ea6622
Rink Springer <rink@rink.nu>
2018-06-02 12:16:51 +0200

    include: Add correct C++ support stuff to ioctl.h

--- include/sys/ioctl.h
+++ include/sys/ioctl.h
@@ -1,6 +1,12 @@
 #ifndef __SYS_IOCTL_H__
 #define __SYS_IOCTL_H__
 
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
 int ioctl(int fd, unsigned long request, ...);
+
+__END_DECLS
 
 #endif /* __SYS_IOCTL_H__ */
