e3f79a2c0d348c39a89e5448a782fc0bf736b396
Rink Springer <rink@rink.nu>
2018-08-07 18:48:33 +0200

    Allow setjmp.h to be used from C++
    Note that you should ever _want_ to...

--- include/setjmp.h
+++ include/setjmp.h
@@ -1,10 +1,16 @@
 #ifndef __SETJMP_H__
 #define __SETJMP_H__
 
+#include <sys/cdefs.h>
+
 /* jmp_buf is architecture-dependant */
 #include <machine/setjmp.h>
 
+__BEGIN_DECLS
+
 int setjmp(jmp_buf env);
 void longjmp(jmp_buf env, int val);
+
+__END_DECLS
 
 #endif /* __SETJMP_H__ */
