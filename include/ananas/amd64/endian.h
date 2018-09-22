fce27ecd06cb4c44b80f2cb48fa93a396c1ca6a7
Rink Springer <rink@rink.nu>
2018-09-22 10:04:28 +0200

    include: Provide endian.h
    This is needed by libcxx

---
+++ include/ananas/amd64/endian.h
@@ -0,0 +1,10 @@
+#ifndef AMD64_ENDIAN_H
+#define AMD64_ENDIAN_H
+
+#define BIG_ENDIAN 1234
+#define LITTLE_ENDIAN 3412
+
+// x86 is little endian
+#define BYTE_ORDER LITTLE_ENDIAN
+
+#endif // AMD64_ENDIAN_H
