938740412182bf4871584f8ad4392ab20b4d2547
Rink Springer <rink@rink.nu>
2018-12-18 10:30:05 +0100

    Explicitly add license to contributed libc files

--- lib/libc/functions/stdlib/getprogname.c
+++ lib/libc/functions/stdlib/getprogname.c
@@ -1,3 +1,9 @@
+/*-
+ * SPDX-License-Identifier: Zlib
+ *
+ * Copyright (c) 2009-2018 Rink Springer <rink@rink.nu>
+ * For conditions of distribution and use, see LICENSE file
+ */
 #include <stdlib.h>
 
 extern const char* __progname;
--- lib/libc/platform/ananas/functions/unimplemented/chown.c
+++ lib/libc/platform/ananas/functions/unimplemented/chown.c
@@ -1,3 +1,9 @@
+/*-
+ * SPDX-License-Identifier: Zlib
+ *
+ * Copyright (c) 2009-2018 Rink Springer <rink@rink.nu>
+ * For conditions of distribution and use, see LICENSE file
+ */
 #include <unistd.h>
 
 int chown(const char* path, uid_t owner, gid_t group)
