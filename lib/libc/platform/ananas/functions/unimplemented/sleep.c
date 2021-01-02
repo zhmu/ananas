3e6d51239263663dc1097d9c1fa7778302c59b72
Rink Springer <rink@rink.nu>
2021-01-02 10:58:27 +0100

    libc: stub sleep()
    libstdc++ requires this being available

---
+++ lib/libc/platform/ananas/functions/unimplemented/sleep.c
@@ -0,0 +1,12 @@
+/*-
+ * SPDX-License-Identifier: Zlib
+ *
+ * Copyright (c) 2009-2020 Rink Springer <rink@rink.nu>
+ * For conditions of distribution and use, see LICENSE file
+ */
+#include <unistd.h>
+
+unsigned int sleep(unsigned int seconds)
+{
+    return seconds;
+}
