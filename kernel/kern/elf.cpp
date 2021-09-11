82e25efa7fbb4b2564ea1f209a5158f033653bda
Rink Springer <rink@rink.nu>
2021-09-11 17:38:09 +0200

    kernel: Clean up ELF code a bit
    This is in preparation for making the userland allocation/fill stuff that is
    duplicated in elf.cpp and userland-init.cpp common.

--- kernel/kern/elf.cpp
+++ kernel/kern/elf.cpp
@@ -56,7 +56,7 @@ namespace
         return n;
     }
 
-    static Result read_data(DEntry& dentry, void* buf, off_t offset, size_t len)
+    Result read_data(DEntry& dentry, void* buf, off_t offset, size_t len)
     {
         struct VFS_FILE f;
         memset(&f, 0, sizeof(f));
@@ -75,7 +75,7 @@ namespace
         return Result::Success();
     }
 
-    static Result LoadEhdr(DEntry& dentry, Elf64_Ehdr& ehdr)
+    Result LoadEhdr(DEntry& dentry, Elf64_Ehdr& ehdr)
     {
         if (auto result = read_data(dentry, &ehdr, 0, sizeof(ehdr)); result.IsFailure())
             return result;
@@ -104,9 +104,6 @@ namespace
         return Result::Success();
     }
 
-} // unnamed namespace
-
-struct ELF64Loader final : IExecutor {
     struct AuxArgs {
         addr_t aa_interpreter_base = 0;
         addr_t aa_phdr = 0;
@@ -115,176 +112,136 @@ struct ELF64Loader final : IExecutor {
         addr_t aa_rip = 0;
     };
 
-    Result Verify(DEntry& dentry) override;
-    Result Load(VMSpace& vs, DEntry& dentry, void*& auxargs) override;
-    Result PrepareForExecute(
-        VMSpace& vs, Thread& t, void* auxargs, const char* argv[], const char* envp[]) override;
+    Result LoadPH(VMSpace& vs, DEntry& dentry, const Elf64_Phdr& phdr, addr_t rbase)
+    {
+        /* Construct the flags for the actual mapping */
+        unsigned int flags = vm::flag::User;
+        if (phdr.p_flags & PF_R)
+            flags |= vm::flag::Read;
+        if (phdr.p_flags & PF_W)
+            flags |= vm::flag::Write;
+        if (phdr.p_flags & PF_X)
+            flags |= vm::flag::Execute;
 
-  private:
-    Result LoadPH(VMSpace& vs, DEntry& dentry, const Elf64_Phdr& phdr, addr_t rbase) const;
-    Result LoadFile(
-        VMSpace& vs, DEntry& dentry, const Elf64_Ehdr& ehdr, addr_t rbase, addr_t& exec_addr) const;
-};
+        // If this mapping is writable, do not share it - don't want changes to end
+        // up in the inode
+        if (flags & vm::flag::Write)
+            flags |= vm::flag::Private;
 
-Result ELF64Loader::LoadPH(VMSpace& vs, DEntry& dentry, const Elf64_Phdr& phdr, addr_t rbase) const
-{
-    /* Construct the flags for the actual mapping */
-    unsigned int flags = vm::flag::User;
-    if (phdr.p_flags & PF_R)
-        flags |= vm::flag::Read;
-    if (phdr.p_flags & PF_W)
-        flags |= vm::flag::Write;
-    if (phdr.p_flags & PF_X)
-        flags |= vm::flag::Execute;
+        /*
+         * We can only map things at PAGE_SIZE offsets and these must start at some
+         * PAGE_SIZE-aligned offset.
+         *
+         * Note that we may map extra bytes here, this shouldn't really matter since
+         * everything we map here won't make it back to disk, and it is likely
+         * available anyway (in theory, the file could be execute-only, but you can
+         * still read the extra pieces in memory...)
+         *
+         * Finally, we have a separate mapping for things outside of the file as
+         * that makes stuff easier to manage (plus, mapping extending past the file
+         * length may be silently truncated)
+         *
+         *  virt_extra                      virt_end
+         * <---------->                        /
+         * +----------+------------+----------+
+         * |          |XXXXXXXXXXXX|          |
+         * +----------+------------+----------+
+         *           /             |
+         *       virt_begin    ROUND_UP(v_addr + filesz)
+         */
+        addr_t virt_begin = ROUND_DOWN(phdr.p_vaddr, PAGE_SIZE);
+        addr_t virt_end = ROUND_UP((phdr.p_vaddr + phdr.p_filesz), PAGE_SIZE);
+        addr_t virt_extra = phdr.p_vaddr - virt_begin;
+        off_t doffset = phdr.p_offset - virt_extra;
+        off_t filesz = phdr.p_filesz + virt_extra;
+        if (doffset & (PAGE_SIZE - 1)) {
+            kprintf("elf64_load_ph: refusing to map offset %d not a page-multiple\n", doffset);
+            return Result::Failure(ENOEXEC);
+        }
 
-    // If this mapping is writable, do not share it - don't want changes to end
-    // up in the inode
-    if (flags & vm::flag::Write)
-        flags |= vm::flag::Private;
-
-    /*
-     * We can only map things at PAGE_SIZE offsets and these must start at some
-     * PAGE_SIZE-aligned offset.
-     *
-     * Note that we may map extra bytes here, this shouldn't really matter since
-     * everything we map here won't make it back to disk, and it is likely
-     * available anyway (in theory, the file could be execute-only, but you can
-     * still read the extra pieces in memory...)
-     *
-     * Finally, we have a separate mapping for things outside of the file as
-     * that makes stuff easier to manage (plus, mapping extending past the file
-     * length may be silently truncated)
-     *
-     *  virt_extra                      virt_end
-     * <---------->                        /
-     * +----------+------------+----------+
-     * |          |XXXXXXXXXXXX|          |
-     * +----------+------------+----------+
-     *           /             |
-     *       virt_begin    ROUND_UP(v_addr + filesz)
-     */
-    addr_t virt_begin = ROUND_DOWN(phdr.p_vaddr, PAGE_SIZE);
-    addr_t virt_end = ROUND_UP((phdr.p_vaddr + phdr.p_filesz), PAGE_SIZE);
-    addr_t virt_extra = phdr.p_vaddr - virt_begin;
-    off_t doffset = phdr.p_offset - virt_extra;
-    off_t filesz = phdr.p_filesz + virt_extra;
-    if (doffset & (PAGE_SIZE - 1)) {
-        kprintf("elf64_load_ph: refusing to map offset %d not a page-multiple\n", doffset);
-        return Result::Failure(ENOEXEC);
-    }
-
-    // First step is to map the dentry-backed data
-    VMArea* va;
-    if (auto result = vs.MapToDentry(
-            VAInterval{ rbase + virt_begin, rbase + virt_end }, dentry,
-            DEntryInterval{ doffset, doffset + filesz }, flags, va);
-        result.IsFailure())
-        return result;
-    if (phdr.p_filesz == phdr.p_memsz)
-        return Result::Success();
-
-    // Now map the part that isn't dentry-backed, if we have enough
-    addr_t v_extra = virt_end;
-    addr_t v_extra_len = ROUND_UP(phdr.p_vaddr + phdr.p_memsz - v_extra, PAGE_SIZE);
-    if (v_extra_len == 0)
-        return Result::Success();
-    return vs.MapTo(VAInterval{ rbase + v_extra, rbase + v_extra + v_extra_len }, flags, va);
-}
-
-Result ELF64Loader::LoadFile(
-    VMSpace& vs, DEntry& dentry, const Elf64_Ehdr& ehdr, addr_t rbase, addr_t& exec_addr) const
-{
-    /* Process all program headers one by one */
-    for (unsigned int i = 0; i < ehdr.e_phnum; i++) {
-        Elf64_Phdr phdr;
-        if (auto result =
-                read_data(dentry, &phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(phdr));
+        // First step is to map the dentry-backed data
+        VMArea* va;
+        if (auto result = vs.MapToDentry(
+                VAInterval{ rbase + virt_begin, rbase + virt_end }, dentry,
+                DEntryInterval{ doffset, doffset + filesz }, flags, va);
             result.IsFailure())
             return result;
-        if (phdr.p_type != PT_LOAD)
-            continue;
+        if (phdr.p_filesz == phdr.p_memsz)
+            return Result::Success();
 
-        if (auto result = LoadPH(vs, dentry, phdr, rbase); result.IsFailure())
-            return result;
+        // Now map the part that isn't dentry-backed, if we have enough
+        addr_t v_extra = virt_end;
+        addr_t v_extra_len = ROUND_UP(phdr.p_vaddr + phdr.p_memsz - v_extra, PAGE_SIZE);
+        if (v_extra_len == 0)
+            return Result::Success();
+        return vs.MapTo(VAInterval{ rbase + v_extra, rbase + v_extra + v_extra_len }, flags, va);
     }
 
-    exec_addr = ehdr.e_entry;
-    return Result::Success();
-}
+    Result LoadFile(VMSpace& vs, DEntry& dentry, const Elf64_Ehdr& ehdr, addr_t rbase, addr_t& exec_addr)
+    {
+        /* Process all program headers one by one */
+        for (unsigned int i = 0; i < ehdr.e_phnum; i++) {
+            Elf64_Phdr phdr;
+            if (auto result =
+                    read_data(dentry, &phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(phdr));
+                result.IsFailure())
+                return result;
+            if (phdr.p_type != PT_LOAD)
+                continue;
 
-Result ELF64Loader::Verify(DEntry& dentry)
-{
-    // Fetch the ELF header (again - this could be nicer...)
-    Elf64_Ehdr ehdr;
-    if (auto result = LoadEhdr(dentry, ehdr); result.IsFailure())
-        return result;
+            if (auto result = LoadPH(vs, dentry, phdr, rbase); result.IsFailure())
+                return result;
+        }
 
-    // Only accept executables here
-    if (ehdr.e_type != ET_EXEC)
-        return Result::Failure(ENOEXEC);
+        exec_addr = ehdr.e_entry;
+        return Result::Success();
+    }
 
-    return Result::Success();
-}
+    Result LocateInterpreter(const Elf64_Ehdr& ehdr, DEntry& dentry, char*& interp)
+    {
+        for (unsigned int i = 0; i < ehdr.e_phnum; i++) {
+            Elf64_Phdr phdr;
+            if (auto result =
+                    read_data(dentry, &phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(phdr));
+                result.IsFailure())
+                return result;
+            switch (phdr.p_type) {
+                case PT_INTERP: {
+                    if (interp != nullptr) {
+                        kfree(interp);
+                        kprintf("elf64_load: multiple interp headers\n");
+                        return Result::Failure(ENOEXEC);
+                    }
 
-Result ELF64Loader::Load(VMSpace& vs, DEntry& dentry, void*& aa)
-{
-    // Fetch the ELF header (again - this could be nicer...)
-    Elf64_Ehdr ehdr;
-    if (auto result = LoadEhdr(dentry, ehdr); result.IsFailure())
-        return result;
+                    if (phdr.p_filesz >= PAGE_SIZE) {
+                        kprintf("elf64_load: interp too large\n");
+                        return Result::Failure(ENOEXEC);
+                    }
 
-    // Load the file
-    addr_t exec_addr;
-    if (auto result = LoadFile(vs, dentry, ehdr, 0, exec_addr); result.IsFailure())
-        return result;
-
-    // This went okay - see if there any other interesting headers here
-    char* interp = nullptr;
-    for (unsigned int i = 0; i < ehdr.e_phnum; i++) {
-        Elf64_Phdr phdr;
-        if (auto result =
-                read_data(dentry, &phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(phdr));
-            result.IsFailure())
-            return result;
-        switch (phdr.p_type) {
-            case PT_INTERP: {
-                if (interp != nullptr) {
-                    kfree(interp);
-                    kprintf("elf64_load: multiple interp headers\n");
-                    return Result::Failure(ENOEXEC);
-                }
-
-                if (phdr.p_filesz >= PAGE_SIZE) {
-                    kprintf("elf64_load: interp too large\n");
-                    return Result::Failure(ENOEXEC);
-                }
-
-                // Read the interpreter from disk
-                interp = static_cast<char*>(kmalloc(phdr.p_filesz + 1));
-                interp[phdr.p_filesz] = '\0';
-                if (auto result = read_data(dentry, interp, phdr.p_offset, phdr.p_filesz);
-                    result.IsFailure()) {
-                    kfree(interp);
-                    return result;
+                    // Read the interpreter from disk
+                    interp = static_cast<char*>(kmalloc(phdr.p_filesz + 1));
+                    interp[phdr.p_filesz] = '\0';
+                    if (auto result = read_data(dentry, interp, phdr.p_offset, phdr.p_filesz);
+                        result.IsFailure()) {
+                        kfree(interp);
+                        return result;
+                    }
                 }
             }
         }
+
+        return Result::Success();
     }
 
-    // Initialize auxiliary arguments; these will be passed using the auxv mechanism
-    auto auxargs = new AuxArgs;
-    auxargs->aa_entry = exec_addr;
-
-    if (interp != nullptr) {
+    Result LoadInterpreter(VMSpace& vs, const Elf64_Ehdr& ehdr, DEntry& dentry, const char* interp, AuxArgs* auxargs)
+    {
         // We need to use an interpreter to load this
         struct VFS_FILE interp_file;
         // Note that the nullptr requires interp to be absolute
         if (auto result = vfs_open(nullptr, interp, nullptr, &interp_file); result.IsFailure()) {
             kprintf("unable to load ELF interpreter '%s': %d\n", interp, result.AsStatusCode());
-            kfree(interp);
             return result;
         }
-        kfree(interp);
 
         // Load/verify the header
         Elf64_Ehdr interp_ehdr;
@@ -333,19 +290,8 @@ Result ELF64Loader::Load(VMSpace& vs, DEntry& dentry, void*& aa)
         // Invoke the interpreter instead of the executable; it can use AT_ENTRY to
         // determine the executable entry point
         auxargs->aa_rip = interp_entry + interp_rbase;
-    } else {
-        // Directly invoke the static executable; no interpreter present
-        auxargs->aa_rip = exec_addr;
+        return Result::Success();
     }
-
-    aa = static_cast<void*>(auxargs);
-    return Result::Success();
-}
-
-Result ELF64Loader::PrepareForExecute(
-    VMSpace& vs, Thread& t, void* aa, const char* argv[], const char* envp[])
-{
-    auto auxargs = static_cast<AuxArgs*>(aa);
 
     /*
      * Allocate userland stack here - there are a few reasons to do that here:
@@ -359,25 +305,92 @@ Result ELF64Loader::PrepareForExecute(
      *     auxv.
      * (5) We do not have to special-case the userland stack for cloning/exec.
      */
-    VMArea* va;
-    vs.MapTo(
-        VAInterval{ USERLAND_STACK_ADDR, USERLAND_STACK_ADDR + THREAD_STACK_SIZE },
-        vm::flag::User | vm::flag::Read | vm::flag::Write | vm::flag::Private, va);
+    VMPage& CreateStack(VMSpace& vs, const addr_t stackEnd)
+    {
+        VMArea* va;
+        vs.MapTo(
+            VAInterval{ USERLAND_STACK_ADDR, stackEnd },
+            vm::flag::User | vm::flag::Read | vm::flag::Write | vm::flag::Private, va);
 
-    // Pre-fault the first page so that we can put stuff in it
-    constexpr auto stack_end = USERLAND_STACK_ADDR + THREAD_STACK_SIZE;
-    constexpr auto stack_page_virt = stack_end - PAGE_SIZE;
-    auto& stack_vp = vmpage::Allocate(0);
-    const auto page_index = (stack_page_virt - USERLAND_STACK_ADDR) / PAGE_SIZE;
-    KASSERT(page_index < va->va_pages.size(), "oeps %d %d", page_index, va->va_pages.size());
-    va->va_pages[page_index] = &stack_vp;
-    stack_vp.Map(vs, *va, stack_end - PAGE_SIZE);
+        // Pre-fault the first page so that we can put stuff in it
+        auto stackPageVirt = stackEnd - PAGE_SIZE;
+        auto& stack_vp = vmpage::Allocate(0);
+        const auto page_index = (stackPageVirt  - USERLAND_STACK_ADDR) / PAGE_SIZE;
+        KASSERT(page_index < va->va_pages.size(), "oeps %d %d", page_index, va->va_pages.size());
+        va->va_pages[page_index] = &stack_vp;
+        stack_vp.Map(vs, *va, stackPageVirt);
+        return stack_vp;
+    }
+} // unnamed namespace
+
+struct ELF64Loader final : IExecutor {
+    Result Verify(DEntry& dentry) override;
+    Result Load(VMSpace& vs, DEntry& dentry, void*& auxargs) override;
+    Result PrepareForExecute(
+        VMSpace& vs, Thread& t, void* auxargs, const char* argv[], const char* envp[]) override;
+};
+
+Result ELF64Loader::Verify(DEntry& dentry)
+{
+    // Fetch the ELF header (again - this could be nicer...)
+    Elf64_Ehdr ehdr;
+    if (auto result = LoadEhdr(dentry, ehdr); result.IsFailure())
+        return result;
+
+    // Only accept executables here
+    if (ehdr.e_type != ET_EXEC)
+        return Result::Failure(ENOEXEC);
+
+    return Result::Success();
+}
+
+Result ELF64Loader::Load(VMSpace& vs, DEntry& dentry, void*& aa)
+{
+    // Fetch the ELF header (again - this could be nicer...)
+    Elf64_Ehdr ehdr;
+    if (auto result = LoadEhdr(dentry, ehdr); result.IsFailure())
+        return result;
+
+    // Load the file
+    addr_t exec_addr;
+    if (auto result = LoadFile(vs, dentry, ehdr, 0, exec_addr); result.IsFailure())
+        return result;
+
+    // This went okay - see if there any other interesting headers here
+    char* interp = nullptr;
+    if (auto result = LocateInterpreter(ehdr, dentry, interp); result.IsFailure())
+        return result;
+
+    // Initialize auxiliary arguments; these will be passed using the auxv mechanism
+    auto auxargs = new AuxArgs;
+    auxargs->aa_entry = exec_addr;
+
+    auto result = Result::Success();
+    if (interp != nullptr) {
+        result = LoadInterpreter(vs, ehdr, dentry, interp, auxargs);
+        kfree(interp);
+    } else {
+        // Directly invoke the static executable; no interpreter present
+        auxargs->aa_rip = exec_addr;
+    }
+
+    aa = static_cast<void*>(auxargs);
+    return result;
+}
+
+Result ELF64Loader::PrepareForExecute(
+    VMSpace& vs, Thread& t, void* aa, const char* argv[], const char* envp[])
+{
+    auto auxargs = static_cast<AuxArgs*>(aa);
+
+    const auto stackEnd = USERLAND_STACK_ADDR + THREAD_STACK_SIZE;
+    auto& stack_vp = CreateStack(vs, stackEnd);
 
     /*
      * Calculate the amount of space we need; the ELF ABI specifies the stack to
      * be laid out as follows:
      *
-     * padding_bytes_needed + 0              ends at stack_end
+     * padding_bytes_needed + 0              ends at stackEnd
      * data_bytes_needed    + <data>         contains strings for argv/envp
      *                      + AT_NULL
      *                      | auxv[...
@@ -401,14 +414,14 @@ Result ELF64Loader::PrepareForExecute(
     const size_t bytes_needed = record_bytes_needed + data_bytes_needed + padding_bytes_needed;
     KASSERT(bytes_needed < PAGE_SIZE, "TODO deal with more >1 page here!");
 
-    // Allocate a backing page for the userland stack and fill it
+    // Map the last page of userland stack and fill it
     {
         auto p = static_cast<char*>(kmem_map(
             stack_vp.GetPage()->GetPhysicalAddress(), PAGE_SIZE, vm::flag::Read | vm::flag::Write));
         memset(p, 0, PAGE_SIZE);
         {
             addr_t stack_ptr = reinterpret_cast<addr_t>(p) + PAGE_SIZE - bytes_needed;
-            register_t stack_data_ptr = stack_end - data_bytes_needed - padding_bytes_needed;
+            register_t stack_data_ptr = stackEnd - data_bytes_needed - padding_bytes_needed;
 
             place(stack_ptr, static_cast<register_t>(argc));
             for (size_t n = 0; n < argc; n++) {
@@ -446,7 +459,7 @@ Result ELF64Loader::PrepareForExecute(
     }
 
     t.SetName(argv[0]);
-    md::thread::SetupPostExec(t, auxargs->aa_rip, stack_end - bytes_needed);
+    md::thread::SetupPostExec(t, auxargs->aa_rip, stackEnd - bytes_needed);
 
     stack_vp.Unlock();
     delete auxargs;
