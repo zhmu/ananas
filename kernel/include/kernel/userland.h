56ae0e4ff9f88a0d01021e27450e55330ffde74c
Rink Springer <rink@rink.nu>
2021-09-14 22:56:35 +0200

    kernel: Forgot to add this file

---
+++ kernel/include/kernel/userland.h
@@ -0,0 +1,29 @@
+#include <ananas/types.h>
+
+struct VMPage;
+struct VMSpace;
+
+namespace userland
+{
+    VMPage& CreateStack(VMSpace& vs, const addr_t stackEnd);
+    VMPage& CreateCode(VMSpace& vs, const addr_t virt, const addr_t len);
+
+    void* MapPageToKernel(VMPage& vp);
+    void UnmapPage(void* p);
+
+    size_t CalculateVectorStorageInBytes(const char* p[], size_t& num_entries);
+    size_t CalculatePaddingBytesNeeded(size_t n);
+
+    template<typename T>
+    void Store(addr_t& ptr, const T& data, size_t n)
+    {
+        memcpy(reinterpret_cast<void*>(ptr), &data, n);
+        ptr += n;
+    }
+
+    template<typename T>
+    void Store(addr_t& ptr, const T& data)
+    {
+        Store(ptr, data, sizeof(data));
+    }
+}
