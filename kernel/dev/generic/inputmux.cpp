1802f887e6979f74289c7eeaefa6089976c3aff6
Rink Springer <rink@rink.nu>
2021-10-03 10:17:45 +0200

    kernel: Add inputmux device

--- kernel/dev/generic/inputmux.cpp
+++ kernel/dev/generic/inputmux.cpp
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: Zlib
  *
- * Copyright (c) 2009-2018 Rink Springer <rink@rink.nu>
+ * Copyright (c) 2009-2021 Rink Springer <rink@rink.nu>
  * For conditions of distribution and use, see LICENSE file
  */
 #include "kernel/lock.h"
@@ -41,4 +41,90 @@ namespace input_mux
         for (auto& consumer : consumers)
             consumer.OnEvent(event);
     }
+
+    namespace
+    {
+        class InputMux : public Device, IDeviceOperations, ICharDeviceOperations, IInputConsumer
+        {
+          public:
+            InputMux()
+                : mutex{"inputmux"}, dataAvailable{"inputmux"}
+            {
+                d_Major = device_manager::AllocateMajor();
+                d_Unit = 0;
+                strcpy(d_Name, "inputmux");
+            }
+            virtual ~InputMux() = default;
+
+            IDeviceOperations& GetDeviceOperations() override { return *this; }
+            ICharDeviceOperations* GetCharDeviceOperations() override { return this; }
+
+            Result Attach() override {
+                RegisterConsumer(*this);
+                return Result::Success();
+            }
+            Result Detach() override {
+                UnregisterConsumer(*this);
+                return Result::Success();
+            }
+
+            Result Open(Process* p) override
+            {
+                // Flush the queue on opens to avoid old stuff getting in the
+                // way. This assumes there's only a single consumer at a time
+                MutexGuard g(mutex);
+                q_readpos = q_writepos;
+                return Result::Success();
+            }
+
+            Result Read(void* buf, size_t len, off_t offset) override
+            {
+                mutex.Lock();
+                while(true) {
+                    if (q_readpos != q_writepos) {
+                        const auto event = event_queue[q_readpos];
+                        q_readpos = (q_readpos + 1) % event_queue.size();
+                        mutex.Unlock();
+
+                        const auto chunk = len < sizeof(event) ? len : sizeof(len);
+                        memcpy(buf, &event, chunk);
+                        return Result::Success(chunk);
+                    }
+
+                    dataAvailable.Wait(mutex);
+                }
+                // NOTREACHED
+            }
+
+            bool CanRead() override
+            {
+                MutexGuard g(mutex);
+                return q_readpos != q_writepos;
+            }
+
+            Result Write(const void* buffer, size_t len, off_t offset) override
+            {
+                // Do not accept anything send to us just yet
+                return Result::Success(0);
+            }
+
+            void OnEvent(const AIMX_EVENT& event) override
+            {
+                MutexGuard g(mutex);
+                event_queue[q_writepos] = event;
+                q_writepos = (q_writepos + 1) % event_queue.size();
+                dataAvailable.Broadcast();
+            }
+
+            Mutex mutex;
+            ConditionVariable dataAvailable;
+            util::array<AIMX_EVENT, 32> event_queue;
+            unsigned int q_writepos = 0;
+            unsigned int q_readpos = 0;
+        };
+    }
+
+    const init::OnInit init(init::SubSystem::Device, init::Order::Last, []() {
+        device_manager::AttachSingle(*new InputMux);
+    });
 } // namespace input_mux
