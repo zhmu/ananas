84ee36fae1583bce3557c090f3f5a4f1c94b553d
Rink Springer <rink@rink.nu>
2021-10-03 10:25:41 +0200

    kernel: Implement openpt() syscall

--- kernel/sys/openpt.cpp
+++ kernel/sys/openpt.cpp
@@ -6,11 +6,24 @@
  */
 #include <ananas/types.h>
 #include "kernel/result.h"
+#include "kernel/dev/pseudotty.h"
+#include "kernel/process.h"
+#include "kernel/fd.h"
 #include "syscall.h"
 
 #include "kernel/lib.h"
 
 Result sys_openpt(int flags)
 {
-    panic("openpt");
+    auto& proc = process::GetCurrent();
+
+    /* Obtain a new handle */
+    FD* fd;
+    fdindex_t index_out;
+    if (auto result = fd::Allocate(FD_TYPE_FILE, proc, 0, fd, index_out); result.IsFailure())
+        return result;
+
+    auto pseudoTTY = pseudotty::AllocatePseudoTTY();
+    fd->fd_data.d_vfs_file.f_device = pseudoTTY->p_master; // XXX refcount
+    return Result::Success(index_out);
 }
