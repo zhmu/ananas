66caeed8cfe6f4915652499f513bfe5569208c0e
Rink Springer <rink@rink.nu>
2021-12-12 21:42:11 +0100

    newlib: Remove duplicate stuff from sys/ now that it is in ananas/

--- lib/newlib-4.1.0/newlib/libc/sys/ananas/sys/termios.h
+++ lib/newlib-4.1.0/newlib/libc/sys/ananas/sys/termios.h
@@ -1,1 +1,41 @@
+/*-
+ * SPDX-License-Identifier: Zlib
+ *
+ * Copyright (c) 2009-2021 Rink Springer <rink@rink.nu>
+ * For conditions of distribution and use, see LICENSE file
+ */
+#pragma once
+
 #include <ananas/termios.h>
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+#define TCSANOW 1
+#define TCSADRAIN 2
+#define TCSAFLUSH 3
+
+int tcgetattr(int fildes, struct termios* termios_p);
+int tcsetattr(int fildes, int optional_actions, const struct termios* termios_p);
+
+speed_t cfgetispeed(const struct termios* termios_p);
+speed_t cfgetospeed(const struct termios* termios_p);
+int cfsetispeed(struct termios* termios_p, speed_t speed);
+int cfsetospeed(struct termios* termios_p, speed_t speed);
+
+#define TCOOFF 1
+#define TCOON 2
+#define TCIOFF 3
+#define TCION 4
+
+int tcflow(int fildes, int action);
+
+#define TCIFLUSH 1
+#define TCOFLUSH 2
+#define TCIOFLUSH 3
+
+int tcflush(int fildes, int queue_selector);
+
+int tcsendbreak(int fildes, int duration);
+
+__END_DECLS
--- lib/newlib-4.1.0/newlib/libc/sys/ananas/sys/un.h
+++ lib/newlib-4.1.0/newlib/libc/sys/ananas/sys/un.h
@@ -4,16 +4,6 @@
  * Copyright (c) 2009-2021 Rink Springer <rink@rink.nu>
  * For conditions of distribution and use, see LICENSE file
  */
-#include <sys/socket.h>
+#pragma once
 
-#ifndef __SYS_UN_H__
-#define __SYS_UN_H__
-
-#define UNIX_PATH_MAX 96
-
-struct sockaddr_un {
-    sa_family_t sun_family;
-    char sun_path[UNIX_PATH_MAX];
-};
-
-#endif /* __SYS_UN_H__ */
+#include <ananas/net/un.h>
